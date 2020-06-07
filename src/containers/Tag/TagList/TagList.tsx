3e9e31a935386fb690bfe6135cc4d47b428a865a
Kurund Jalmi <kurund.jalmi@webaccessglobal.com>
2020-06-07 20:48:52 +0530

    listing is now working with backend

--- src/containers/Tag/TagList/TagList.tsx
+++ src/containers/Tag/TagList/TagList.tsx
@@ -1,45 +1,89 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useState } from 'react';
 import { Redirect, Link } from 'react-router-dom';
+import { useQuery, gql } from '@apollo/client';
+import {
+  Paper,
+  TableContainer,
+  Table,
+  TableRow,
+  TableCell,
+  TableHead,
+  TableBody,
+} from '@material-ui/core';
 
-import { AppState } from '../../../config/store';
 import { Tag } from '../../../store/Tag/types';
-import * as tagActions from '../../../store/Tag/actions';
+import styles from './TagList.module.css';
+
+const GET_TAGS = gql`
+  {
+    tags {
+      id
+      label
+      description
+    }
+  }
+`;
+
+const DELETE_TAG = gql`
+  mutation deleteTag($id: ID!) {
+    deleteTag(id: $id) {
+      errors {
+        key
+        message
+      }
+    }
+  }
+`;
 
 export interface TagListProps {}
 
 export const TagList: React.SFC<TagListProps> = (props) => {
   const [newTag, setNewTag] = useState(false);
 
-  const tagList = useSelector((state: AppState) => {
-    return state.tag.tags;
-  });
+  const { loading, error, data } = useQuery(GET_TAGS);
 
-  const dispatch = useDispatch();
-  const onFetchTags = useCallback(() => {
-    dispatch(tagActions.fetchTags());
-  }, [dispatch]);
+  const onTagDelete = (tagId: number) => {};
 
-  const onTagDelete = (tagId: number) => {
-    dispatch(tagActions.deleteTag(tagId));
-  };
+  // TO-DO: Need to figure out how to use apollo with redux hence keeping below commented for now
+  // const tagList = useSelector((state: AppState) => {
+  //   return state.tag.tags;
+  // });
 
-  useEffect(() => {
-    onFetchTags();
-  }, [onFetchTags]);
+  // const dispatch = useDispatch();
+
+  // const onFetchTags = useCallback(() => {
+  //   dispatch(tagActions.fetchTags());
+  // }, [dispatch]);
+
+  // const onTagDelete = (tagId: number) => {
+  //   dispatch(tagActions.deleteTag(tagId));
+  // };
+
+  // useEffect(() => {
+  //   onFetchTags();
+  // }, [onFetchTags]);
 
   if (newTag) {
     return <Redirect to="/tag/add" />;
   }
 
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  const tagList = data.tags;
+
   let listing: any;
   if (tagList.length > 0) {
     listing = tagList.map((n: Tag) => {
       return (
-        <div key={n.id}>
-          {n.name} <Link to={'/tag/' + n.id + '/edit'}>Edit</Link>{' '}
-          <button onClick={() => onTagDelete(n.id)}>Delete</button>
-        </div>
+        <TableRow key={n.id}>
+          <TableCell component="th" scope="row">
+            {n.label}
+          </TableCell>
+          <TableCell>
+            <Link to={'/tag/' + n.id + '/edit'}>Edit</Link>{' '}
+            <button onClick={() => onTagDelete(n.id)}>Delete</button>
+          </TableCell>
+        </TableRow>
       );
     });
   } else {
@@ -53,11 +97,17 @@ export const TagList: React.SFC<TagListProps> = (props) => {
         <button onClick={() => setNewTag(true)}>New Tag</button>
       </div>
       <br />
-      <div>
-        <strong>Name</strong>
-      </div>
-      <br />
-      {listing}
+      <TableContainer component={Paper}>
+        <Table className={styles.Table} aria-label="tag listing">
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody></TableBody>
+          {listing}
+        </Table>
+      </TableContainer>
     </div>
   );
 };
