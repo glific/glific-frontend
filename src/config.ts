669eaa550330178d2600db2315eed8cc5dbe0dbd
Vaibhav Rathore <vaibhav@coloredcow.com>
2020-06-09 15:16:47 +0530

    created env.example

--- src/config.ts
+++ src/config.ts
@@ -1,19 +1,3 @@
 const envVariables = process.env;
-let uri: any;
 
-switch (envVariables.NODE_ENV) {
-  case 'development':
-    uri = envVariables.REACT_APP_DEVELOPMENT_API;
-    break;
-
-  case 'production':
-    uri = envVariables.REACT_APP_PRODUCTION_API;
-    break;
-
-  case 'test':
-    break;
-
-  default:
-}
-
-export const URI = uri;
+export const URI = envVariables.REACT_APP_GLIFIC_API;
