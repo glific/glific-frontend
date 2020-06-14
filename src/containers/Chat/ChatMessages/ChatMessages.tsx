0bc6b015efe250bb9b93545788ccdc67d3f8da91
Kurund Jalmi <kurund.jalmi@webaccessglobal.com>
2020-06-14 15:54:55 +0530

    now we can save the messages to backend, time get started with subscriptions

--- src/containers/Chat/ChatMessages/ChatMessages.tsx
+++ src/containers/Chat/ChatMessages/ChatMessages.tsx
@@ -1,11 +1,14 @@
-import React, { useState } from 'react';
-import { useQuery } from '@apollo/client';
+import React, { useCallback } from 'react';
+import { useQuery, useMutation } from '@apollo/client';
 
 import { ContactBar } from './ContactBar/ContactBar';
 import { ChatMessage } from './ChatMessage/ChatMessage';
 import { ChatInput } from './ChatInput/ChatInput';
 import styles from './ChatMessages.module.css';
-import { GET_CONVERSATION_MESSAGE_QUERY } from '../../../graphql/queries/Chat';
+import {
+  GET_CONVERSATION_MESSAGE_QUERY,
+  CREATE_MESSAGE_MUTATION,
+} from '../../../graphql/queries/Chat';
 import { Container } from '@material-ui/core';
 
 export interface ChatMessagesProps {
@@ -27,35 +30,78 @@ interface ChatMessagesInterface {
   };
 }
 
+interface ConversationResult {
+  chatMessages: any[];
+}
+
 type OptionalChatQueryResult = ChatMessagesInterface | null;
 
 export const ChatMessages: React.SFC<ChatMessagesProps> = ({ chatId }) => {
-  const [chat, setChat] = useState<OptionalChatQueryResult>(null);
-
   const { loading, error, data } = useQuery<any>(GET_CONVERSATION_MESSAGE_QUERY, {
     variables: {
       nc: 1,
-      sc: 25,
+      sc: 100,
       filter: { id: chatId },
     },
   });
 
+  const chatMessages = data?.conversations;
+  const [createMessage] = useMutation(CREATE_MESSAGE_MUTATION);
+
+  const onSendMessage = useCallback(
+    (body: string) => {
+      const payload = {
+        body: body,
+        senderId: 1,
+        receiverId: chatId,
+        type: 'TEXT',
+        flow: 'OUTBOUND',
+      };
+
+      createMessage({
+        variables: { input: payload },
+        optimisticResponse: {
+          __typename: 'Mutation',
+          createMessage: {
+            __typename: 'Message',
+            body: body,
+            senderId: 1,
+            receiverId: chatId,
+            flow: 'OUTBOUND',
+            type: 'TEXT',
+          },
+        },
+        update: (client, { data }) => {
+          if (data && data.createMessage) {
+            // add new conversation
+            const newConversations = {
+              ...chatMessages[0],
+              messages: chatMessages[0].messages.concat(data.createMessage.message),
+            };
+
+            client.writeQuery({
+              query: GET_CONVERSATION_MESSAGE_QUERY,
+              variables: { chatId },
+              data: {
+                chatMessages: { ...chatMessages, 0: { ...newConversations } },
+              },
+            });
+          }
+        },
+      });
+    },
+    [chatMessages, chatId, createMessage]
+  );
+
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
 
-  if (data === undefined || data.conversations === undefined) {
-    return null;
-  }
-
-  const onSendMessage = (content: string) => {};
-
-  let conversations = data.conversations;
-
   let messageList;
   let contactName;
-  if (conversations.length > 0) {
-    contactName = conversations[0].contact.name;
-    messageList = conversations.map((conversation: any) => {
+
+  if (chatMessages.length > 0) {
+    contactName = chatMessages[0].contact.name;
+    messageList = chatMessages.map((conversation: any) => {
       return conversation.messages.map((message: any, index: number) => {
         return <ChatMessage {...message} key={index} />;
       });
--- src/graphql/queries/Chat.ts
+++ src/graphql/queries/Chat.ts
@@ -29,3 +29,22 @@ export const GET_CONVERSATION_MESSAGE_QUERY = gql`
     }
   }
 `;
+
+export const CREATE_MESSAGE_MUTATION = gql`
+  mutation createMessage($input: MessageInput!) {
+    createMessage(input: $input) {
+      message {
+        id
+        body
+        sender {
+          id
+        }
+        receiver {
+          id
+        }
+        type
+        flow
+      }
+    }
+  }
+`;
