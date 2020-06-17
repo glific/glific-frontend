d7daa3330ed26375a76d3a433e5c2e1c92313dd9
Vaibhav Rathore <vaibhav@coloredcow.com>
2020-06-17 22:25:47 +0530

    test case Button

---
+++ src/components/UI/Form/Button/Button.test.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+// import { Checkbox as CheckboxElement } from '@material-ui/core';
+import { Button } from './Button';
+
+describe('<Button />', () => {
+  let isClicked = false;
+  const buttonCallback = () => {
+    isClicked = true;
+  };
+
+  it('renders <Button /> component', () => {
+    const wrapper = shallow(<Button>My Button</Button>);
+    expect(wrapper).toBeTruthy();
+  });
+
+  it('should have correct label', () => {
+    const wrapper = shallow(<Button>My Button</Button>);
+    expect(wrapper.text()).toEqual('My Button');
+  });
+
+  it('should trigger onclick callback when clicked', () => {
+    const wrapper = shallow(<Button onClick={buttonCallback}>My Button</Button>);
+    wrapper.invoke('onClick')();
+    expect(isClicked).toEqual(true);
+  });
+});
