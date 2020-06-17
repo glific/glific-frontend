84b4dec60c9cd225725a4a6bad817a9c523311b7
Vaibhav Rathore <vaibhav@coloredcow.com>
2020-06-17 22:28:21 +0530

    removed conflicts

--- src/components/UI/Form/Dropdown/Dropdown.test.jsx
+++ src/components/UI/Form/Dropdown/Dropdown.test.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import Adapter from 'enzyme-adapter-react-16';
-import { shallow, configure } from 'enzyme';
+import { shallow } from 'enzyme';
 import { Select } from '@material-ui/core';
 import { Dropdown } from './Dropdown';
-
-configure({ adapter: new Adapter() });
 
 describe('<Dropdown />', () => {
   it('renders <Dropdown /> component', () => {
--- src/components/UI/Form/Input/Input.test.jsx
+++ src/components/UI/Form/Input/Input.test.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import Adapter from 'enzyme-adapter-react-16';
-import { shallow, configure } from 'enzyme';
+import { shallow } from 'enzyme';
 import { TextField } from '@material-ui/core';
 import { Input } from './Input';
-
-configure({ adapter: new Adapter() });
 
 describe('<Input />', () => {
   const wrapper = shallow(
