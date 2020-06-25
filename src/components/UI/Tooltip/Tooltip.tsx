a0b68d8731f1c2ef74307e786fca070a8812a79d
Cindy <cindyzhang0718@gmail.com>
2020-06-25 12:01:45 -0400

    changed name to TooltipElement

--- src/components/UI/Tooltip/Tooltip.tsx
+++ src/components/UI/Tooltip/Tooltip.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import * as tooltip from '@material-ui/core/Tooltip';
+import * as TooltipElement from '@material-ui/core/Tooltip';
 
 interface TooltipProps {
   title: String;
-  placement: tooltip.TooltipProps['placement'];
+  placement: TooltipElement.TooltipProps['placement'];
   children: React.ReactNode;
 }
 
 export const Tooltip: React.SFC<TooltipProps> = (props: TooltipProps) => {
   return (
-    <tooltip.default title={props.title} placement={props.placement} arrow>
+    <TooltipElement.default title={props.title} placement={props.placement} arrow>
       <div>{props.children}</div>
-    </tooltip.default>
+    </TooltipElement.default>
   );
 };
