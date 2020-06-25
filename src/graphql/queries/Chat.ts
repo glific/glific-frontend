4a1d508e4b1689f3e2aeaff7e5c795bfe0c2a62d
Calvin Chen <chencalvin99@berkeley.edu>
2020-06-24 17:50:56 -0700

    Updating single convo query endpoint

--- src/graphql/queries/Chat.ts
+++ src/graphql/queries/Chat.ts
@@ -1,26 +1,26 @@
 import { gql } from '@apollo/client';
 
-export const GET_CONVERSATION_QUERY = gql`
-  query conversations($count: Int!, $size: Int!) {
-    conversations(numberOfConversations: $count, sizeOfConversations: $size) {
-      contact {
-        id
-        name
-      }
-      messages {
-        id
-        body
-        insertedAt
-        tags {
-          id
-          label
-        }
-      }
-    }
-  }
-`;
+// export const GET_CONVERSATION_QUERY = gql`
+//   query conversations($count: Int!, $size: Int!) {
+//     conversations(numberOfConversations: $count, sizeOfConversations: $size) {
+//       contact {
+//         id
+//         name
+//       }
+//       messages {
+//         id
+//         body
+//         insertedAt
+//         tags {
+//           id
+//           label
+//         }
+//       }
+//     }
+//   }
+// `;
 
-export const NEW_CONVO_QUERY = gql`
+export const GET_CONVERSATION_QUERY = gql`
   query conversations($contactOpts: Opts!, $filter: ConversationsFilter!, $messageOpts: Opts!) {
     conversations(contactOpts: $contactOpts, filter: $filter, messageOpts: $messageOpts) {
       contact {
@@ -40,9 +40,35 @@ export const NEW_CONVO_QUERY = gql`
   }
 `;
 
+// export const GET_CONVERSATION_MESSAGE_QUERY = gql`
+//   query conversation($size: Int!, $contactId: Gid!, $filter: ConversationFilter!) {
+//     conversation(sizeOfConversations: $size, contactId: $contactId, filter: $filter) {
+//       contact {
+//         id
+//         name
+//       }
+//       messages {
+//         id
+//         body
+//         insertedAt
+//         receiver {
+//           id
+//         }
+//         sender {
+//           id
+//         }
+//         tags {
+//           id
+//           label
+//         }
+//       }
+//     }
+//   }
+// `;
+
 export const GET_CONVERSATION_MESSAGE_QUERY = gql`
-  query conversation($size: Int!, $contactId: Gid!, $filter: ConversationFilter!) {
-    conversation(sizeOfConversations: $size, contactId: $contactId, filter: $filter) {
+  query conversation($contactId: Gid!, $filter: ConversationFilter!, $messageOpts: Opts!) {
+    conversation(contactId: $contactId, filter: $filter, messageOpts: $messageOpts) {
       contact {
         id
         name
