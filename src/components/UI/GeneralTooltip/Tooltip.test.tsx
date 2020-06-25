dd4560b0dfd96b77c1e95bd9de8ee2ac957da422
Cindy <cindyzhang0718@gmail.com>
2020-06-25 00:25:45 -0400

    added more tests

--- src/components/UI/GeneralTooltip/Tooltip.test.tsx
+++ src/components/UI/GeneralTooltip/Tooltip.test.tsx
@@ -5,12 +5,39 @@ import { Tooltip } from './Tooltip';
 describe('Tooltip test', () => {
   const createTooltip = () => (
     <Tooltip title="test" placement="right">
-      <h1>children</h1>
+      children
     </Tooltip>
   );
 
   it('renders component properly', () => {
     const wrapper = shallow(createTooltip());
     expect(wrapper).toBeTruthy();
+  });
+
+  it('renders Tooltip', () => {
+    const wrapper = shallow(createTooltip());
+    expect(wrapper.find(Tooltip)).toBeTruthy();
+    console.log(wrapper.props());
+    console.log('hi');
+  });
+
+  it('displays correct title', () => {
+    const wrapper = shallow(createTooltip());
+    expect(wrapper.prop('title')).toEqual('test');
+  });
+
+  it('has the correct placement', () => {
+    const wrapper = shallow(createTooltip());
+    expect(wrapper.prop('placement')).toEqual('right');
+  });
+
+  it('component shows arrow', () => {
+    const wrapper = shallow(createTooltip());
+    expect(wrapper.prop('arrow')).toEqual(true);
+  });
+
+  it('receives children as prop', () => {
+    const wrapper = shallow(createTooltip());
+    expect(wrapper.prop('children')).toBeTruthy;
   });
 });
