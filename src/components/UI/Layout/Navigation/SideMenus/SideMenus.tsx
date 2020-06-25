4592f04ba1d2cfcfb6e396c0230c7e80f788322d
Kurund Jalmi <kurund.jalmi@webaccessglobal.com>
2020-06-25 14:59:16 +0530

    warning fixes

--- src/components/UI/Layout/Navigation/SideMenus/SideMenus.tsx
+++ src/components/UI/Layout/Navigation/SideMenus/SideMenus.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { ListItem, ListItemIcon, ListItemText, List } from '@material-ui/core';
 
 import { sideDrawerMenus } from '../../../../../config/menu';
