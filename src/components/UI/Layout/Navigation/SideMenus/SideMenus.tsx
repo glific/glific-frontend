1ce02cae1ccfd5b22884b57d36b5d33475b5f227
Calvin Chen <chencalvin99@berkeley.edu>
2020-06-25 13:20:22 -0700

    Minor changes to MenuItem list to support showing which route is selected

--- src/components/UI/Layout/Navigation/SideMenus/SideMenus.tsx
+++ src/components/UI/Layout/Navigation/SideMenus/SideMenus.tsx
@@ -8,9 +8,17 @@ import ListIcon from '../../../ListIcon/ListIcon';
 export interface SideMenusProps {}
 
 const SideMenus: React.SFC<SideMenusProps> = () => {
-  const menuList = sideDrawerMenus.map((menu) => {
+  const [selectedIndex, setSelectedIndex] = React.useState(0);
+  const menuList = sideDrawerMenus.map((menu, i) => {
     return (
-      <ListItem button key={menu.icon} component={NavLink} to={menu.path}>
+      <ListItem
+        button
+        selected={selectedIndex === i}
+        key={menu.icon}
+        component={NavLink}
+        to={menu.path}
+        onClick={() => setSelectedIndex(i)}
+      >
         <ListItemIcon>
           <ListIcon icon={menu.icon} />
         </ListItemIcon>
