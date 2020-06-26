215e37415f27de0001dbffbbc86125716bda1f09
Dorjay Yolmo <dorjay.yolmo@think201.com>
2020-06-26 14:20:26 +0530

    Include Mock Provider

--- src/containers/Chat/ChatMessages/ChatMessage/ChatMessage.test.tsx
+++ src/containers/Chat/ChatMessages/ChatMessage/ChatMessage.test.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import ChatMessage from './ChatMessage';
+import { ApolloProvider } from '@apollo/client';
+
+import gqlClient from '../../../../config/apolloclient';
 import moment from 'moment';
 
 describe('<ChatMessage />', () => {
@@ -16,7 +19,13 @@ describe('<ChatMessage />', () => {
     insertedAt,
   };
 
-  const wrapper = shallow(<ChatMessage {...defaultProps} />);
+  const wrapper = mount(
+    <ApolloProvider client={gqlClient}>
+      <ChatMessage {...defaultProps} />
+    </ApolloProvider>
+  );
+
+  // const wrapper = shallow(<ChatMessage {...defaultProps} />);
   test('it should render the message content correctly', () => {
     expect(wrapper.find('[data-testid="content"]').text()).toEqual('Hello there!');
   });
