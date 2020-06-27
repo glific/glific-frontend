a699f08661e3adc348cd2444954c47162991fe95
Calvin Chen <chencalvin99@berkeley.edu>
2020-06-26 18:11:16 -0700

    Adding Material UI mini variant drawer functionality

--- src/components/UI/Layout/Navigation/SideDrawer/SideDrawer.tsx
+++ src/components/UI/Layout/Navigation/SideDrawer/SideDrawer.tsx
@@ -10,7 +10,11 @@ import {
   Toolbar,
   Typography,
 } from '@material-ui/core';
-
+import clsx from 'clsx';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import SideMenus from '../SideMenus/SideMenus';
 import * as constants from '../../../../../common/constants';
 
@@ -20,15 +24,66 @@ const drawerWidth = constants.SIDE_DRAWER_WIDTH;
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
+    root: {
+      display: 'flex',
+    },
+    appBar: {
+      zIndex: theme.zIndex.drawer + 1,
+      transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+    },
+    appBarShift: {
+      marginLeft: drawerWidth,
+      width: `calc(100% - ${drawerWidth}px)`,
+      transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    menuButton: {
+      marginRight: 36,
+    },
+    hide: {
+      display: 'none',
+    },
     drawer: {
+      width: drawerWidth,
+      flexShrink: 0,
+      whiteSpace: 'nowrap',
+    },
+    drawerOpen: {
+      width: drawerWidth,
+      transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    drawerClose: {
+      transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      overflowX: 'hidden',
+      width: theme.spacing(7) + 1,
       [theme.breakpoints.up('sm')]: {
-        width: drawerWidth,
-        flexShrink: 0,
+        width: theme.spacing(9) + 1,
       },
     },
-    drawerPaper: {
-      width: drawerWidth,
+    toolbar: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'flex-end',
+      padding: theme.spacing(0, 1),
+      // necessary for content to be below app bar
+      ...theme.mixins.toolbar,
     },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+    },
+    drawerPaper: {},
   })
 );
 
@@ -36,11 +91,30 @@ export const SideDrawer: React.SFC<SideDrawerProps> = (props) => {
   const classes = useStyles();
   const theme = useTheme();
   const [mobileOpen, setMobileOpen] = useState(false);
+  const [fullOpen, setFullOpen] = React.useState(false);
 
   const drawer = (
     <div>
       <Toolbar>
-        <Typography variant="h6">Glific</Typography>
+        {fullOpen ? (
+          <>
+            <Typography variant="h6">Glific</Typography>
+            <div className={classes.toolbar}>
+              <IconButton onClick={() => setFullOpen(false)}>
+                {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+              </IconButton>
+            </div>
+          </>
+        ) : (
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={() => setFullOpen(true)}
+            // className={(classes.menuButton, clsx({ [classes.hide]: fullOpen }))}
+          >
+            <MenuIcon />
+          </IconButton>
+        )}
       </Toolbar>
       <Divider />
       <SideMenus />
@@ -51,6 +125,7 @@ export const SideDrawer: React.SFC<SideDrawerProps> = (props) => {
 
   return (
     <nav className={classes.drawer} aria-label="navigation menus">
+      {/* What is the purpose of this drawer? */}
       <Hidden smUp implementation="css">
         <Drawer
           container={container}
@@ -68,13 +143,24 @@ export const SideDrawer: React.SFC<SideDrawerProps> = (props) => {
           {drawer}
         </Drawer>
       </Hidden>
+      {/* Rendered */}
       <Hidden xsDown implementation="css">
         <Drawer
+          // classes={{
+          //   paper: classes.drawerPaper,
+          // }}
+          className={clsx(classes.drawer, {
+            [classes.drawerOpen]: fullOpen,
+            [classes.drawerClose]: !fullOpen,
+          })}
           classes={{
-            paper: classes.drawerPaper,
+            paper: clsx({
+              [classes.drawerOpen]: fullOpen,
+              [classes.drawerClose]: !fullOpen,
+            }),
           }}
           variant="permanent"
-          open
+          // open
         >
           {drawer}
         </Drawer>
