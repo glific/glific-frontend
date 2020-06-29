0aef060e72c397519983f02535ce81fde51342c4
Kurund Jalmi <kurund.jalmi@webaccessglobal.com>
2020-06-30 00:58:46 +0530

    updated mutation and fixed the message save

--- src/graphql/mutations/Chat.ts
+++ src/graphql/mutations/Chat.ts
@@ -1,6 +1,6 @@
 import { gql } from '@apollo/client';
 
-export const CREATE_MESSAGE_MUTATION = gql`
+export const CREATE_AND_SEND_MESSAGE_MUTATION = gql`
 
 mutation createAndSendMessage($input: MessageInput!) {
   createAndSendMessage(input: $input) {
