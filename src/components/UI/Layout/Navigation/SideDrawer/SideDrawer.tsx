9e1bcca085d328baa5ad2b07859f0b80badc6f16
Calvin Chen <chencalvin99@berkeley.edu>
2020-06-30 13:01:45 -0700

    Removing unused files

--- src/components/UI/Layout/Navigation/SideDrawer/SideDrawer.tsx
+++ src/components/UI/Layout/Navigation/SideDrawer/SideDrawer.tsx
@@ -13,8 +13,6 @@ import {
 import clsx from 'clsx';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import SideMenus from '../SideMenus/SideMenus';
 import * as constants from '../../../../../common/constants';
 
