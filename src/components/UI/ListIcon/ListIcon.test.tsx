4560e72aa6cfba57e7e8abb443440d17bac2e0dd
Calvin Chen <chencalvin99@berkeley.edu>
2020-07-01 12:20:16 -0700

    Modifying test

--- src/components/UI/ListIcon/ListIcon.test.tsx
+++ src/components/UI/ListIcon/ListIcon.test.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { ListIcon } from './ListIcon';
 import { shallow, mount } from 'enzyme';
 
-import chatIcon from '../../../assets/images/icons/MenuItems/Chat.svg';
-import tagIcon from '../../../assets/images/icons/MenuItems/Tag.svg';
-import broadcastIcon from '../../../assets/images/icons/MenuItems/Broadcast.svg';
-import automationIcon from '../../../assets/images/icons/MenuItems/Automation.svg';
-import collectionsIcon from '../../../assets/images/icons/MenuItems/Collection.svg';
-import goalsIcon from '../../../assets/images/icons/MenuItems/Goal.svg';
-import analyticsIcon from '../../../assets/images/icons/MenuItems/Analytics.svg';
+import chatIcon from '../../../assets/images/icons/Chat/Unselected.svg';
+import tagIcon from '../../../assets/images/icons/Tags/Unselected.svg';
+import broadcastIcon from '../../../assets/images/icons/Broadcast/Unselected.svg';
+import automationIcon from '../../../assets/images/icons/Automations/Unselected.svg';
+import collectionsIcon from '../../../assets/images/icons/Collections/Unselected.svg';
+import goalsIcon from '../../../assets/images/icons/Goals/Unselected.svg';
+import analyticsIcon from '../../../assets/images/icons/Analytics/Unselected.svg';
 
 describe('list icon tests', () => {
   const iconList: { [iconName: string]: string } = {
@@ -38,7 +38,7 @@ describe('list icon tests', () => {
     // Check without styling
     for (let i = 0; i < keys.length; i++) {
       const wrapper = shallow(createIcon(keys[i], false));
-      expect(wrapper.find('img').props().src).toEqual(capitalize(keys[i]).concat('.svg'));
+      expect(wrapper.find('img').props().src).toEqual('Unselected.svg');
     }
     // Check with styling
     for (let i = 0; i < keys.length; i++) {
