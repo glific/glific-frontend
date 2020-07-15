7cc3057aa76c773df7731e1ee8b630a329aaaa2c
Kurund Jalmi <kurund.jalmi@webaccessglobal.com>
2020-07-15 22:38:13 +0530

    added test case for error handler

---
+++ src/containers/ErrorHandler/ErrorHandler.test.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { cleanup, render } from '@testing-library/react';
+import { MockedProvider } from '@apollo/client/testing';
+
+import { ErrorHandler } from './ErrorHandler';
+import { ERROR_MESSAGE } from '../../graphql/queries/Notification';
+
+const mocks = [
+  {
+    request: {
+      query: ERROR_MESSAGE,
+    },
+    result: {
+      data: {
+        errorMessage: 'An error has occured.',
+      },
+    },
+  },
+];
+
+describe('<ErrorHandler />', () => {
+  afterEach(cleanup);
+
+  test('it should render <ErrorHandler /> component correctly', async () => {
+    const { findByText, getByText } = render(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <ErrorHandler />
+      </MockedProvider>
+    );
+
+    // loading is show initially
+    expect(getByText('Loading...')).toBeInTheDocument();
+  });
+});
