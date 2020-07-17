ecc5f77d24a0c0704db0b6a44366bd447b9f5c68
Calvin Chen <chencalvin99@berkeley.edu>
2020-07-16 18:36:50 -0700

    Importing and polishing changes from old dynamic-search branch

--- src/containers/Chat/ChatConversations/ChatConversations.tsx
+++ src/containers/Chat/ChatConversations/ChatConversations.tsx
@@ -1,141 +1,56 @@
 import React, { useState } from 'react';
 import { Typography, List, Toolbar, Container } from '@material-ui/core';
-import ChatConversation from './ChatConversation/ChatConversation';
 import styles from './ChatConversations.module.css';
-import Loading from '../../../components/UI/Layout/Loading/Loading';
 import { SearchBar } from './SearchBar';
-import { GET_CONVERSATION_QUERY, FILTER_CONVERSATIONS_QUERY } from '../../../graphql/queries/Chat';
-import { useApolloClient, useLazyQuery } from '@apollo/client';
 import selectedChatIcon from '../../../assets/images/icons/Chat/Selected.svg';
+import ConversationList from './ConversationList/ConversationList';
 
-export interface ChatConversationsProps {}
+export interface ChatConversationsProps {
+  contactId: number;
+}
 
-export const ChatConversations: React.SFC<ChatConversationsProps> = () => {
+export const ChatConversations: React.SFC<ChatConversationsProps> = (props) => {
   // get the conversations stored from the cache
-  // create an instance of apolloclient
-  const client = useApolloClient();
   const [searchVal, setSearchVal] = useState('');
-  const [selectedIndex, setSelectedIndex] = useState(0); // This won't work all the time, if the default chat opened is not at index 0. (see ChatMessage for similar behavior).
+  const [selectedContactId, setSelectedContactId] = useState(props.contactId);
 
-  const queryVariables = {
-    contactOpts: {
-      limit: 50,
-    },
-    filter: {},
-    messageOpts: {
-      limit: 100,
-    },
+  const handleChange = (event: any) => {
+    setSearchVal(event.target.value);
   };
 
-  const filterVariables = () => {
-    return {
-      term: searchVal,
-      messageOpts: {
-        limit: 10,
-      },
-      contactOpts: {
-        limit: 10,
-      },
-    };
-  };
-
-  const data: any = client.readQuery({
-    query: GET_CONVERSATION_QUERY,
-    variables: queryVariables,
-  });
-
-  const [queryFilterConvos, { called, loading, error, data: searchData }] = useLazyQuery<any>(
-    FILTER_CONVERSATIONS_QUERY,
-    {
-      variables: filterVariables(),
-    }
-  );
-
-  // Other cases
-  if (loading) return <Loading />;
-  if (error) return <p>Error :(</p>;
-
-  if (data === undefined) {
-    return <p>Error :(</p>;
-  }
-
-  // Retrieving all convos or the ones searched by.
-  let conversations = data.conversations;
-
-  if (searchVal && called) {
-    conversations = searchData.search.filter((n: any) => n.__typename === 'Conversation'); // Trying to only get conversation types from search query.
-  }
-
-  // build the conversation list only if there are conversations
-  let conversationList;
-  if (conversations.length > 0) {
-    conversationList = conversations.map((conversation: any, index: number) => {
-      let lastMeessage = [];
-      if (conversation.messages.length > 0) {
-        lastMeessage = conversation.messages[0];
-      }
-      return (
-        <ChatConversation
-          key={conversation.contact.id}
-          selected={selectedIndex === index}
-          onClick={(i: number) => setSelectedIndex(i)}
-          index={index}
-          contactId={conversation.contact.id}
-          contactName={
-            conversation.contact.name ? conversation.contact.name : conversation.contact.phone
-          }
-          lastMessage={lastMeessage}
-        />
-      );
-    });
-  } else {
-    conversationList = <p data-testid="empty-result">You do not have any conversations.</p>;
-  }
-
-  const handleSearch = (e: any) => {
-    e.preventDefault();
-    let searchVal = e.target.searchInput.value.trim();
-    setSearchVal(searchVal);
-    if (!called) {
-      queryFilterConvos();
-      return <Loading />;
-    }
-    setSelectedIndex(-1);
-  };
+  // const resetSearch = () => {
+  //   setSearchVal('');
+  // };
 
   return (
     <Container className={styles.ChatConversations} disableGutters>
-      {/* Styling toolbar for design */}
-      <Toolbar style={{ padding: '0 24px 0 12px' }}>
+      <Toolbar className={styles.ToolBar}>
         <div className={styles.IconBackground}>
           <img src={selectedChatIcon} height="24" className={styles.Icon} alt="Conversation" />
         </div>
         <div className={styles.Title}>
-          <Typography
-            style={{
-              fontFamily: 'Heebo, Sans-Serif',
-              fontSize: '24px',
-              color: '#073F24',
-              marginTop: '3px',
-            }}
-            variant="h6"
-          >
+          <Typography className={styles.TitleText} variant="h6">
             Chats
           </Typography>
         </div>
       </Toolbar>
       <SearchBar
-        handleSubmit={handleSearch}
+        // handleSubmit={handleSearch}
+        handleChange={handleChange}
         onReset={() => setSearchVal('')}
         searchVal={searchVal}
       />
-      <Container className={styles.ListingContainer} disableGutters>
-        {conversationList ? (
-          <List className={styles.StyledList}>{conversationList}</List>
-        ) : (
-          { conversationList }
-        )}
-      </Container>
+      <ConversationList
+        searchVal={searchVal}
+        selectedContactId={selectedContactId}
+        setSelectedContactId={(i: number) => {
+          setSelectedContactId(i);
+        }}
+        // selectedContact={selectedIndex}
+        // setSelectedIndex={(i: number) => {
+        // setSelectedIndex(i);
+        // }}
+      />
     </Container>
   );
 };
---
+++ src/containers/Chat/ChatConversations/ConversationList/ConversationList.tsx
@@ -0,0 +1,138 @@
+import React, { useCallback, useEffect, useLayoutEffect, useRef } from 'react';
+import { List, Container } from '@material-ui/core';
+import ChatConversation from '../ChatConversation/ChatConversation';
+import styles from './ConversationList.module.css';
+import Loading from '../../../../components/UI/Layout/Loading/Loading';
+import {
+  GET_CONVERSATION_QUERY,
+  FILTER_CONVERSATIONS_QUERY,
+} from '../../../../graphql/queries/Chat';
+import { useApolloClient, useLazyQuery, useQuery } from '@apollo/client';
+
+interface ConversationListProps {
+  searchVal: string;
+  selectedContactId: number;
+  setSelectedContactId: (i: number) => void;
+}
+
+export const ConversationList: React.SFC<ConversationListProps> = (props) => {
+  const client = useApolloClient();
+  const firstUpdate = useRef(true);
+  const queryVariables = {
+    contactOpts: {
+      limit: 50,
+    },
+    filter: {},
+    messageOpts: {
+      limit: 100,
+    },
+  };
+  const filterVariables = () => {
+    return {
+      term: props.searchVal,
+      messageOpts: {
+        limit: 10,
+      },
+      contactOpts: {
+        limit: 10,
+      },
+    };
+  };
+
+  const data: any = client.readQuery({
+    query: GET_CONVERSATION_QUERY,
+    variables: queryVariables,
+  });
+
+  const [
+    getFilterConvos,
+    { called, loading, error, data: searchData, refetch, fetchMore, updateQuery },
+  ] = useLazyQuery<any>(FILTER_CONVERSATIONS_QUERY, {
+    variables: filterVariables(),
+  });
+
+  useLayoutEffect(() => {
+    if (firstUpdate.current) {
+      firstUpdate.current = false;
+      return;
+    }
+    props.setSelectedContactId(-1);
+    // if (refetch) {
+    //   console.log('i found refetch');
+    // }
+    // if (fetchMore) {
+    //   console.log('i found fetchmore');
+    //   fetchMore({
+    //     query: FILTER_CONVERSATIONS_QUERY,
+    //     variables: filterVariables(),
+    //     updateQuery: (prev, { fetchMoreResult }) => {
+    //       return fetchMoreResult;
+    //       // return getNewSearch(prev, fetchMoreResult);
+    //     },
+    //   });
+    // }
+  }, [props.searchVal]);
+
+  // const getNewSearch = useCallback((prevResult: any, newData: any) => {
+  //   console.log(prevResult);
+  //   console.log(newData);
+  //   return newData.search;
+  // }, []);
+
+  // Other cases
+  if (called && loading) return <Loading />;
+  if (called && error) return <p>Error :(</p>;
+
+  if (data === undefined) {
+    return <p>Error :(</p>;
+  }
+
+  // Retrieving all convos or the ones searched by.
+  let conversations = data.conversations;
+
+  if (props.searchVal && !called) {
+    getFilterConvos();
+  }
+
+  if (called && props.searchVal !== '') {
+    conversations = searchData.search.filter((n: any) => n.__typename === 'Conversation'); // Trying to only get conversation types from search query.
+  }
+
+  // build the conversation list only if there are conversations
+  let conversationList;
+  if (conversations.length > 0) {
+    conversationList = conversations.map((conversation: any, index: number) => {
+      let lastMessage = [];
+      if (conversation.messages.length > 0) {
+        lastMessage = conversation.messages[0];
+      }
+      return (
+        <ChatConversation
+          key={conversation.contact.id}
+          selected={props.selectedContactId === conversation.contact.id}
+          onClick={(i: number) => props.setSelectedContactId(conversation.contact.id)}
+          index={index}
+          contactId={conversation.contact.id}
+          contactName={
+            conversation.contact.name ? conversation.contact.name : conversation.contact.phone
+          }
+          lastMessage={lastMessage}
+        />
+      );
+    });
+  } else {
+    conversationList = <p data-testid="empty-result">You do not have any conversations.</p>;
+  }
+
+  return (
+    <Container className={styles.ListingContainer} disableGutters>
+      {conversationList ? (
+        <List className={styles.StyledList}>{conversationList}</List>
+      ) : (
+        { conversationList }
+      )}
+    </Container>
+  );
+};
+
+export default ConversationList;
