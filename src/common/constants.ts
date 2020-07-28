eaa3f44a87fa6f8441bf1c9296b8896c17a812dc
Kurund Jalmi <kurund.jalmi@webaccessglobal.com>
2020-07-28 23:09:56 +0530

    update endpoint

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -4,5 +4,5 @@ export const TIME_FORMAT = 'HH:mm';
 export const REACT_APP_GLIFIC_REGISTRATION_API =
   process.env.REACT_APP_GLIFIC_API + '/v1/registration';
 export const REACT_APP_GLIFIC_AUTHENTICATION_API =
-  process.env.REACT_APP_GLIFIC_API + '/v1/registration/send-registration-otp';
+  process.env.REACT_APP_GLIFIC_API + '/v1/registration/send-otp';
 export const USER_SESSION = process.env.REACT_APP_GLIFIC_API + '/v1/session';
