e2c0565021edd6c7f9b3960c945ceb877fcd80cb
Calvin Chen <chencalvin99@berkeley.edu>
2020-07-28 20:10:52 -0700

    Trying to format emojis and resolve issues

--- src/components/UI/Form/WhatsAppEditor/WhatsAppEditor.tsx
+++ src/components/UI/Form/WhatsAppEditor/WhatsAppEditor.tsx
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
-import { Editor, EditorState, RichUtils, getDefaultKeyBinding } from 'draft-js';
-import 'draft-js/dist/Draft.css';
+import { EditorState, RichUtils, getDefaultKeyBinding } from 'draft-js';
+// import 'draft-js/dist/Draft.css';
 import { convertToWhatsApp } from '../../../../common/RichEditor';
+import Editor from 'draft-js-plugins-editor';
+
+import createEmojiPlugin from 'draft-js-emoji-plugin';
+// import editorStyles from './editorStyles.css'
 
 export interface WhatsAppEditorProps {
   setMessage(message: string): void;
@@ -9,6 +13,9 @@ export interface WhatsAppEditorProps {
 }
 
 export const WhatsAppEditor: React.SFC<WhatsAppEditorProps> = (props) => {
+  const emojiPlugin = createEmojiPlugin();
+  const { EmojiSuggestions, EmojiSelect } = emojiPlugin;
+  const plugins = [emojiPlugin];
   const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
 
   const handleChange = (editorState: any) => {
@@ -16,39 +23,44 @@ export const WhatsAppEditor: React.SFC<WhatsAppEditorProps> = (props) => {
     props.setMessage(convertToWhatsApp(editorState));
   };
 
-  const handleKeyCommand = (command: string, editorState: any) => {
-    // On enter, submit. Otherwise, deal with commands like normal.
-    if (command === 'enter') {
-      // Convert Draft.js to WhatsApp
-      props.sendMessage();
-      const newState = EditorState.createEmpty();
-      setEditorState(EditorState.moveFocusToEnd(newState));
-      return 'handled';
-    }
-    const newState = RichUtils.handleKeyCommand(editorState, command);
-    if (newState) {
-      setEditorState(newState);
-      return 'handled';
-    }
-    return 'not-handled';
-  };
+  // const handleKeyCommand = (command: string, editorState: any) => {
+  //   // On enter, submit. Otherwise, deal with commands like normal.
+  //   if (command === 'enter') {
+  //     // Convert Draft.js to WhatsApp
+  //     props.sendMessage();
+  //     const newState = EditorState.createEmpty();
+  //     setEditorState(EditorState.moveFocusToEnd(newState));
+  //     return 'handled';
+  //   }
+  //   const newState = RichUtils.handleKeyCommand(editorState, command);
+  //   if (newState) {
+  //     setEditorState(newState);
+  //     return 'handled';
+  //   }
+  //   return 'not-handled';
+  // };
 
-  const keyBindingFn = (e: any) => {
-    // Shift-enter is by default supported. Only 'enter' needs to be changed.
-    if (e.keyCode === 13 && !e.nativeEvent.shiftKey) {
-      return 'enter';
-    }
-    return getDefaultKeyBinding(e);
-  };
+  // const keyBindingFn = (e: any) => {
+  //   // Shift-enter is by default supported. Only 'enter' needs to be changed.
+  //   if (e.keyCode === 13 && !e.nativeEvent.shiftKey) {
+  //     return 'enter';
+  //   }
+  //   return getDefaultKeyBinding(e);
+  // };
 
   return (
-    <Editor
-      editorState={editorState}
-      placeholder="Start typing..."
-      onChange={handleChange}
-      handleKeyCommand={handleKeyCommand}
-      keyBindingFn={keyBindingFn}
-    />
+    // <Editor
+    //   editorState={editorState}
+    //   placeholder="Start typing..."
+    //   onChange={handleChange}
+    //   handleKeyCommand={handleKeyCommand}
+    //   keyBindingFn={keyBindingFn}
+    // />
+    <>
+      <Editor editorState={editorState} onChange={handleChange} plugins={plugins} />
+      <EmojiSuggestions />
+      <div>{/* <EmojiSelect /> */}</div>
+    </>
   );
 };
 
--- src/containers/Chat/ChatMessages/ChatInput/ChatInput.tsx
+++ src/containers/Chat/ChatMessages/ChatInput/ChatInput.tsx
@@ -3,24 +3,41 @@ import IconButton from '@material-ui/core/IconButton';
 import { Container, Button } from '@material-ui/core';
 import { Picker } from 'emoji-mart';
 import 'emoji-mart/css/emoji-mart.css';
-import WhatsAppEditor from '../../../../components/UI/Form/WhatsAppEditor/WhatsAppEditor';
-
+// import WhatsAppEditor from '../../../../components/UI/Form/WhatsAppEditor/WhatsAppEditor';
+import Editor from 'draft-js-plugins-editor';
+import createEmojiPlugin from 'draft-js-emoji-plugin';
+import defaultTheme from 'draft-js-emoji-plugin';
+// import editorStyles from './editorStyles.css';
+import { EditorState, ContentState, RichUtils, getDefaultKeyBinding } from 'draft-js';
 import styles from './ChatInput.module.css';
-
 import sendMessageIcon from '../../../../assets/images/icons/SendMessage.svg';
+import 'draft-js-emoji-plugin/lib/plugin.css';
+import { convertToWhatsApp } from '../../../../common/RichEditor';
+import { StyledEmojiSelectWrapper, GlobalStyleForEmojiSelect } from './draftJsPluginBaseStyles';
 
 export interface ChatInputProps {
   onSendMessage(content: string): any;
 }
 
+// defaultTheme.emojiSelectPopover = defaultTheme.emojiSelectPopover + ' hi-there';
+
+const emojiTheme = {
+  emojiSelectPopover: styles.EmojiSelector,
+};
+
+const emojiPlugin = createEmojiPlugin({ useNativeArt: true }); // , theme: emojiTheme
+const { EmojiSuggestions, EmojiSelect } = emojiPlugin;
+const plugins = [emojiPlugin];
+
 export const ChatInput: React.SFC<ChatInputProps> = ({ onSendMessage }) => {
   const [message, setMessage] = useState('');
   const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+  const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
 
   const submitMessage = () => {
     // close emoji picker
     setShowEmojiPicker(false);
-
+    console.log(message);
     if (!message) return;
     setMessage('');
 
@@ -42,17 +59,68 @@ export const ChatInput: React.SFC<ChatInputProps> = ({ onSendMessage }) => {
     );
   }
 
+  const handleChange = (editorState: any) => {
+    setEditorState(editorState);
+    setMessage(convertToWhatsApp(editorState));
+  };
+
+  const handleKeyCommand = (command: string, editorState: any) => {
+    // On enter, submit. Otherwise, deal with commands like normal.
+    if (command === 'enter') {
+      // Convert Draft.js to WhatsApp
+      submitMessage();
+      const newState = EditorState.createEmpty();
+      // setEditorState(newState);
+      // setEditorState(EditorState.moveFocusToEnd(newState));
+      setEditorState(
+        EditorState.moveFocusToEnd(
+          EditorState.push(editorState, ContentState.createFromText(''), 'remove-range')
+        )
+      );
+      return 'handled';
+    }
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+    if (newState) {
+      setEditorState(newState);
+      return 'handled';
+    }
+    return 'not-handled';
+  };
+
+  const keyBindingFn = (e: any) => {
+    // Shift-enter is by default supported. Only 'enter' needs to be changed.
+    if (e.keyCode === 13 && !e.nativeEvent.shiftKey) {
+      return 'enter';
+    }
+    return getDefaultKeyBinding(e);
+  };
+
   return (
     <Container className={styles.ChatInput}>
       <div className={styles.ChatInputElements}>
         <div className={styles.InputContainer}>
-          <WhatsAppEditor
+          {/* <WhatsAppEditor
             data-testid="message-input"
             setMessage={(message: string) => setMessage(message)} // Primarily for message length
             sendMessage={() => submitMessage()}
-          />
+          /> */}
+          <div className={styles.Editor}>
+            <Editor
+              editorState={editorState}
+              onChange={handleChange}
+              plugins={plugins}
+              handleKeyCommand={handleKeyCommand}
+              keyBindingFn={keyBindingFn}
+            />
+            {/* <EmojiSuggestions /> */}
+          </div>
+          {/* <StyledEmojiSelectWrapper>
+            <GlobalStyleForEmojiSelect />
+            <EmojiSelect />
+          </StyledEmojiSelectWrapper> */}
+          {/* </div> */}
         </div>
-        <div className={styles.EmojiContainer}>
+        {/* <div className={styles.EmojiContainer}>
           <IconButton
             data-testid="emoji-picker"
             color="primary"
@@ -64,19 +132,22 @@ export const ChatInput: React.SFC<ChatInputProps> = ({ onSendMessage }) => {
               😀
             </span>
           </IconButton>
-        </div>
-        <div className={styles.SendButtonContainer}>
-          <Button
-            className={styles.SendButton}
-            data-testid="send-button"
-            variant="contained"
-            color="primary"
-            onClick={submitMessage}
-            disabled={message.length === 0}
-          >
-            Send
-            <img className={styles.SendIcon} src={sendMessageIcon} alt="Send Message" />
-          </Button>
+        </div> */}
+        <div className={styles.Buttons}>
+          <EmojiSelect />
+          <div className={styles.SendButtonContainer}>
+            <Button
+              className={styles.SendButton}
+              data-testid="send-button"
+              variant="contained"
+              color="primary"
+              onClick={submitMessage}
+              disabled={message.length === 0}
+            >
+              Send
+              <img className={styles.SendIcon} src={sendMessageIcon} alt="Send Message" />
+            </Button>
+          </div>
         </div>
       </div>
       {emojiPicker}
