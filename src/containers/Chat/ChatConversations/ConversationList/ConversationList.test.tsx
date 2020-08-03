32ad44dce73c56ab99f52e5f884727666b7e524c
Vaibhav Rathore <vaibhav@coloredcow.com>
2020-08-03 20:26:34 +0530

    minor fixes

--- src/containers/Chat/ChatConversations/ConversationList/ConversationList.test.tsx
+++ src/containers/Chat/ChatConversations/ConversationList/ConversationList.test.tsx
@@ -3,11 +3,11 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { render, wait, fireEvent } from '@testing-library/react';
 import ConversationList from './ConversationList';
 import { MockedProvider } from '@apollo/client/testing';
-import { ChatConversationMocks, conversations } from './../ChatConversations.test.helper';
+import { ChatConversationMocks } from './../ChatConversations.test.helper';
 
 const mocks = ChatConversationMocks;
 
-const conversationList = (conversation: any) => (
+const conversationList = (
   <MockedProvider mocks={mocks} addTypename={false}>
     <Router>
       <ConversationList
@@ -15,24 +15,19 @@ const conversationList = (conversation: any) => (
         selectedContactId={2}
         setSelectedContactId={jest.fn()}
         savedSearchCriteria=""
-        conversations={conversation}
       />
     </Router>
   </MockedProvider>
 );
 
 test('it should render ConversationsList properly', () => {
-  const { container } = render(conversationList(conversations));
+  const { container } = render(conversationList);
   expect(container).toBeInTheDocument();
 });
 
-test('it shows error if conversations are undefined', () => {
-  const { getByText } = render(conversationList(undefined));
-  expect(getByText('Error :(')).toBeInTheDocument();
-});
-
-test('it shows a conversation on clicking a contact', () => {
-  const { getAllByTestId, getByText } = render(conversationList(conversations));
+test('it shows a conversation on clicking a contact', async () => {
+  const { getAllByTestId, getByText } = render(conversationList);
+  await wait();
   fireEvent.click(getAllByTestId('list')[0]);
-  expect(getByText('Hi')).toBeInTheDocument();
+  expect(getByText('Hello')).toBeInTheDocument();
 });
