34580b3850dfc68e4470fc403036a254f0972c00
Kurund Jalmi <kurund.jalmi@webaccessglobal.com>
2020-08-03 22:00:12 +0530

    added tests for staff management

---
+++ src/containers/StaffManagement/StaffManagement.test.helper.tsx
@@ -0,0 +1,53 @@
+import { GET_USERS_QUERY, FILTER_USERS, USER_COUNT } from '../../graphql/queries/StaffManagement';
+
+export const STAFF_MANAGEMENT_MOCKS = [
+  {
+    request: {
+      query: GET_USERS_QUERY,
+      variables: { id: 1 },
+    },
+    result: {
+      data: {
+        user: {
+          user: {
+            groups: [],
+            id: '1',
+            name: 'Glific Admin',
+            phone: '919900991223',
+            roles: ['admin'],
+          },
+        },
+      },
+    },
+  },
+  {
+    request: {
+      query: FILTER_USERS,
+      variables: { filter: { name: '' }, opts: { limit: 10, offset: 0, order: 'ASC' } },
+    },
+    result: {
+      data: {
+        users: [
+          {
+            groups: [],
+            id: '1',
+            name: 'Glific Admin',
+            phone: '919900991223',
+            roles: ['admin'],
+          },
+        ],
+      },
+    },
+  },
+  {
+    request: {
+      query: USER_COUNT,
+      variables: { filter: { name: '' } },
+    },
+    result: {
+      data: {
+        countUsers: 1,
+      },
+    },
+  },
+];
