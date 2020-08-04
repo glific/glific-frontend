48c374a3937fd66a6cf0642443962e0e282b55db
Kurund Jalmi <kurund.jalmi@webaccessglobal.com>
2020-08-04 19:38:04 +0530

    test clean up

--- src/containers/Template/Form/HSM/HSM.test.tsx
+++ src/containers/Template/Form/HSM/HSM.test.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import { render, wait } from '@testing-library/react';
-import { BrowserRouter as Router } from 'react-router-dom';
 import { MockedProvider } from '@apollo/client/testing';
 
 import { HSM } from './HSM';
-import { Switch, Route } from 'react-router-dom';
-import { HSMList } from '../../List/HSMList/HSMList';
 import { TEMPLATE_MOCKS } from '../../Template.test.helper';
 
 const mocks = TEMPLATE_MOCKS;
@@ -13,12 +10,7 @@ const mocks = TEMPLATE_MOCKS;
 test('HSM form is loaded correctly in edit mode', async () => {
   const { getByText } = render(
     <MockedProvider mocks={mocks} addTypename={false}>
-      <Router>
-        <Switch>
-          <Route path="/template" exact component={HSMList} />
-        </Switch>
-        <HSM match={{ params: { id: 1 } }} />
-      </Router>
+      <HSM match={{ params: { id: 1 } }} />
     </MockedProvider>
   );
   await wait();
