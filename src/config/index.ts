fbc1c27b7ab7d004d1633f37be0006b21adcd5ff
satendra-sr <officialsatendrarawat@gmail.com>
2020-08-24 20:36:16 +0530

    Env variable fallbacks

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 const envVariables = process.env;
 
-export const URI = envVariables.REACT_APP_GLIFIC_API;
-export const SOCKET = envVariables.REACT_APP_WEB_SOCKET;
+export const URI = envVariables.REACT_APP_GLIFIC_API  ? envVariables.REACT_APP_GLIFIC_API : 'https://glificuat.coloredcow.com:8443/api' ;
+export const SOCKET = envVariables && envVariables.REACT_APP_WEB_SOCKET ? envVariables.REACT_APP_WEB_SOCKET : 'ws://glificuat.coloredcow.com:8443/socket';
 export const SENTRY_DSN = envVariables.SENTRY_DSN;
-export const FLOW_EDITOR_API = envVariables.REACT_APP_FLOW_EDITOR_API;
+export const FLOW_EDITOR_API = envVariables && envVariables.REACT_APP_FLOW_EDITOR_API ? envVariables.REACT_APP_FLOW_EDITOR_API : 'https://glificuat.coloredcow.com:8443/flow-editor/';
