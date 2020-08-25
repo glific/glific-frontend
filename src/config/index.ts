31a4a3fc85c2e1e271b50f520790aa7ae3705bd7
satendra-sr <officialsatendrarawat@gmail.com>
2020-08-25 23:16:31 +0530

    updated URLs

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 const envVariables = process.env;
 
-export const URI = envVariables.REACT_APP_GLIFIC_API  ? envVariables.REACT_APP_GLIFIC_API : 'https://glificuat.coloredcow.com:8443/api' ;
-export const SOCKET = envVariables && envVariables.REACT_APP_WEB_SOCKET ? envVariables.REACT_APP_WEB_SOCKET : 'ws://glificuat.coloredcow.com:4000/socket';
+export const URI = envVariables.REACT_APP_GLIFIC_API  ? envVariables.REACT_APP_GLIFIC_API : 'https://www.glificuat.coloredcow.com:8443/api' ;
+export const SOCKET = envVariables && envVariables.REACT_APP_WEB_SOCKET ? envVariables.REACT_APP_WEB_SOCKET : 'ws://www.glificuat.coloredcow.com:4000/socket';
 export const SENTRY_DSN = envVariables.SENTRY_DSN;
-export const FLOW_EDITOR_API = envVariables && envVariables.REACT_APP_FLOW_EDITOR_API ? envVariables.REACT_APP_FLOW_EDITOR_API : 'https://glificuat.coloredcow.com:8443/flow-editor/';
+export const FLOW_EDITOR_API = envVariables && envVariables.REACT_APP_FLOW_EDITOR_API ? envVariables.REACT_APP_FLOW_EDITOR_API : 'https://www.glificuat.coloredcow.com:8443/flow-editor/';
