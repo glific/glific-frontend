0f20a93b267ca7262234002db80af1014b0a1f9b
satendra-sr <officialsatendrarawat@gmail.com>
2020-08-26 18:25:35 +0530

    api url updates

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,8 +1,8 @@
 export const SIDE_DRAWER_WIDTH = 233;
 export const DATE_FORMAT = 'DD/MM/YY';
 export const TIME_FORMAT = 'HH:mm';
-export const REACT_APP_GLIFIC_REGISTRATION_API = process.env.REACT_APP_GLIFIC_API ? process.env.REACT_APP_GLIFIC_API + '/v1/registration' : 'https://www.glificuat.coloredcow.com:8443/api/v1/registration';
+export const REACT_APP_GLIFIC_REGISTRATION_API = process.env.REACT_APP_GLIFIC_API ? process.env.REACT_APP_GLIFIC_API + '/v1/registration' : 'https://api.glificuat.coloredcow.com/api/v1/registration';
 export const REACT_APP_GLIFIC_AUTHENTICATION_API =
-  process.env.REACT_APP_GLIFIC_API ?  process.env.REACT_APP_GLIFIC_API + '/v1/registration/send-otp': 'https://www.glificuat.coloredcow.com:8443/api/v1/registration/send-otp';
-export const USER_SESSION = process.env.REACT_APP_GLIFIC_API ? process.env.REACT_APP_GLIFIC_API + '/v1/session' : 'https://www.glificuat.coloredcow.com:8443/api/v1/session';
-export const RESET_PASSWORD = process.env.REACT_APP_GLIFIC_API ? process.env.REACT_APP_GLIFIC_API + '/v1/registration/reset-password' : 'https://www.glificuat.coloredcow.com:8443/api/v1/registration/reset-password';
+  process.env.REACT_APP_GLIFIC_API ?  process.env.REACT_APP_GLIFIC_API + '/v1/registration/send-otp': 'https://api.glificuat.coloredcow.com/api/v1/registration/send-otp';
+export const USER_SESSION = process.env.REACT_APP_GLIFIC_API ? process.env.REACT_APP_GLIFIC_API + '/v1/session' : 'https://api.glificuat.coloredcow.com/api/v1/session';
+export const RESET_PASSWORD = process.env.REACT_APP_GLIFIC_API ? process.env.REACT_APP_GLIFIC_API + '/v1/registration/reset-password' : 'https://api.glificuat.coloredcow.com/api/v1/registration/reset-password';
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 const envVariables = process.env;
 
-export const URI = envVariables.REACT_APP_GLIFIC_API  ? envVariables.REACT_APP_GLIFIC_API : 'https://www.glificuat.coloredcow.com:8443/api' ;
-export const SOCKET = envVariables && envVariables.REACT_APP_WEB_SOCKET ? envVariables.REACT_APP_WEB_SOCKET : 'wss://www.glificuat.coloredcow.com:8443/socket';
+export const URI = envVariables.REACT_APP_GLIFIC_API  ? envVariables.REACT_APP_GLIFIC_API : 'https://api.glificuat.coloredcow.com/api' ;
+export const SOCKET = envVariables && envVariables.REACT_APP_WEB_SOCKET ? envVariables.REACT_APP_WEB_SOCKET : 'wss://api.glificuat.coloredcow.com/socket';
 export const SENTRY_DSN = envVariables.SENTRY_DSN;
-export const FLOW_EDITOR_API = envVariables && envVariables.REACT_APP_FLOW_EDITOR_API ? envVariables.REACT_APP_FLOW_EDITOR_API : 'https://www.glificuat.coloredcow.com:8443/flow-editor/';
+export const FLOW_EDITOR_API = envVariables && envVariables.REACT_APP_FLOW_EDITOR_API ? envVariables.REACT_APP_FLOW_EDITOR_API : 'https://api.glificuat.coloredcow.com/flow-editor/';
