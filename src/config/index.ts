cb4ee5c021e19469a9b09ec9e5749118e8505ae8
satendra-sr <officialsatendrarawat@gmail.com>
2020-08-28 01:50:29 +0530

    Docker file updated for env variables

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 const envVariables = process.env;
 
-export const URI = envVariables.REACT_APP_GLIFIC_API  ? envVariables.REACT_APP_GLIFIC_API : 'https://api.glificuat.coloredcow.com/api' ;
-export const SOCKET = envVariables && envVariables.REACT_APP_WEB_SOCKET ? envVariables.REACT_APP_WEB_SOCKET : 'wss://api.glificuat.coloredcow.com/socket';
+export const URI = envVariables.REACT_APP_GLIFIC_API;
+export const SOCKET = envVariables.REACT_APP_WEB_SOCKET;
 export const SENTRY_DSN = envVariables.SENTRY_DSN;
-export const FLOW_EDITOR_API = envVariables && envVariables.REACT_APP_FLOW_EDITOR_API ? envVariables.REACT_APP_FLOW_EDITOR_API : 'https://api.glificuat.coloredcow.com/flow-editor/';
+export const FLOW_EDITOR_API = envVariables.REACT_APP_FLOW_EDITOR_API;
