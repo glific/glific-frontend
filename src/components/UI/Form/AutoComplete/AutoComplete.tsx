f3732f4c72e02d82a626bc9776d4968a80aa4533
Kurund Jalmi <kurund.jalmi@webaccessglobal.com>
2020-08-28 23:37:19 +0530

    readd checkboxes in autocomplete

--- src/components/UI/Form/AutoComplete/AutoComplete.tsx
+++ src/components/UI/Form/AutoComplete/AutoComplete.tsx
@@ -65,6 +65,12 @@ export const AutoComplete: React.SFC<AutocompleteProps> = ({
               />
             ))
           }
+          renderOption={(option, { selected }) => (
+            <React.Fragment>
+              <Checkbox icon={icon} checked={selected} />
+              {option[optionLabel]}
+            </React.Fragment>
+          )}
           renderInput={(params) => (
             <TextField
               {...params}
