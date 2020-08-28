87e4876b6a937cf9ab6e5be1ed1d044e32fbec8b
satendra-sr <officialsatendrarawat@gmail.com>
2020-08-28 12:07:55 +0530

    reverted workaround changes

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,9 +1,10 @@
 export const SIDE_DRAWER_WIDTH = 233;
 export const DATE_FORMAT = 'DD/MM/YY';
 export const TIME_FORMAT = 'HH:mm';
-export const REACT_APP_GLIFIC_REGISTRATION_API = process.env.REACT_APP_GLIFIC_API + '/v1/registration';
+export const REACT_APP_GLIFIC_REGISTRATION_API =
+  process.env.REACT_APP_GLIFIC_API + '/v1/registration';
 export const REACT_APP_GLIFIC_AUTHENTICATION_API =
-   process.env.REACT_APP_GLIFIC_API + '/v1/registration/send-otp';
+  process.env.REACT_APP_GLIFIC_API + '/v1/registration/send-otp';
 export const USER_SESSION = process.env.REACT_APP_GLIFIC_API + '/v1/session';
 export const RESET_PASSWORD = process.env.REACT_APP_GLIFIC_API + '/v1/registration/reset-password';
 export const RENEW_TOKEN = USER_SESSION + '/renew';
