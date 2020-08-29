56d4d54d8842f11c7c04d5ec7f94966ac63475bc
Kurund Jalmi <kurund.jalmi@webaccessglobal.com>
2020-08-29 16:39:22 +0530

    missed one file

--- src/containers/StaffManagement/StaffManagment.test.tsx
+++ src/containers/StaffManagement/StaffManagment.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, wait } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { MockedProvider } from '@apollo/client/testing';
 
 import { StaffManagement } from './StaffManagement';
