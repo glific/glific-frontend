f66d07a0d4aa8268f40f7a3018f5eb86c33b3eae
Dignesh Gujarathi <dignesh.gujarathi@webaccessglobal.com>
2020-09-02 19:40:27 +0530

    Updated filter to ES6

--- src/containers/Tag/Tag.tsx
+++ src/containers/Tag/Tag.tsx
@@ -57,9 +57,7 @@ export const Tag: React.SFC<TagProps> = ({ match }) => {
   let tags = [];
   // remove the self tag from list
   if (data && match && match.params.id) {
-    tags = data.tags.filter(function (tag: any) {
-      return tag.id !== match.params.id;
-    });
+    tags = data.tags.filter((tag: any) => tag.id !== match.params.id);
   }
 
   const getColorCode = (code: string) => {
