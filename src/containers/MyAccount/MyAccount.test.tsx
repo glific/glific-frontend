f84940ef468140d870898b605d4cb0972ca85fa3
Kurund Jalmi <kurund.jalmi@webaccessglobal.com>
2020-09-05 17:46:49 +0530

    test fixes

--- src/containers/MyAccount/MyAccount.test.tsx
+++ src/containers/MyAccount/MyAccount.test.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import MyAccount from './MyAccount';
+import { render } from '@testing-library/react';
+import { MockedProvider } from '@apollo/client/testing';
+
+import { MyAccount } from './MyAccount';
+import { MY_ACCOUNT_MOCKS } from './MyAccount.test.helper';
+
+const mocks = MY_ACCOUNT_MOCKS;
 
 describe('<MyAccount />', () => {
-  test('it should mount', () => {
-    render(<MyAccount />);
-    
-    const myAccount = screen.getByTestId('MyAccount');
+  test('it should mount', async () => {
+    const { getByText, findByTestId } = render(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <MyAccount />
+      </MockedProvider>
+    );
 
-    expect(myAccount).toBeInTheDocument();
+    // loading is show initially
+    expect(getByText('Loading...')).toBeInTheDocument();
+
+    const myAccount = await findByTestId('MyAccount');
+    expect(myAccount).toHaveTextContent('Change Password');
   });
 });
