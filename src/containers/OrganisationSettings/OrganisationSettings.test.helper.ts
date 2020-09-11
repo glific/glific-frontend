69319ed48e90f9eb4cc3d5271a83bf1f262e0a87
Dignesh Gujarathi <dignesh.gujarathi@webaccessglobal.com>
2020-09-11 10:34:41 +0530

    Updated test cases

--- src/containers/OrganisationSettings/OrganisationSettings.test.helper.ts
+++ src/containers/OrganisationSettings/OrganisationSettings.test.helper.ts
@@ -1,5 +1,5 @@
 import { GET_AUTOMATIONS } from '../../graphql/queries/Automation';
-import { GET_ORGANIZATION, GET_ORGANIZATIONS } from '../../graphql/queries/Organization';
+import { GET_ORGANIZATION } from '../../graphql/queries/Organization';
 import { GET_LANGUAGES } from '../../graphql/queries/List';
 
 export const LIST_ITEM_MOCKS = [
@@ -31,17 +31,13 @@ export const LIST_ITEM_MOCKS = [
       data: {
         flows: [
           {
-            __typename: 'Flow',
             id: '1',
             name: 'Help Workflow',
-            shortcode: 'help',
             uuid: '3fa22108-f464-41e5-81d9-d8a298854429',
           },
           {
-            __typename: 'Flow',
             id: '10',
             name: 'SoL Feedback',
-            shortcode: 'solfeedback',
             uuid: '6c21af89-d7de-49ac-9848-c9febbf737a5',
           },
         ],
@@ -83,6 +79,7 @@ export const LIST_ITEM_MOCKS = [
             providerKey: 'ADD_PROVIDER_API_KEY',
             providerPhone: '917834811114',
             shortcode: 'Glific',
+            name: 'Glific',
           },
         },
       },
@@ -123,6 +120,7 @@ export const LIST_ITEM_MOCKS = [
             providerKey: 'ADD_PROVIDER_API_KEY',
             providerPhone: '917834811114',
             shortcode: 'Glific',
+            name: 'Glific',
           },
         },
       },
