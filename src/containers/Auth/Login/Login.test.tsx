ae73e4323b5b5751d8f6c737b6b71448087035af
Kurund Jalmi <kurund.jalmi@webaccessglobal.com>
2020-09-16 00:40:32 +0530

    login is 90%

--- src/containers/Auth/Login/Login.test.tsx
+++ src/containers/Auth/Login/Login.test.tsx
@@ -3,15 +3,21 @@ import { render, screen } from '@testing-library/react';
 import UserEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
 import axios from 'axios';
+import { MockedProvider } from '@apollo/client/testing';
 
 import { Login } from './Login';
+import { MY_ACCOUNT_MOCKS } from '../../MyAccount/MyAccount.test.helper';
+
+const mocks = MY_ACCOUNT_MOCKS;
 
 jest.mock('axios');
 
 const wrapper = (
-  <MemoryRouter>
-    <Login />
-  </MemoryRouter>
+  <MockedProvider mocks={mocks}>
+    <MemoryRouter>
+      <Login />
+    </MemoryRouter>
+  </MockedProvider>
 );
 
 describe('<Login />', () => {
@@ -26,10 +32,36 @@ describe('<Login />', () => {
   });
 
   it('test the login form submission with correct creds', async () => {
-    render(wrapper);
+    const { container } = render(wrapper);
+    const phone = screen.getByRole('textbox');
+    UserEvent.type(phone, '+919978776554');
+
+    const password = container.querySelector('input[type="password"]');
+    UserEvent.type(password, 'pass123456');
+
+    // click on login
+    const loginButton = screen.getByText('LOGIN');
+    UserEvent.click(loginButton);
+
+    // let's mock successful registration submission
+    const responseData = { data: { data: { data: {} } } };
+    axios.post.mockImplementationOnce(() => Promise.resolve(responseData));
   });
 
   it('test the login form submission with incorrect creds', async () => {
-    render(wrapper);
+    const { container } = render(wrapper);
+    const phone = screen.getByRole('textbox');
+    UserEvent.type(phone, '+919978776554');
+
+    const password = container.querySelector('input[type="password"]');
+    UserEvent.type(password, 'pass123456');
+
+    // click on login
+    const loginButton = screen.getByText('LOGIN');
+    UserEvent.click(loginButton);
+
+    // set the mock error case while login
+    const errorMessage = 'Cannot login';
+    axios.post.mockImplementationOnce(() => Promise.reject(new Error(errorMessage)));
   });
 });
