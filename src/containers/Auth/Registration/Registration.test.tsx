79b708bdca2b4c8febc7e77a7b42d25c73fa12e9
Kurund Jalmi <kurund.jalmi@webaccessglobal.com>
2020-09-16 00:26:21 +0530

    registration is 100%

--- src/containers/Auth/Registration/Registration.test.tsx
+++ src/containers/Auth/Registration/Registration.test.tsx
@@ -27,17 +27,38 @@ describe('<Registration />', () => {
   });
 
   test('it should submit the form correctly', async () => {
-    render(wrapper);
+    const { container } = render(wrapper);
     const inputElements = screen.getAllByRole('textbox');
     UserEvent.type(inputElements[0], 'JaneDoe');
     UserEvent.type(inputElements[1], '+919978776554');
 
-    // let's mock successful registration submission
-    const responseData = { data: { data: { data: {} } } };
-    axios.post.mockImplementationOnce(() => Promise.resolve(responseData));
+    const password = container.querySelector('input[type="password"]');
+    UserEvent.type(password, 'pass123456');
 
     // click on continue
     const continueButton = screen.getByText('CONTINUE');
     UserEvent.click(continueButton);
+
+    // let's mock successful registration submission
+    const responseData = { data: { data: { data: {} } } };
+    axios.post.mockImplementationOnce(() => Promise.resolve(responseData));
+  });
+
+  test('it should submit the form correctly and give error', async () => {
+    const { container } = render(wrapper);
+    const inputElements = screen.getAllByRole('textbox');
+    UserEvent.type(inputElements[0], 'JaneDoe');
+    UserEvent.type(inputElements[1], '+919978776554');
+
+    const password = container.querySelector('input[type="password"]');
+    UserEvent.type(password, 'pass123456');
+
+    // click on continue
+    const continueButton = screen.getByText('CONTINUE');
+    UserEvent.click(continueButton);
+
+    // set the mock error case while registration
+    const errorMessage = 'Cannot register 919978776554';
+    axios.post.mockImplementationOnce(() => Promise.reject(new Error(errorMessage)));
   });
 });
