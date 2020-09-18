58c23ef49f15b84afe97a24c48a3acb071b6e85d
Dignesh Gujarathi <dignesh.gujarathi@webaccessglobal.com>
2020-09-18 20:04:45 +0530

    Updated OrganisationSettings

--- src/containers/OrganisationSettings/OrganisationSettings.test.tsx
+++ src/containers/OrganisationSettings/OrganisationSettings.test.tsx
@@ -1,35 +1,69 @@
 import React from 'react';
-import { render, wait } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import { shallow } from 'enzyme';
+import { render, screen, fireEvent, wait, findByText } from '@testing-library/react';
 import { OrganisationSettings } from './OrganisationSettings';
 import { MockedProvider } from '@apollo/client/testing';
 import { LIST_ITEM_MOCKS } from './OrganisationSettings.test.helper';
 
 const mocks = LIST_ITEM_MOCKS;
 
-const wrapper = shallow(
+const wrapper = (
   <MockedProvider mocks={mocks} addTypename={false}>
     <OrganisationSettings match={{ params: { id: 1 } }} />
   </MockedProvider>
 );
 
 describe('<OrganisationSettings />', () => {
-  it('should render OrganisationSettings', () => {
-    expect(wrapper.exists()).toBe(true);
+  it('renders component properly', async () => {
+    const { findByTestId } = render(wrapper);
+    const container = await findByTestId('formLayout');
+    expect(container).toHaveTextContent('Organisation name');
   });
 });
 
-test('should load the OrganisationSettings edit', async () => {
-  const { getByText } = render(
-    <MockedProvider mocks={mocks} addTypename={false}>
-      <OrganisationSettings match={{ params: { id: 1 } }} />
-    </MockedProvider>
-  );
+describe('should load the <OrganisationSettings />', () => {
+  test('should load the OrganisationSettings edit', async () => {
+    const { getByText } = render(wrapper);
 
-  // loading is show initially
-  expect(getByText('Loading...')).toBeInTheDocument();
+    // loading is show initially
+    expect(getByText('Loading...')).toBeInTheDocument();
 
-  await wait();
-  expect(getByText('Settings')).toBeInTheDocument();
+    await wait();
+    expect(getByText('Settings')).toBeInTheDocument();
+  });
+});
+
+describe('Check form fields <OrganisationSettings />', () => {
+  test('Check label of fields', async () => {
+    const { getByText, findByTestId } = render(wrapper);
+    // loading is show initially
+    expect(getByText('Loading...')).toBeInTheDocument();
+
+    await wait();
+    const container = await findByTestId('formLayout');
+    expect(container).toHaveTextContent('Organisation name');
+    expect(container).toHaveTextContent('Gupshup API key');
+    expect(container).toHaveTextContent('Gupshup WhatsApp number');
+    expect(container).toHaveTextContent('Active language(s)');
+    expect(container).toHaveTextContent('Default language');
+    expect(container).toHaveTextContent('Hours of operations');
+    expect(container).toHaveTextContent('Opens');
+    expect(container).toHaveTextContent('Closes');
+    expect(container).toHaveTextContent('Select days');
+    expect(container).toHaveTextContent('Select default flow');
+  });
+});
+
+describe('Checked Hours of operations', () => {
+  test('Checked Hours of operations', async () => {
+    const { getByText, findByTestId } = render(wrapper);
+    // loading is show initially
+    expect(getByText('Loading...')).toBeInTheDocument();
+
+    await wait();
+    const container = await findByTestId('formLayout');
+    await wait();
+    const checkbox = screen.getByRole('checkbox');
+    fireEvent.change(checkbox, { target: { value: 'true' } });
+    expect(checkbox.value).toBe('true');
+  });
 });
