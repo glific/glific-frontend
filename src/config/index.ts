3d5a5f6a06f412139726f016319a97f7caf755ae
Vaibhav Rathore <vaibhav@coloredcow.com>
2020-09-24 21:44:07 +0530

    websocket link updated

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,6 +7,6 @@ const HOSTNAME = window.location.hostname;
 const GLIFIC_API_URL = API_PORT ? `${PROTOCOL}//${HOSTNAME}:${API_PORT}/${API_PREFIX}` : `${PROTOCOL}//${HOSTNAME}/${API_PREFIX}`;
 
 export const URI = GLIFIC_API_URL;
-export const SOCKET = API_PORT ? `ws://${HOSTNAME}:${API_PORT}` : `ws://${HOSTNAME}`;
+export const SOCKET = API_PORT ? `ws://${HOSTNAME}:${API_PORT}/socket` : `ws://${HOSTNAME}/socket`;
 export const SENTRY_DSN = envVariables.SENTRY_DSN;
 export const FLOW_EDITOR_API = GLIFIC_API_URL + '/flow-editor/';
