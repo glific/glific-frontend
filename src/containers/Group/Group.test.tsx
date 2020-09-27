68a90513e420bfd091a351a8a1078fdeec3293e7
Kurund Jalmi <kurund.jalmi@webaccessglobal.com>
2020-09-27 20:41:02 +0530

    more clean up

--- src/containers/Group/Group.test.tsx
+++ src/containers/Group/Group.test.tsx
@@ -4,11 +4,12 @@ import UserEvent from '@testing-library/user-event';
 import { MockedProvider } from '@apollo/client/testing';
 
 import { Group } from './Group';
-import { getGroupsQuery } from '../../mocks/Group';
+import { getGroupQuery, getGroupUsers } from '../../mocks/Group';
 import { getUsersQuery } from '../../mocks/User';
+import { getOrganizationQuery } from '../../mocks/Organization';
 
-const mocks = [getGroupsQuery, getUsersQuery];
-
+const mocks = [...getGroupQuery, getUsersQuery, ...getOrganizationQuery, ...getGroupUsers];
+console.log(mocks);
 const wrapper = (
   <MockedProvider mocks={mocks}>
     <Group match={{ params: { id: 1 } }} />
@@ -23,7 +24,6 @@ describe('<Group />', () => {
     expect(getByText('Loading...')).toBeInTheDocument();
 
     await wait();
-    screen.debug();
-    expect(getByText('Groups')).toBeInTheDocument();
+    expect(getByText('Add a new group')).toBeInTheDocument();
   });
 });
