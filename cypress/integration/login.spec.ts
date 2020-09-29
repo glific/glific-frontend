a9c07dc957209f97b74464f16ceac89f15a901af
Kurund Jalmi <kurund.jalmi@webaccessglobal.com>
2020-09-30 00:50:59 +0530

    let's add login test

--- cypress/integration/login.spec.ts
+++ cypress/integration/login.spec.ts
@@ -1,5 +1,18 @@
-describe('Login', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(true);
+describe('Login page', () => {
+  it('Load the login page', () => {
+    cy.visit('/');
+    cy.get('h4').should('contain', 'Login to your account');
+  });
+
+  it('Check validations', () => {
+    cy.contains('LOGIN').click();
+    cy.get('p').should('contain', 'Input required');
+  });
+
+  it('Successful login', () => {
+    cy.get('input[type=tel]').type('8450938576');
+    cy.get('input[type=password]').type('secret1234');
+    cy.contains('LOGIN').click();
+    cy.get('h6').should('contain', 'Chats');
   });
 });
