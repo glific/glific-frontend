543934cbceb422565d61da914025554357dafb3a
Kurund Jalmi <kurund.jalmi@webaccessglobal.com>
2020-09-29 18:12:00 +0530

    logout test fixes

--- src/containers/Auth/Logout/Logout.test.tsx
+++ src/containers/Auth/Logout/Logout.test.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
 
 import { Logout } from './Logout';
 
 describe('<Logout />', () => {
-  test('it should mount', () => {
-    render(<Logout />);
+  test('it should render', () => {
+    render(
+      <MemoryRouter>
+        <Logout />
+      </MemoryRouter>
+    );
   });
 });
