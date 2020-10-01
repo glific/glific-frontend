40e215c78f0462205a9f94ed772f612e8d60f052
Kurund Jalmi <kurund.jalmi@webaccessglobal.com>
2020-10-01 20:23:54 +0530

    test cleanup

--- src/components/UI/Timer/Timer.test.tsx
+++ src/components/UI/Timer/Timer.test.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { render, screen, wait } from '@testing-library/react';
+
 import { Timer } from './Timer';
 
 const timer = (time: Date) => <Timer time={time}></Timer>;
 
 test('it should render timer component', () => {
-  const wrapper = mount(timer(new Date()));
-  expect(wrapper.exists()).toBe(true);
+  render(timer(new Date()));
+  expect(screen.getByTestId('timerCount')).toBeInTheDocument();
 });
 
-test('it should display the remaining time', () => {
+test('it should display the remaining time', async () => {
   const date = new Date();
   date.setHours(date.getHours() - 2);
-  const wrapper = mount(timer(date));
-  expect(wrapper.find('[data-testid="timerCount"]').text()).toBe('22');
+
+  const { findByTestId } = render(timer(date));
+
+  await wait();
+  const timerElement = await findByTestId('timerCount');
+  screen.debug();
+  expect(timerElement).toHaveTextContent('22');
 });
