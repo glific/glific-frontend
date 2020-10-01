6fbae47f70a91c6a845b3a3b7db0ca038a38b1f8
Vaibhav Rathore <vaibhav@coloredcow.com>
2020-10-01 19:14:36 +0530

    simulator ui done

--- src/components/simulator/Simulator.tsx
+++ src/components/simulator/Simulator.tsx
@@ -1,16 +1,23 @@
 import React, { useState, useEffect, useRef } from 'react';
 import styles from './Simulator.module.css';
 import { ReactComponent as SimulatorIcon } from '../../assets/images/icons/Simulator.svg';
-import SendIcon from '@material-ui/icons/Send';
+import AttachFileIcon from '@material-ui/icons/AttachFile';
 import { Button } from '@material-ui/core';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import DefaultWhatsappImage from '../../assets/images/whatsappDefault.jpg';
 import { useQuery } from '@apollo/client';
-
+import DoneAllIcon from '@material-ui/icons/DoneAll';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import MicIcon from '@material-ui/icons/Mic';
+import CallIcon from '@material-ui/icons/Call';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import VideocamIcon from '@material-ui/icons/Videocam';
 import CameraAltIcon from '@material-ui/icons/CameraAlt';
 import Axios from 'axios';
 import { SEARCH_QUERY } from '../../graphql/queries/Search';
 import { SEARCH_QUERY_VARIABLES } from '../../common/constants';
+import moment from 'moment';
+import { TIME_FORMAT } from '../../common/constants';
 
 export const Simulator: React.FC = (props) => {
   const [showSimulator, setShowSimulator] = useState(false);
@@ -36,7 +43,12 @@ export const Simulator: React.FC = (props) => {
     return direction === 'send' ? styles.SendMessage : styles.ReceivedMessage;
   };
 
-  const renderMessage = (text: string, direction: string, index: number): JSX.Element => {
+  const renderMessage = (
+    text: string,
+    direction: string,
+    index: number,
+    insertedAt: string
+  ): JSX.Element => {
     return (
       <div className={getStyleForDirection(direction)} key={index}>
         {text
@@ -48,6 +60,9 @@ export const Simulator: React.FC = (props) => {
               );
             })
           : null}
+
+        <span>{moment(insertedAt).format(TIME_FORMAT)}</span>
+        {direction === 'received' ? <DoneAllIcon /> : null}
       </div>
     );
   };
@@ -55,9 +70,9 @@ export const Simulator: React.FC = (props) => {
   const simulatedMessages = messages
     .map((simulatorMessage: any, index: number) => {
       if (simulatorMessage.receiver.id === '2') {
-        return renderMessage(simulatorMessage.body, 'send', index);
+        return renderMessage(simulatorMessage.body, 'send', index, simulatorMessage.insertedAt);
       } else {
-        return renderMessage(simulatorMessage.body, 'received', index);
+        return renderMessage(simulatorMessage.body, 'received', index, simulatorMessage.insertedAt);
       }
     })
     .reverse();
@@ -105,14 +120,19 @@ export const Simulator: React.FC = (props) => {
             <div className={styles.Header}>
               <ArrowBackIcon />
               <img src={DefaultWhatsappImage} alt="default Image" />
-              Simulated Beneficiary
+              <span>Beneficiary</span>
+              <div>
+                <VideocamIcon />
+                <CallIcon />
+                <MoreVertIcon />
+              </div>
             </div>
             <div className={styles.Messages} ref={messageRef}>
               {simulatedMessages}
-              <div id="bottom" style={{ float: 'left', clear: 'both', marginTop: 20 }} />
             </div>
             <div className={styles.Controls}>
               <div>
+                <InsertEmoticonIcon className={styles.Icon} />
                 <input
                   type="text"
                   onKeyPress={(event: any) => {
@@ -124,7 +144,8 @@ export const Simulator: React.FC = (props) => {
                   placeholder={'Type a message'}
                   onChange={(event) => setInputMessage(event.target.value)}
                 />
-                <CameraAltIcon />
+                <AttachFileIcon className={styles.AttachFileIcon} />
+                <CameraAltIcon className={styles.Icon} />
               </div>
 
               <Button
@@ -133,18 +154,8 @@ export const Simulator: React.FC = (props) => {
                 className={styles.SendButton}
                 onClick={() => sendMessage()}
               >
-                <SendIcon />
+                <MicIcon />
               </Button>
-            </div>
-
-            <div className={styles.Footer}>
-              <div className={styles.show_context_button}>
-                <div className="context-button">
-                  <span className="fe-x"></span>
-                </div>
-              </div>
-
-              <span className={styles.Reset + ' ' + (true ? styles.active : styles.inactive)} />
             </div>
           </div>
         </div>
