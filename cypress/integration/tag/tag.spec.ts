03496212df58b227cb0a794b375486f9112cd468
Kurund Jalmi <kurund.jalmi@webaccessglobal.com>
2020-10-03 00:45:47 +0530

    added tag tests

--- cypress/integration/tag/tag.spec.ts
+++ cypress/integration/tag/tag.spec.ts
@@ -1,10 +1,25 @@
 describe('Tag', () => {
+  const tagName = 'Random Tag ' + Math.random();
+
   beforeEach(function () {
     // login before each test
     cy.login();
-  });
-
-  it('Load Tag listing', () => {
     cy.visit('/tag');
   });
+
+  it('should load tag list', () => {
+    cy.get('h5').should('contain', 'Tags');
+  });
+
+  it('should create new tag', () => {
+    cy.contains('CREATE TAG').click();
+    cy.get('input[name=label]').type(tagName);
+    cy.get('textarea:first').type('This is random tag description');
+    cy.contains('Save').click();
+    cy.get('div').should('contain', 'Tag created successfully');
+    //cy.get('div').should('contain', tagName);
+  });
+
+  it('should edit tag', () => {});
+  it('should delete tag', () => {});
 });
