1768e2887acf4db1253ba1022072bcbf2da7e7f6
Kurund Jalmi <kurund.jalmi@webaccessglobal.com>
2020-10-04 01:12:30 +0530

    speed send add

--- cypress/integration/template/SpeedSend.spec.ts
+++ cypress/integration/template/SpeedSend.spec.ts
@@ -1,4 +1,6 @@
 describe('Speed Send', () => {
+  const speedSendName = 'Random Speed Send ' + Math.random();
+
   beforeEach(function () {
     // login before each test
     cy.login();
@@ -9,7 +11,14 @@ describe('Speed Send', () => {
     cy.get('h5').should('contain', 'Speed sends');
   });
 
-  it('should create new speed send', () => {});
+  it('should create new speed send', () => {
+    cy.contains('CREATE SPEED SEND').click();
+    cy.get('input[name=label]').type(speedSendName);
+    cy.get('[contenteditable]').click();
+    cy.get('[contenteditable]').type('Test speed send message');
+    cy.contains('Save').click();
+    cy.get('div').should('contain', 'Speed send created successfully');
+  });
 
   it('should edit speed send', () => {});
 
