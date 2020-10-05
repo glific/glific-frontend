e5dd6cd4fc8ad4f35c72034c287434a16da6eeea
Vaibhav Rathore <vaibhav@coloredcow.com>
2020-10-05 22:17:46 +0530

    added host prefix

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,16 +3,17 @@ const envVariables = process.env;
 const API_PORT = envVariables.REACT_APP_GLIFIC_API_PORT;
 const PROTOCOL = window.location.protocol;
 const HOSTNAME = window.location.hostname;
-const GLIFIC_BACKEND_URL = API_PORT
+const HOST_PREFIX = envVariables.REACT_APP_HOST_PREFIX;
+let GLIFIC_BACKEND_URL = API_PORT
   ? `${PROTOCOL}//${HOSTNAME}:${API_PORT}`
   : `${PROTOCOL}//${HOSTNAME}`;
+
+GLIFIC_BACKEND_URL = HOST_PREFIX ? `${HOST_PREFIX}.${GLIFIC_BACKEND_URL}` : GLIFIC_BACKEND_URL;
 
 export const SOCKET = API_PORT ? `ws://${HOSTNAME}:${API_PORT}/socket` : `ws://${HOSTNAME}/socket`;
 export const SENTRY_DSN = envVariables.SENTRY_DSN;
 export const FLOW_EDITOR_API = GLIFIC_BACKEND_URL + '/flow-editor/';
-export const GLIFIC_API_URL = API_PORT
-  ? `${PROTOCOL}//${HOSTNAME}:${API_PORT}/api`
-  : `${PROTOCOL}//${HOSTNAME}/api`;
+export const GLIFIC_API_URL = GLIFIC_BACKEND_URL + '/api';
 export const REACT_APP_GLIFIC_REGISTRATION_API = GLIFIC_API_URL + '/v1/registration';
 export const REACT_APP_GLIFIC_AUTHENTICATION_API = GLIFIC_API_URL + '/v1/registration/send-otp';
 export const USER_SESSION = GLIFIC_API_URL + '/v1/session';
