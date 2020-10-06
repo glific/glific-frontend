fb32ab20c64ac53449b11a3c0f79593a474b7599
Dignesh Gujarathi <dignesh.gujarathi@webaccessglobal.com>
2020-10-06 12:06:52 +0530

    Updated test cases

--- src/containers/SettingList/Organisation/Organisation.test.tsx
+++ src/containers/SettingList/Organisation/Organisation.test.tsx
@@ -26,3 +26,44 @@ describe('<Organisation />', () => {
     expect(getByText('Back to settings')).toBeInTheDocument();
   });
 });
+
+describe('<Organisation />', () => {
+  it('SAVE component properly', async () => {
+    const { getByText } = render(wrapper);
+    // loading is show initially
+    expect(getByText('Loading...')).toBeInTheDocument();
+    await wait();
+    // click on SAVE
+    const saveButton = screen.getByText('Save');
+    UserEvent.click(saveButton);
+    await wait();
+    expect(getByText('Settings edited successfully!')).toBeInTheDocument();
+  });
+});
+
+describe('<Organisation />', () => {
+  it('Click on Cancel', async () => {
+    const { getByText } = render(wrapper);
+    // loading is show initially
+    expect(getByText('Loading...')).toBeInTheDocument();
+    await wait();
+    // click on Cancel
+    const Button = screen.getByText('Cancel');
+    expect(Button).toBeInTheDocument();
+    UserEvent.click(Button);
+  });
+});
+
+describe('Checked Hours of operations', () => {
+  test('Checked Hours of operations', async () => {
+    const { getByText, findByTestId } = render(wrapper);
+    // loading is show initially
+    expect(getByText('Loading...')).toBeInTheDocument();
+
+    await wait();
+    await wait();
+    const checkbox = screen.getByRole('checkbox');
+    fireEvent.change(checkbox, { target: { value: 'true' } });
+    expect(checkbox.value).toBe('true');
+  });
+});
