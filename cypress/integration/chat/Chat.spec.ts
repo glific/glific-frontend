b1724e7403ae84b70b991a5fc9eceeb3e9a62cf0
Kurund Jalmi <kurund.jalmi@webaccessglobal.com>
2020-10-07 00:15:36 +0530

    chat tests

--- cypress/integration/chat/Chat.spec.ts
+++ cypress/integration/chat/Chat.spec.ts
@@ -1,13 +1,28 @@
 describe('Chats', () => {
+  const messageText = 'Sample Message for testing ' + +new Date();
+
   beforeEach(function () {
     // login before each test
     cy.login();
     cy.visit('/chat');
   });
-  it('should load the correct message', () => {});
-  it('should send the correct message', () => {});
-  it('should tag the message correctly', () => {});
-  it('should remove message tag correctly', () => {});
-  it('should send the speed send', () => {});
-  it('should send add to speed send', () => {});
+  it('should load the correct message', () => {
+    cy.contains('Default receiver').click();
+    cy.get('div').should('contain', 'Default message body');
+  });
+
+  it('should send the message correctly', () => {
+    cy.get('.DraftEditor-editorContainer').click({ force: true });
+    cy.get('.DraftEditor-editorContainer').type(messageText);
+    cy.contains('Send').click();
+    cy.get('div').should('contain', messageText);
+  });
+
+  // it('should tag the message correctly', () => {});
+
+  // it('should remove message tag correctly', () => {});
+
+  // it('should send the speed send', () => {});
+
+  // it('should send add to speed send', () => {});
 });
