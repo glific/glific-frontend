e64436001164c81ce986bfe566f7982f52274717
Kurund Jalmi <kurund.jalmi@webaccessglobal.com>
2020-10-15 22:17:55 +0530

    let's try sending params to backend

--- src/config/absinthe.ts
+++ src/config/absinthe.ts
@@ -1,8 +1,11 @@
 import { SOCKET } from '.';
+import { getAuthSession } from '../services/AuthService';
 const AbsintheSocket = require('@absinthe/socket');
 const SocketApolloLink = require('@absinthe/socket-apollo-link');
 const Socket = require('phoenix');
 
+// get auth token
+const accessToken = getAuthSession('access_token');
 export default SocketApolloLink.createAbsintheSocketLink(
-  AbsintheSocket.create(new Socket.Socket(SOCKET))
+  AbsintheSocket.create(new Socket.Socket(SOCKET, { params: { token: accessToken } }))
 );
