12f78cd266b8335218f7521e84659323049ac29e
Kurund Jalmi <kurund.jalmi@webaccessglobal.com>
2020-10-16 15:04:58 +0530

    let try finite reconnection attempts

--- src/config/absinthe.ts
+++ src/config/absinthe.ts
@@ -6,6 +6,14 @@ const Socket = require('phoenix');
 
 // get auth token
 const accessToken = getAuthSession('access_token');
-export default SocketApolloLink.createAbsintheSocketLink(
-  AbsintheSocket.create(new Socket.Socket(SOCKET, { params: { token: accessToken } }))
-);
+
+// token is used in the backend to identify the user and potentially the organization associated with the same.
+// this is more like an authentication header, which WS does not support. Hence, it is passed as params.
+// create socket connection
+const socketConnection = new Socket.Socket(SOCKET, { params: { token: accessToken } });
+// let's re-try connection only few times
+socketConnection.reconnectAfterMs = (tries: any) => {
+  return [10, 50, 100, 150, 200, 250, 500, 1000, 2000][tries - 1] || 5000;
+};
+
+export default SocketApolloLink.createAbsintheSocketLink(AbsintheSocket.create(socketConnection));
