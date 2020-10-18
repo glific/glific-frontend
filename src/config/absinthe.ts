ce4d3d7a92b364118d405cf69d340f92870ffdde
Kurund Jalmi <kurund.jalmi@webaccessglobal.com>
2020-10-18 23:01:10 +0530

    token implementation clean up

--- src/config/absinthe.ts
+++ src/config/absinthe.ts
@@ -4,12 +4,19 @@ const AbsintheSocket = require('@absinthe/socket');
 const SocketApolloLink = require('@absinthe/socket-apollo-link');
 const PhoenixSocket = require('phoenix');
 
-// get auth token
-const accessToken = getAuthSession('access_token');
-
 // token is used in the backend to identify the user and potentially the organization associated with the same.
 // this is more like an authentication header, which WS does not support. Hence, it is passed as params.
-// create socket connection
-const socketConnection = new PhoenixSocket.Socket(SOCKET, { params: { token: accessToken } });
+// note that params are called as a function because we want to get the user token after authentication.
+// create Phoenix socket connection
+const socketConnection = new PhoenixSocket.Socket(SOCKET, {
+  params: () => {
+    if (getAuthSession('access_token')) {
+      return { token: getAuthSession('access_token') };
+    } else {
+      return {};
+    }
+  },
+});
 
+// wrap the Phoenix socket in an AbsintheSocket and export
 export default SocketApolloLink.createAbsintheSocketLink(AbsintheSocket.create(socketConnection));
