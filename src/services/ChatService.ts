eb4f116c4d2aa5f1717eb09f62585dbbcb9a5674
Kurund Jalmi <kurund.jalmi@webaccessglobal.com>
2020-10-19 17:25:38 +0530

    fresh start to conversation restructuring attempt #5(may be)

---
+++ src/services/ChatService.ts
@@ -0,0 +1,11 @@
+// This service includes all the actions related to conversations storing
+
+// write conversation to cache
+export const saveConverations = () => {
+
+}
+
+// read the conversation from cache
+export const getConverations = () => {
+
+}
