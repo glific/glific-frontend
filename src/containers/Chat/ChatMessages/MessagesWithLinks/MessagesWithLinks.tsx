badd694bafab46ffcfd1ac26963c8e28f32660e5
Vaibhav Rathore <vaibhav@coloredcow.com>
2020-11-21 13:15:31 +0530

    sync with master

--- src/containers/Chat/ChatMessages/MessagesWithLinks/MessagesWithLinks.tsx
+++ src/containers/Chat/ChatMessages/MessagesWithLinks/MessagesWithLinks.tsx
@@ -14,8 +14,7 @@ export const MessagesWithLinks: React.FC<MessagesWithLinksProps> = (
 ) => {
   const { message } = props;
   let linkPreview = null;
-  let messagebody:any = WhatsAppToJsx(props.message);
-  let array;
+  let messagebody: any = WhatsAppToJsx(message);
   const regexForLink = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/gi;
 
   // first element is the url, if the url is sent
