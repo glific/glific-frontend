680bd5b3608a9b2bb2a3fc0d27eeb87026019ddc
Kurund Jalmi <kurund.jalmi@webaccessglobal.com>
2020-11-25 23:09:07 +0530

    minor fix

--- src/containers/Auth/ResetPassword/ResetPasswordConfirmOTP.test.tsx
+++ src/containers/Auth/ResetPassword/ResetPasswordConfirmOTP.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, wait, waitFor } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import UserEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router';
 import axios from 'axios';
