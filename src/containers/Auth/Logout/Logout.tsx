a16c07d6fd1621e4301be4b398567302f10de0cd
Kurund Jalmi <kurund.jalmi@webaccessglobal.com>
2020-11-29 22:10:30 +0530

    api fixes, code restructuring

--- src/containers/Auth/Logout/Logout.tsx
+++ src/containers/Auth/Logout/Logout.tsx
@@ -5,7 +5,7 @@ import { useApolloClient } from '@apollo/client';
 
 import { resetRole } from '../../../context/role';
 import { SessionContext } from '../../../context/session';
-import { clearAuthSession, clearUserSession } from '../../../services/AuthService';
+import { clearAuthSession, clearUserSession, getAuthSession } from '../../../services/AuthService';
 import { USER_SESSION } from '../../../config';
 
 export interface LogoutProps {}
@@ -16,27 +16,29 @@ export const Logout: React.SFC<LogoutProps> = () => {
 
   // let's notify the backend when user logs out
   const userLogout = () => {
-    axios.delete(USER_SESSION, {});
+    // get the auth token from session
+    axios.defaults.headers.common.Authorization = getAuthSession('access_token');
+    axios.delete(USER_SESSION, {}).then(() => {
+      // clear local storage auth session
+      clearAuthSession();
+
+      // update the context
+      setAuthenticated(false);
+
+      // clear local storage user session
+      clearUserSession();
+
+      // clear role & access permissions
+      resetRole();
+
+      // clear apollo cache
+      client.clearStore();
+    });
   };
 
   useEffect(() => {
     userLogout();
   }, []);
-
-  // clear local storage auth session
-  clearAuthSession();
-
-  // update the context
-  setAuthenticated(false);
-
-  // clear local storage user session
-  clearUserSession();
-
-  // clear role & access permissions
-  resetRole();
-
-  // clear apollo cache
-  client.clearStore();
 
   return <Redirect to="/login" />;
 };
