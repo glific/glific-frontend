b4ef8e38fef3542dcd5dd348a8b416b47322d3e6
Kurund Jalmi <kurund.jalmi@webaccessglobal.com>
2020-11-29 22:01:49 +0530

    let's clear backend tokens after logout

--- src/containers/Auth/Logout/Logout.tsx
+++ src/containers/Auth/Logout/Logout.tsx
@@ -1,16 +1,27 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
+import axios from 'axios';
 import { Redirect } from 'react-router-dom';
 import { useApolloClient } from '@apollo/client';
 
 import { resetRole } from '../../../context/role';
 import { SessionContext } from '../../../context/session';
 import { clearAuthSession, clearUserSession } from '../../../services/AuthService';
+import { USER_SESSION } from '../../../config';
 
 export interface LogoutProps {}
 
 export const Logout: React.SFC<LogoutProps> = () => {
   const { setAuthenticated } = useContext(SessionContext);
   const client = useApolloClient();
+
+  // let's notify the backend when user logs out
+  const userLogout = () => {
+    axios.delete(USER_SESSION, {});
+  };
+
+  useEffect(() => {
+    userLogout();
+  }, []);
 
   // clear local storage auth session
   clearAuthSession();
