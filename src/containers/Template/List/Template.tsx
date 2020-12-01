5748d3e1ffa32a10d770053b532517d0d9928ec0
Kurund Jalmi <kurund.jalmi@webaccessglobal.com>
2020-12-02 00:05:02 +0530

    css fixes for template

--- src/containers/Template/List/Template.tsx
+++ src/containers/Template/List/Template.tsx
@@ -7,14 +7,14 @@ import { setVariables } from '../../../common/constants';
 import { GET_TEMPLATES_COUNT, FILTER_TEMPLATES } from '../../../graphql/queries/Template';
 import { DELETE_TEMPLATE } from '../../../graphql/mutations/Template';
 
-const columnNames = ['LABEL', 'BODY', 'ACTIONS', 'LAST MODIFIED'];
-const columnStyles = [styles.Label, styles.Body, styles.Actions];
+const columnNames = ['LABEL', 'BODY', 'LAST MODIFIED', 'ACTIONS'];
+const columnStyles = [styles.Label, styles.Body, styles.LastModified, styles.Actions];
 
 const getLabel = (label: string) => <div className={styles.LabelText}>{label}</div>;
 
 const getBody = (text: string) => <p className={styles.TableText}>{WhatsAppToJsx(text)}</p>;
 
-const getUpdatedAt = (date: string) => <div className={styles.LabelText}>{date}</div>;
+const getUpdatedAt = (date: string) => <div className={styles.LastModified}>{date}</div>;
 
 const getColumns = ({ label, body, updatedAt }: any) => ({
   label: getLabel(label),
