0c35b43dfbff0228a5724811a93b3ce0abbf416c
mdshamoon <mohdshamoon841@gmail.com>
2021-01-21 18:13:14 +0530

    appsignal issues

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,12 @@ import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider, CssBaseline } from '@material-ui/core';
 import Appsignal from '@appsignal/javascript';
 import { ErrorBoundary } from '@appsignal/react';
+import WindowEvents from `@appsignal/plugin-window-events`;
+import BreadcrumbsNetwork from '@appsignal/plugin-breadcrumbs-network';
+import PathDecorator from `@appsignal/plugin-path-decorator`;
 
 import theme from './config/theme';
-import { APPSIGNAL_API_KEY } from './config';
+import { APPSIGNAL_API_KEY, APPSINAL_REVISION } from './config';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -15,7 +18,9 @@ let appComponent = <App />;
 if (APPSIGNAL_API_KEY) {
   const appsignal = new Appsignal({
     key: APPSIGNAL_API_KEY,
+    revision: APPSINAL_REVISION,
   });
+  appsignal.use(BreadcrumbsNetwork.plugin());
 
   appComponent = (
     <ErrorBoundary instance={appsignal}>
