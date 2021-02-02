1e272e3b02a300594b88da13a8645dfb5e1b7e3e
DigneshGujarathi <dignesh.gujarathi@webaccessglobal.com>
2021-02-02 19:30:36 +0530

    Removed duplicate data

--- src/containers/Chat/Chat.test.tsx
+++ src/containers/Chat/Chat.test.tsx
@@ -2,61 +2,13 @@ import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { cleanup, render } from '@testing-library/react';
 import { Chat } from './Chat';
-import { CONVERSATION_MOCKS } from '../../mocks/Chat';
 import { setUserSession } from '../../services/AuthService';
 
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-import { SEARCH_QUERY } from '../../graphql/queries/Search';
+import { searchQuery } from './ChatMessages/ChatMessages.test';
 
 const cache = new InMemoryCache({ addTypename: false });
-cache.writeQuery({
-  query: SEARCH_QUERY,
-  variables: {
-    filter: {},
-    messageOpts: { limit: 50 },
-    contactOpts: { limit: 50 },
-  },
-  data: {
-    search: [
-      {
-        group: null,
-        contact: {
-          id: '2',
-          name: 'Jane Doe',
-          phone: '987654321',
-          maskedPhone: '98****321',
-          lastMessageAt: '2020-06-29T09:31:47Z',
-          status: 'VALID',
-          bspStatus: 'SESSION_AND_HSM',
-        },
-        messages: [
-          {
-            id: '1',
-            body: 'Hey there whats up?',
-            insertedAt: '2020-06-25T13:36:43Z',
-            location: null,
-            receiver: {
-              id: '1',
-            },
-            sender: {
-              id: '2',
-            },
-            tags: [
-              {
-                id: '1',
-                label: 'important',
-                colorCode: '#00d084',
-              },
-            ],
-            type: 'TEXT',
-            media: null,
-            errors: null,
-          },
-        ],
-      },
-    ],
-  },
-});
+cache.writeQuery(searchQuery);
 
 const client = new ApolloClient({
   cache: cache,
@@ -90,7 +42,7 @@ describe('<Chat />', () => {
     expect(getByText('Loading...')).toBeInTheDocument();
     // check if chat conversations are displayed
     const ChatConversation = await findByTestId('beneficiaryName');
-    expect(ChatConversation).toHaveTextContent('Jane Doe');
+    expect(ChatConversation).toHaveTextContent('Effie Cormier');
 
     // check if tags are displayed in the ChatMessages
     const ConversationTag = await findAllByText('important');
@@ -101,6 +53,6 @@ describe('<Chat />', () => {
     const { findByTestId } = render(wrapper);
 
     const ChatConversation = await findByTestId('beneficiaryName');
-    expect(ChatConversation).toHaveTextContent('Jane Doe');
+    expect(ChatConversation).toHaveTextContent('Effie Cormier');
   });
 });
