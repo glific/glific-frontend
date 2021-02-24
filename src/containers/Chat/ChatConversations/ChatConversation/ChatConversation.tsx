ed60bdbd3a026638f72a14742812f8df3f921f2b
mdshamoon <mohdshamoon841@gmail.com>
2021-02-24 16:05:40 +0530

    check if there is a message

--- src/containers/Chat/ChatConversations/ChatConversation/ChatConversation.tsx
+++ src/containers/Chat/ChatConversations/ChatConversation/ChatConversation.tsx
@@ -46,13 +46,16 @@ const updateMessageCache = (client: any, id: any) => {
     fragment: MESSAGE_FRAGMENT,
   });
 
-  const messageCopy = JSON.parse(JSON.stringify(message));
-  messageCopy.isRead = true;
-  client.writeFragment({
-    id: `Message:${id}`,
-    fragment: MESSAGE_FRAGMENT,
-    data: messageCopy,
-  });
+  if (message) {
+    const messageCopy = JSON.parse(JSON.stringify(message));
+
+    messageCopy.isRead = true;
+    client.writeFragment({
+      id: `Message:${id}`,
+      fragment: MESSAGE_FRAGMENT,
+      data: messageCopy,
+    });
+  }
 
   return null;
 };
@@ -76,8 +79,10 @@ const ChatConversation: React.SFC<ChatConversationProps> = (props) => {
   } = props;
 
   const [markAsRead] = useMutation(MARK_AS_READ, {
-    onCompleted: () => {
-      updateMessageCache(client, lastMessage.id);
+    onCompleted: (data) => {
+      if (data.markContactMessagesAsRead) {
+        updateMessageCache(client, lastMessage.id);
+      }
     },
   });
   // there might be some cases when there are no conversations againist the contact. So need to handle that
