1a9768dcd261103ff7d9c2074b0c1cca0baa8ab9
mdshamoon <mohdshamoon841@gmail.com>
2021-03-01 16:31:43 +0530

    ci update

--- src/config/index.ts
+++ src/config/index.ts
@@ -34,5 +34,4 @@ export const APP_NAME = appName || 'Glific: Two way communication platform';
 export const LOGFLARE_API = envVariables.REACT_APP_LOGFLARE_API_KEY;
 export const LOGFLARE_SOURCE = envVariables.REACT_APP_LOGFLARE_SOURCE_TOKEN;
 export const GLIFIC_DOCS_URL = 'http://docs.glific.org';
-
 export const CORS_PROXY_URL = 'https://cors-anywhere.tides.coloredcow.com';
