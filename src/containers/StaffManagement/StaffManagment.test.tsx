41cc05f83c6594c6925db820c0c540276e7c95e7
DigneshGujarathi <dignesh.gujarathi@webaccessglobal.com>
2021-03-03 12:51:33 +0530

    code cleanup

--- src/containers/StaffManagement/StaffManagment.test.tsx
+++ src/containers/StaffManagement/StaffManagment.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, waitForElement, fireEvent } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { MockedProvider } from '@apollo/client/testing';
 
 import { StaffManagement } from './StaffManagement';
