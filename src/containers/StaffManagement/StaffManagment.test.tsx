e06e8b691abf41d78c58d2d23281007fb2abf257
DigneshGujarathi <dignesh.gujarathi@webaccessglobal.com>
2021-03-03 12:26:16 +0530

    added test case for help

--- src/containers/StaffManagement/StaffManagment.test.tsx
+++ src/containers/StaffManagement/StaffManagment.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, waitForElement, fireEvent } from '@testing-library/react';
 import { MockedProvider } from '@apollo/client/testing';
 
 import { StaffManagement } from './StaffManagement';
@@ -7,16 +7,33 @@ import { STAFF_MANAGEMENT_MOCKS } from './StaffManagement.test.helper';
 
 const mocks = STAFF_MANAGEMENT_MOCKS;
 
+const staffManagement = (
+  <MockedProvider mocks={mocks} addTypename={false}>
+    <StaffManagement match={{ params: { id: 1 } }} />
+  </MockedProvider>
+);
+
 test('should load the staff user edit form', async () => {
-  const { getByText, findByTestId } = render(
-    <MockedProvider mocks={mocks} addTypename={false}>
-      <StaffManagement match={{ params: { id: 1 } }} />
-    </MockedProvider>
-  );
+  const { getByText, findByTestId } = render(staffManagement);
 
   // loading is show initially
   expect(getByText('Loading...')).toBeInTheDocument();
 
   const formLayout = await findByTestId('formLayout');
   expect(formLayout).toHaveTextContent('Username');
+});
+
+test('should display help popup', async () => {
+  const { getByText } = render(staffManagement);
+
+  // loading is show initially
+  expect(getByText('Loading...')).toBeInTheDocument();
+
+  await waitForElement(() => getByText('help?'));
+  const help = getByText('help?');
+  expect(help).toBeInTheDocument();
+  fireEvent.click(help);
+
+  const header = getByText('User roles');
+  expect(header).toBeInTheDocument();
 });
