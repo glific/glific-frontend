10be73a02edbab4284f598ec21deb97d539d1ed7
DigneshGujarathi <dignesh.gujarathi@webaccessglobal.com>
2021-03-04 14:06:08 +0530

    minor fix

--- src/containers/List/List.tsx
+++ src/containers/List/List.tsx
@@ -561,7 +561,7 @@ export const List: React.SFC<ListProps> = ({
       {backLink}
       {/* Rendering list of items */}
 
-      {itemList.length > 0 ? displayList : <div>There are no {listItemName}s.</div>}
+      {itemList ? displayList : <div>There are no {listItemName}s.</div>}
     </>
   );
 };
