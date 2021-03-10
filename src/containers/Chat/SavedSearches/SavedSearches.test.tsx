169c6a65d8ccb91e9ce0a12c2ba4b6c6d32be91c
DigneshGujarathi <dignesh.gujarathi@webaccessglobal.com>
2021-03-10 18:15:45 +0530

    Updated test

--- src/containers/Chat/SavedSearches/SavedSearches.test.tsx
+++ src/containers/Chat/SavedSearches/SavedSearches.test.tsx
@@ -2,10 +2,25 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import SavedSearches from './SavedSearches';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+import { searchQuery } from '../ChatMessages/ChatMessages.test';
+
+const cache = new InMemoryCache({ addTypename: false });
+cache.writeQuery(searchQuery);
+
+const client = new ApolloClient({
+  cache,
+  assumeImmutableResults: true,
+});
 
 describe('<SavedSearches />', () => {
   test('it should mount', () => {
-    render(<SavedSearches />);
+    render(
+      <ApolloProvider client={client}>
+        <SavedSearches />
+      </ApolloProvider>
+    );
 
     const savedSearches = screen.getByTestId('SavedSearches');
 
