9c760832b28e5e4e08902ec623fc081077ba9f7c
mdshamoon <mohdshamoon841@gmail.com>
2021-03-11 16:14:57 +0530

    code cleanup

--- src/containers/Trigger/Trigger.test.tsx
+++ src/containers/Trigger/Trigger.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { cleanup, fireEvent, render, waitFor } from '@testing-library/react';
+import { fireEvent, render, waitFor } from '@testing-library/react';
 import { MockedProvider } from '@apollo/client/testing';
 
 import { Trigger } from './Trigger';
