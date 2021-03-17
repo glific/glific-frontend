71ea2c712c694a202e13ea77b55544e877d118c3
mdshamoon <mohdshamoon841@gmail.com>
2021-03-17 14:53:59 +0530

    code cleanups

--- src/containers/Trigger/TriggerList/TriggerList.tsx
+++ src/containers/Trigger/TriggerList/TriggerList.tsx
@@ -5,6 +5,7 @@ import { useHistory } from 'react-router-dom';
 import styles from './TriggerList.module.css';
 import { ReactComponent as TriggerIcon } from '../../../assets/images/icons/Trigger/Union.svg';
 import { ReactComponent as ClockIcon } from '../../../assets/images/icons/Trigger/Clock.svg';
+import { ReactComponent as ClockInactiveIcon } from '../../../assets/images/icons/Trigger/Inactive.svg';
 import { ReactComponent as DuplicateIcon } from '../../../assets/images/icons/Flow/Duplicate.svg';
 import { List } from '../../List/List';
 import { TRIGGER_LIST_QUERY, TRIGGER_QUERY_COUNT } from '../../../graphql/queries/Trigger';
@@ -27,12 +28,10 @@ const getTooltip = (frequency: any, days: any) => {
   return `Repeat: ${frequency}${frequency === 'weekly' ? `(${obj.toString()})` : ''}`;
 };
 
-const getName = (flow: any, startAt: any, frequency: any, days: any) => (
+const getName = (flow: any, startAt: any, frequency: any, days: any, isActive: any) => (
   <p className={styles.LabelText}>
     <Tooltip title={getTooltip(frequency, days)} tooltipClass={styles.Tooltip} placement="right">
-      <span className={styles.TriggerIcon}>
-        <ClockIcon />
-      </span>
+      <span className={styles.TriggerIcon}>{isActive ? <ClockIcon /> : <ClockInactiveIcon />}</span>
     </Tooltip>
     {`${flow.name}_${moment(startAt).format('DD/MM/yyyy_hh:mmA')}`}
   </p>
@@ -44,8 +43,8 @@ const getEndDate = (date: any) => (
 
 const getCollections = (group: any) => <p className={styles.Collection}>{group.label}</p>;
 
-const getColumns = ({ endDate, group, frequency, days, flow, startAt }: any) => ({
-  name: getName(flow, startAt, frequency, days),
+const getColumns = ({ endDate, group, frequency, days, flow, startAt, isActive }: any) => ({
+  name: getName(flow, startAt, frequency, days, isActive),
   startAt: getEndDate(endDate),
   collections: getCollections(group),
 });
