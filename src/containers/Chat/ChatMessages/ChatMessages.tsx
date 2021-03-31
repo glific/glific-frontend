990c62ba45d2898947ac1e2142b09519d042eab9
mdshamoon <mohdshamoon841@gmail.com>
2021-03-31 11:59:14 +0530

    added error for collections

--- src/containers/Chat/ChatMessages/ChatMessages.tsx
+++ src/containers/Chat/ChatMessages/ChatMessages.tsx
@@ -255,6 +255,12 @@ export const ChatMessages: React.SFC<ChatMessagesProps> = ({ contactId, collecti
 
   const [sendMessageToCollection] = useMutation(CREATE_AND_SEND_MESSAGE_TO_COLLECTION_MUTATION, {
     refetchQueries: [{ query: SEARCH_QUERY, variables: SEARCH_QUERY_VARIABLES }],
+    onError: (error: any) => {
+      if (error.message) {
+        setNotification(client, error.message, 'warning');
+      }
+      return null;
+    },
   });
 
   const updatePayload = (payload: any, selectedTemplate: any, variableParam: any) => {
