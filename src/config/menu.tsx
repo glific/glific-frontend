626ba0bc4b29f69b69fe483dd0f50a6fbb172e1c
Kurund Jalmi <kurund.jalmi@gmail.com>
2021-04-07 23:55:19 +0100

    simplify the translation structure

--- src/config/menu.tsx
+++ src/config/menu.tsx
@@ -2,37 +2,37 @@ import i18n from 'i18next';
 
 export const sideDrawerMenus = [
   {
-    title: i18n.t('menu.chats'),
+    title: i18n.t('chats'),
     path: '/chat',
     icon: 'chat',
   },
   {
-    title: i18n.t('menu.tags'),
+    title: i18n.t('tags'),
     path: '/tag',
     icon: 'tag',
   },
   {
-    title: i18n.t('menu.speedsends'),
+    title: i18n.t('speedsends'),
     path: '/speed-send',
     icon: 'speed-send',
   },
   {
-    title: i18n.t('menu.flows'),
+    title: i18n.t('flows'),
     path: '/flow',
     icon: 'flow',
   },
   {
-    title: i18n.t('menu.triggers'),
+    title: i18n.t('triggers'),
     path: '/trigger',
     icon: 'trigger',
   },
   {
-    title: i18n.t('menu.searches'),
+    title: i18n.t('searches'),
     path: '/search',
     icon: 'search',
   },
   {
-    title: i18n.t('menu.templates'),
+    title: i18n.t('templates'),
     path: '/template',
     icon: 'template',
   },
@@ -40,30 +40,30 @@ export const sideDrawerMenus = [
 
 export const staffManagementMenus = [
   {
-    title: i18n.t('menu.collections'),
+    title: i18n.t('collections'),
     path: '/collection',
   },
   {
-    title: i18n.t('menu.staffmanagement'),
+    title: i18n.t('staffmanagement'),
     path: '/staff-management',
   },
   {
-    title: i18n.t('menu.blockedcontacts'),
+    title: i18n.t('blockedcontacts'),
     path: '/blocked-contacts',
   },
 ];
 
 export const userAccountMenus = [
   {
-    title: i18n.t('menu.myprofile'),
+    title: i18n.t('myprofile'),
     path: '/user-profile',
   },
   {
-    title: i18n.t('menu.myaccount'),
+    title: i18n.t('myaccount'),
     path: '/myaccount',
   },
   {
-    title: i18n.t('menu.logout'),
+    title: i18n.t('logout'),
     path: '/logout/user',
     className: 'Danger',
   },
