3532a22aaf9067df00c8783295b5aa6063bb1de3
Kurund Jalmi <kurund.jalmi@gmail.com>
2021-04-10 23:31:17 +0100

    add menu

---
+++ src/config/menu.ts
@@ -0,0 +1,68 @@
+export const sideDrawerMenus = [
+  {
+    title: 'chats',
+    path: '/chat',
+    icon: 'chat',
+  },
+  {
+    title: 'tags',
+    path: '/tag',
+    icon: 'tag',
+  },
+  {
+    title: 'speedsends',
+    path: '/speed-send',
+    icon: 'speed-send',
+  },
+  {
+    title: 'flows',
+    path: '/flow',
+    icon: 'flow',
+  },
+  {
+    title: 'triggers',
+    path: '/trigger',
+    icon: 'trigger',
+  },
+  {
+    title: 'searches',
+    path: '/search',
+    icon: 'search',
+  },
+  {
+    title: 'templates',
+    path: '/template',
+    icon: 'template',
+  },
+];
+
+export const staffManagementMenus = [
+  {
+    title: 'collections',
+    path: '/collection',
+  },
+  {
+    title: 'staffmanagement',
+    path: '/staff-management',
+  },
+  {
+    title: 'blockedcontacts',
+    path: '/blocked-contacts',
+  },
+];
+
+export const userAccountMenus = [
+  {
+    title: 'myprofile',
+    path: '/user-profile',
+  },
+  {
+    title: 'myaccount',
+    path: '/myaccount',
+  },
+  {
+    title: 'logout',
+    path: '/logout/user',
+    className: 'Danger',
+  },
+];
