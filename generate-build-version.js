167291693ee7e4d68e4aa76ef05cdae44972baea
Shreesh Nirgudkar <shreesh.nirgudkar@gmail.com>
2021-04-19 17:15:38 +0530

    Added generate-build-version script to add version in public/meta.json

---
+++ generate-build-version.js
@@ -0,0 +1,21 @@
+/* eslint-disable consistent-return */
+/* eslint-disable no-console */
+const fs = require('fs');
+
+const packageJson = require('./package.json');
+
+const appVersion = packageJson.version;
+
+const jsonData = {
+  version: appVersion,
+};
+
+const jsonContent = JSON.stringify(jsonData);
+
+fs.writeFile('./public/meta.json', jsonContent, 'utf8', (err) => {
+  if (err) {
+    console.log('An error occured while writing JSON Object to meta.json');
+    return console.log(err);
+  }
+  console.log('meta.json file has been saved with latest version number');
+});
