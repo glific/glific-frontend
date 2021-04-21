06a1313d547a6d5130a9c4845beced2247b09da1
mdshamoon <mohdshamoon841@gmail.com>
2021-04-21 14:40:13 +0530

    fixed test case

--- src/containers/Template/List/SpeedSendList/SpeedSendList.test.tsx
+++ src/containers/Template/List/SpeedSendList/SpeedSendList.test.tsx
@@ -5,6 +5,7 @@ import { MockedProvider } from '@apollo/client/testing';
 import { SpeedSendList } from './SpeedSendList';
 import { within } from '@testing-library/dom';
 import { TEMPLATE_MOCKS } from '../../Template.test.helper';
+import { setUserSession } from '../../../../services/AuthService';
 
 afterEach(cleanup);
 const mocks = TEMPLATE_MOCKS;
@@ -20,12 +21,15 @@ const speedSend = (
 test('SpeedSendList has proper headers', async () => {
   const { getByText, container } = render(speedSend);
 
+  setUserSession(JSON.stringify({ organization: { id: '1' }, roles: ['Admin'] }));
+
   await waitFor(() => {
     expect(getByText('Speed sends')).toBeInTheDocument();
   });
 
-  const { getByText: getByTextTableheader } = within(container.querySelector('thead'));
-  expect(getByTextTableheader('LABEL')).toBeInTheDocument();
-  expect(getByTextTableheader('BODY')).toBeInTheDocument();
-  expect(getByTextTableheader('ACTIONS')).toBeInTheDocument();
+  await waitFor(() => {
+    expect(getByText('LABEL')).toBeInTheDocument();
+    expect(getByText('BODY')).toBeInTheDocument();
+    expect(getByText('ACTIONS')).toBeInTheDocument();
+  });
 });
