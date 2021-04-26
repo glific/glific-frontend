0519975875833bf0a9388aa63313faec64f350d6
Shreesh Nirgudkar <shreesh.nirgudkar@gmail.com>
2021-04-26 20:04:31 +0530

    Code cleanup and added test case for organization

---
+++ src/containers/Organization/Organization.test.tsx
@@ -0,0 +1,54 @@
+import { MockedProvider } from '@apollo/client/testing';
+import { render, screen } from '@testing-library/react';
+import * as Yup from 'yup';
+import UserEvent from '@testing-library/user-event';
+
+import { MemoryRouter } from 'react-router-dom';
+import { Input } from '../../components/UI/Form/Input/Input';
+import { Organization } from './Organization';
+import { act } from 'react-dom/test-utils';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('NGO name is required'),
+});
+
+const props = {
+  pageTitle: 'Setup your NGO on Glific',
+  buttonText: 'GET STARTED',
+  formFields: [
+    {
+      component: Input,
+      name: 'name',
+      type: 'text',
+      placeholder: 'NGO name',
+    },
+  ],
+  validationSchema: schema,
+  saveHandler: jest.fn(),
+  initialFormValues: {
+    name: '',
+  },
+  errorMessage: { global: 'Something went wrong' },
+};
+
+const wrapper = (
+  <MockedProvider addTypename={false}>
+    <MemoryRouter>
+      <Organization {...props} />
+    </MemoryRouter>
+  </MockedProvider>
+);
+
+test('it should render component and show error messages', async () => {
+  const { container, findByTestId } = render(wrapper);
+
+  const registration = await findByTestId('RegistrationContainer');
+  expect(registration).toBeInTheDocument();
+
+  const submit = await findByTestId('SubmitButton');
+  act(() => {
+    UserEvent.click(submit);
+  });
+
+  expect(container.getElementsByClassName('ErrorMessage')[0]).toBeInTheDocument();
+});
--- src/containers/Organization/Organization.tsx
+++ src/containers/Organization/Organization.tsx
@@ -19,7 +19,6 @@ export interface OrganizationProps {
   validationSchema?: any;
   titleSubText?: string;
   errorMessage?: any;
-  successMessage?: string;
 }
 
 export const Organization: React.SFC<OrganizationProps> = (props) => {
@@ -32,7 +31,6 @@ export const Organization: React.SFC<OrganizationProps> = (props) => {
     validationSchema,
     titleSubText,
     errorMessage,
-    successMessage,
   } = props;
 
   const [loading, setLoading] = useState(false);
@@ -74,69 +72,63 @@ export const Organization: React.SFC<OrganizationProps> = (props) => {
     setCaptcha(null);
   };
 
-  let formElements;
-  // we should not render form elements when displaying success message
-  if (!successMessage) {
-    formElements = (
-      <>
-        <div className={boxTitleClass.join(' ')}>
-          <Typography variant="h4" classes={{ root: styles.TitleText }}>
-            {pageTitle}
-          </Typography>
-        </div>
-        <div className={styles.SubText}>{titleSubText}</div>
+  const formElements = (
+    <>
+      <div className={boxTitleClass.join(' ')}>
+        <Typography variant="h4" classes={{ root: styles.TitleText }}>
+          {pageTitle}
+        </Typography>
+      </div>
+      <div className={styles.SubText}>{titleSubText}</div>
 
-        <Formik
-          initialValues={initialFormValues}
-          validationSchema={validationSchema}
-          onSubmit={(item, { setErrors }) => {
-            setLoading(true);
-            /**
-             * SetError and SetLoading is used to set server side messages
-             * and toggle loading on and off
-             */
-            saveHandler(item, captcha, setErrors, setLoading);
-          }}
-        >
-          {({ submitForm }) => (
-            <div className={styles.CenterBox}>
-              <Form className={styles.Form}>
-                {formFields.map((field, index) => {
-                  let fieldInfo = { ...field };
-                  if (field.type === 'phone') {
-                    fieldInfo = { ...field, handlePhone };
-                  }
-                  const key = index;
-                  return <Field className={styles.Form} key={key} {...fieldInfo} />;
-                })}
-                <Captcha onChange={handleCaptchaChange} onError={handleCaptchaError} />
-                <div className={styles.CenterButton}>
-                  <Button
-                    variant={buttonContainedVariant ? 'contained' : 'outlined'}
-                    color="primary"
-                    onClick={submitForm}
-                    className={styles.OrgButton}
-                    data-testid="SubmitButton"
-                    loading={loading}
-                    disabled={!captcha}
-                  >
-                    {loading ? null : buttonText}
-                  </Button>
-                </div>
-                {/* We neeed to add this submit button to enable form sumbitting when user hits enter
-                key. This is an workaround solution till the bug in formik or react is fixed. For
-                more info: https://github.com/formium/formik/issues/1418 */}
-                <input className={styles.SubmitAction} type="submit" />
-              </Form>
-              {displayErrorMessage}
-            </div>
-          )}
-        </Formik>
-      </>
-    );
-  } else {
-    formElements = <div className={styles.SuccessMessage}>{successMessage}</div>;
-  }
+      <Formik
+        initialValues={initialFormValues}
+        validationSchema={validationSchema}
+        onSubmit={(item, { setErrors }) => {
+          setLoading(true);
+          /**
+           * SetError and SetLoading is used to set server side messages
+           * and toggle loading on and off
+           */
+          saveHandler(item, captcha, setErrors, setLoading);
+        }}
+      >
+        {({ submitForm }) => (
+          <div className={styles.CenterBox}>
+            <Form className={styles.Form}>
+              {formFields.map((field, index) => {
+                let fieldInfo = { ...field };
+                if (field.type === 'phone') {
+                  fieldInfo = { ...field, handlePhone };
+                }
+                const key = index;
+                return <Field className={styles.Form} key={key} {...fieldInfo} />;
+              })}
+              <Captcha onChange={handleCaptchaChange} onError={handleCaptchaError} />
+              <div className={styles.CenterButton}>
+                <Button
+                  variant={buttonContainedVariant ? 'contained' : 'outlined'}
+                  color="primary"
+                  onClick={submitForm}
+                  className={styles.OrgButton}
+                  data-testid="SubmitButton"
+                  loading={loading}
+                  disabled={!captcha}
+                >
+                  {loading ? null : buttonText}
+                </Button>
+              </div>
+              {/* We neeed to add this submit button to enable form sumbitting when user hits enter
+               key. This is an workaround solution till the bug in formik or react is fixed. For
+               more info: https://github.com/formium/formik/issues/1418 */}
+              <input className={styles.SubmitAction} type="submit" />
+            </Form>
+            {displayErrorMessage}
+          </div>
+        )}
+      </Formik>
+    </>
+  );
 
   return (
     <div className={styles.Container} data-testid="RegistrationContainer">
