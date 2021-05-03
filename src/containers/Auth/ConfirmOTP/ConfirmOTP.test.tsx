45c8bc9aa8358fc044d97e2f4d96c56407d11101
mdshamoon <mohdshamoon841@gmail.com>
2021-05-03 11:11:21 +0530

    fix confirmOTP test cases

--- src/containers/Auth/ConfirmOTP/ConfirmOTP.test.tsx
+++ src/containers/Auth/ConfirmOTP/ConfirmOTP.test.tsx
@@ -1,14 +1,11 @@
-import { render, screen, wait } from '@testing-library/react';
+import { act, render, screen, wait } from '@testing-library/react';
 import UserEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
 import axios from 'axios';
 
+jest.mock('axios');
+
 import { ConfirmOTP } from './ConfirmOTP';
-
-const mockAxios = jest.genMockFromModule('axios');
-
-// this is the key to fix the axios.create() undefined error!
-mockAxios.create = jest.fn(() => mockAxios);
 
 const defaultProps = { location: { state: { name: '', phoneNumber: '', password: '' } } };
 
@@ -45,8 +42,9 @@ describe('<ConfirmOTP />', () => {
 
     // let's mock successful otp submission
     const responseData = { data: { data: { data: {} } } };
-    mockAxios.post.mockImplementationOnce(() => Promise.resolve(responseData));
-    await wait();
+    act(() => {
+      axios.post.mockImplementationOnce(() => Promise.resolve(responseData));
+    });
   });
 
   it('test the OTP form submission with incorrect OTP', async () => {
@@ -62,8 +60,10 @@ describe('<ConfirmOTP />', () => {
 
     // let's mock error response on otp submission
     const errorMessage = 'We are unable to register, kindly contact your technical team.';
-    mockAxios.post.mockImplementationOnce(() => Promise.reject(new Error(errorMessage)));
-    await wait();
+
+    act(() => {
+      axios.post.mockImplementationOnce(() => Promise.reject(new Error(errorMessage)));
+    });
   });
 
   it('test successful resend functionality', async () => {
@@ -73,8 +73,10 @@ describe('<ConfirmOTP />', () => {
     const responseData = {
       data: { message: 'OTP sent successfully to 919967665667', phone: '919967665667' },
     };
-    mockAxios.post.mockImplementationOnce(() => Promise.resolve(responseData));
-    await wait();
+
+    act(() => {
+      axios.post.mockImplementationOnce(() => Promise.resolve(responseData));
+    });
 
     // click on resend button
     const resendButton = screen.getByTestId('resendOtp');
@@ -86,12 +88,15 @@ describe('<ConfirmOTP />', () => {
 
     // set the mock
     const errorMessage = 'Cannot send the otp to 919967665667';
-    mockAxios.post.mockImplementationOnce(() => Promise.reject(new Error(errorMessage)));
-    await wait();
 
+    act(() => {
+      axios.post.mockImplementationOnce(() => Promise.reject(new Error(errorMessage)));
+    });
     // click on resend button
     const resendButton = screen.getByTestId('resendOtp');
-    UserEvent.click(resendButton);
-    await wait();
+
+    act(() => {
+      UserEvent.click(resendButton);
+    });
   });
 });
