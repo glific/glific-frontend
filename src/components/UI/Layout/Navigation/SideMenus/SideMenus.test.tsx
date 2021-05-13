4c77c3080f5ede498dfa096851c0f8d37f4148b3
kumudini <kumudinikshirsagar2@gmail.com>
2021-05-13 16:06:31 +0530

    fixed test case for sidemenus

--- src/components/UI/Layout/Navigation/SideMenus/SideMenus.test.tsx
+++ src/components/UI/Layout/Navigation/SideMenus/SideMenus.test.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
 import SideMenus from './SideMenus';
 import { getCurrentUserQuery } from '../../../../../mocks/User';
 
 const mocks = [getCurrentUserQuery];
+const cache = new InMemoryCache({ addTypename: false });
+const client = new ApolloClient({
+  cache: cache,
+});
+
 describe('side menu testing', () => {
   const component = (
     <MemoryRouter>
-      <SideMenus opened={false} />
+      <ApolloProvider client={client}>
+        <SideMenus opened={false} />
+      </ApolloProvider>
     </MemoryRouter>
   );
 
