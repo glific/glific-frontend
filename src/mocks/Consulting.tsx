d3c1e2736e03fbf3cac19fd5811b0afa83e2e51a
Shreesh Nirgudkar <shreesh.nirgudkar@gmail.com>
2021-05-14 17:24:50 +0530

    Added test cases for consultingHours component

--- src/mocks/Consulting.tsx
+++ src/mocks/Consulting.tsx
@@ -1,4 +1,9 @@
-import { GET_CONSULTING_HOURS, GET_CONSULTING_HOURS_COUNT } from '../graphql/queries/Consulting';
+import {
+  GET_CONSULTING_HOURS,
+  GET_CONSULTING_HOURS_COUNT,
+  GET_CONSULTING_HOURS_BY_ID,
+} from '../graphql/queries/Consulting';
+import { FILTER_ORGANIZATIONS } from '../graphql/queries/Organization';
 
 const mockListDummyData = new Array(10).fill(null).map((val: any, index: number) => ({
   id: `${index}`,
@@ -45,3 +50,67 @@ export const listingMock = [
     },
   },
 ];
+
+export const getConsultingHour = {
+  request: {
+    query: GET_CONSULTING_HOURS_BY_ID,
+    variables: { id: '0' },
+  },
+  result: {
+    data: {
+      consultingHour: {
+        id: '0',
+        content: 'Testing',
+        isBillable: true,
+        duration: 30,
+        insertedAt: '2021-05-13T05:17:31Z',
+        participants: 'John Doe',
+        staff: `Gfific team`,
+        organizationName: 'Foogle',
+        updatedAt: '2021-05-13T05:35:51Z',
+        when: '2021-05-06T10:30:00Z',
+      },
+    },
+  },
+};
+
+export const getOrganizationList = {
+  request: {
+    query: FILTER_ORGANIZATIONS,
+    variables: {
+      filter: {},
+      opts: {
+        limit: null,
+        offset: 0,
+        order: 'ASC',
+      },
+    },
+  },
+  result: {
+    data: {
+      organizations: [
+        {
+          id: '2',
+          insertedAt: '2021-04-28T05:19:51Z',
+          isActive: true,
+          isApproved: false,
+          name: 'Foogle',
+        },
+        {
+          id: '1',
+          insertedAt: '2021-04-28T05:06:30Z',
+          isActive: true,
+          isApproved: true,
+          name: 'Glific',
+        },
+        {
+          id: '3',
+          insertedAt: '2021-04-28T05:06:30Z',
+          isActive: false,
+          isApproved: false,
+          name: 'Test',
+        },
+      ],
+    },
+  },
+};
