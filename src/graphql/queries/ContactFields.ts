0a646bc6537432ec89d797bf676feb0e84916b20
Shreesh Nirgudkar <shreesh.nirgudkar@gmail.com>
2021-05-26 17:52:25 +0530

    Added queries and mutations for contact fields

---
+++ src/graphql/queries/ContactFields.ts
@@ -0,0 +1,47 @@
+import { gql } from '@apollo/client';
+
+export const GET_ALL_CONTACT_FIELDS = gql`
+  query contactsFields($filter: contactsFieldFilter, $opts: Opts) {
+    contactsFields(filter: $filter, opts: $opts) {
+      valueType
+      updatedAt
+      shortcode
+      scope
+      name
+      insertedAt
+      id
+      organization {
+        shortcode
+        isApproved
+        isActive
+      }
+    }
+  }
+`;
+
+export const GET_CONTACT_FIELD_BY_ID = gql`
+  query contacts_field($id: ID!) {
+    contacts_field(id: $id) {
+      contacts_field {
+        valueType
+        updatedAt
+        shortcode
+        scope
+        name
+        insertedAt
+        id
+        organization {
+          shortcode
+          isApproved
+          isActive
+        }
+      }
+    }
+  }
+`;
+
+export const COUNT_CONTACT_FIELDS = gql`
+  query countContactsFields($filter: ContactsFieldfilter) {
+    countContactsFields(filter: $filter)
+  }
+`;
