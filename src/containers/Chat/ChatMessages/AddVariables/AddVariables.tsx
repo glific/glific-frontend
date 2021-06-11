db6783a5fe05bbf1c6dde1f2cca58b4aea32d64d
kumudini <kumudinikshirsagar2@gmail.com>
2021-06-11 14:38:31 +0530

    added contact variables suggestion

--- src/containers/Chat/ChatMessages/AddVariables/AddVariables.tsx
+++ src/containers/Chat/ChatMessages/AddVariables/AddVariables.tsx
@@ -2,10 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 import { useTranslation } from 'react-i18next';
+import axios from 'axios';
+
+import { FLOW_EDITOR_API } from '../../../../config/index';
+import { getAuthSession } from '../../../../services/AuthService';
 
 import { DialogBox } from '../../../../components/UI/DialogBox/DialogBox';
-import { Input } from '../../../../components/UI/Form/Input/Input';
 import { pattern } from '../../../../common/constants';
+import { AutoComplete } from '../../../../components/UI/Form/AutoComplete/AutoComplete';
 
 export interface AddVariablesPropTypes {
   setVariable: any;
@@ -28,18 +32,56 @@ export const AddVariables: React.FC<AddVariablesPropTypes> = ({
   const [validation, setValidation] = useState<any>({});
   const [initialValues, setInitialValues] = useState<any>({});
   const { t } = useTranslation();
+  const [contactVariables, setContactVariables] = useState<any>([]);
+  let contacts: any = [];
+  let fields: any = [];
+
+  const glificBase = FLOW_EDITOR_API;
+  const prefix1 = '@contact.field.';
+  const prefix2 = '@contact.';
+
+  useEffect(() => {
+    // get fields keys
+    axios
+      .get(`${glificBase}fields`, {
+        headers: { Authorization: getAuthSession('access_token') },
+      })
+      .then((res) => {
+        fields = res.data.results.map((i: any) => ({ ...i, key: prefix1.concat(i.key) }));
+
+        // get contact keys
+        axios
+          .get(`${glificBase}completion`, {
+            headers: { Authorization: getAuthSession('access_token') },
+          })
+          .then((response) => {
+            const properties = response.data.types[5];
+            contacts = properties.properties
+              .map((i: any) => ({ ...i, key: prefix2.concat(i.key) }))
+              .concat(fields);
+            setContactVariables(contacts);
+          });
+      });
+  }, []);
 
   useEffect(() => {
     const isVariable = bodyText.match(pattern);
     if (isVariable) {
       const formFieldItem: any = [];
       const validationObj: any = {};
+
       for (let index = 1; index <= isVariable.length; index += 1) {
         formFieldItem.push({
-          component: Input,
+          component: AutoComplete,
           name: `variable${index}`,
           type: 'text',
-          placeholder: `Variable ${index}`,
+          options: contactVariables,
+          optionLabel: 'key',
+          multiple: false,
+          textFieldProps: {
+            variant: 'outlined',
+            label: `Variable ${index}`,
+          },
         });
 
         validationObj[`variable${index}`] = Yup.string().required(`Variable ${index} is required.`);
@@ -47,7 +89,7 @@ export const AddVariables: React.FC<AddVariablesPropTypes> = ({
       setFormFieldItems(formFieldItem);
       setValidation(validationObj);
     }
-  }, [bodyText]);
+  }, [bodyText, contactVariables]);
 
   useEffect(() => {
     const initialValue: any = {};
