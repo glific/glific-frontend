8e17eeee9061e8cde6110b3d9e6a6487f04491ce
Kurund Jalmi <kurund.jalmi@gmail.com>
2021-06-19 00:21:58 +0100

    remove deprecated wait

--- src/containers/MyAccount/MyAccount.test.tsx
+++ src/containers/MyAccount/MyAccount.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, wait } from '@testing-library/react';
+import { act, render, screen, waitFor } from '@testing-library/react';
 import UserEvent from '@testing-library/user-event';
 import { MockedProvider } from '@apollo/client/testing';
 import axios from 'axios';
@@ -31,153 +31,132 @@ describe('<MyAccount />', () => {
   });
 
   test('generate OTP success flow', async () => {
-    const { container, findByTestId } = render(wrapper);
-
-    await wait();
+    const { container } = render(wrapper);
 
     // let's mock successful sending of OTP
     const responseData = { data: { data: { data: {} } } };
     axios.post.mockImplementationOnce(() => Promise.resolve(responseData));
-    await wait();
 
-    // click on generate OTP
-    const generateOTPButton = screen.getByText('Generate OTP');
-    UserEvent.click(generateOTPButton);
-    await wait();
+    await waitFor(() => {
+      // click on generate OTP
+      const generateOTPButton = screen.getByText('Generate OTP');
+      UserEvent.click(generateOTPButton);
+    });
 
     // set the mock
     const resendPasswordResponse = {
       data: { message: 'OTP sent successfully to 919967665667', phone: '919967665667' },
     };
     axios.post.mockImplementationOnce(() => Promise.resolve(resendPasswordResponse));
-    await wait();
 
-    // click on resend button
-    const resendButton = screen.getByTestId('resendOtp');
-    UserEvent.click(resendButton);
+    await waitFor(() => {
+      // click on resend button
+      const resendButton = screen.getByTestId('resendOtp');
+      UserEvent.click(resendButton);
+    });
 
     // enter otp
     const input = container.querySelector('input[type="text"]');
     UserEvent.type(input, '76554');
-    await wait();
 
     // enter password
     const password = container.querySelector('input[type="password"]');
     UserEvent.type(password, 'pass123456');
-    await wait();
 
     // view password
-    const passwordToggle = await findByTestId('passwordToggle');
+    const passwordToggle = screen.getByTestId('passwordToggle');
     UserEvent.click(passwordToggle);
-    await wait();
 
     // click on save button
     const saveButton = screen.getByText('Save');
     UserEvent.click(saveButton);
-    await wait();
   });
 
   test('generate OTP error response', async () => {
     const { findByTestId } = render(wrapper);
 
-    await wait();
-
     // let's mock error case sending of OTP
     const errorMessage = 'Cannot register 919967665667';
     axios.post.mockImplementationOnce(() => Promise.reject(new Error(errorMessage)));
-    await wait();
 
-    // click on generate OTP
-    const generateOTPButton = screen.getByText('Generate OTP');
-    UserEvent.click(generateOTPButton);
-    await wait();
+    await waitFor(() => {
+      // click on generate OTP
+      const generateOTPButton = screen.getByText('Generate OTP');
+      UserEvent.click(generateOTPButton);
+    });
 
     // close the alert
     const closeAlert = await findByTestId('crossIcon');
     UserEvent.click(closeAlert);
-    await wait();
   });
 
   test('generate OTP success flow with cancel', async () => {
     render(wrapper);
 
-    await wait();
-
     // let's mock successful sending of OTP
     const responseData = { data: { data: { data: {} } } };
     axios.post.mockImplementationOnce(() => Promise.resolve(responseData));
-    await wait();
 
-    // click on generate OTP
-    const generateOTPButton = screen.getByText('Generate OTP');
-    UserEvent.click(generateOTPButton);
-    await wait();
+    await waitFor(() => {
+      // click on generate OTP
+      const generateOTPButton = screen.getByText('Generate OTP');
+      UserEvent.click(generateOTPButton);
+    });
 
     // click on CANCEL button
     const cancelButton = screen.getByText('Cancel');
     UserEvent.click(cancelButton);
-    await wait();
   });
 
   test('generate OTP error with incorrect OTP', async () => {
     const { container } = render(wrapper);
 
-    await wait();
-
     // let's mock successful sending of OTP
     const responseData = { data: { data: { data: {} } } };
     axios.post.mockImplementationOnce(() => Promise.resolve(responseData));
-    await wait();
 
-    // click on generate OTP
-    const generateOTPButton = screen.getByText('Generate OTP');
-    UserEvent.click(generateOTPButton);
-    await wait();
+    await waitFor(() => {
+      // click on generate OTP
+      const generateOTPButton = screen.getByText('Generate OTP');
+      UserEvent.click(generateOTPButton);
+    });
 
     // enter otp
     const input = container.querySelector('input[type="text"]');
     UserEvent.type(input, '1234');
-    await wait();
 
     // enter password
     const password = container.querySelector('input[type="password"]');
     UserEvent.type(password, 'pass123456');
-    await wait();
 
     // click on save button
     const saveButton = screen.getByText('Save');
     UserEvent.click(saveButton);
-    await wait();
   });
 
   test('generate OTP error with too many attempts', async () => {
     const { container } = render(wrapper);
 
-    await wait();
-
     // let's mock successful sending of OTP
     const responseData = { data: { data: { data: {} } } };
     axios.post.mockImplementationOnce(() => Promise.resolve(responseData));
-    await wait();
 
-    // click on generate OTP
-    const generateOTPButton = screen.getByText('Generate OTP');
-    UserEvent.click(generateOTPButton);
-    await wait();
+    await waitFor(() => {
+      // click on generate OTP
+      const generateOTPButton = screen.getByText('Generate OTP');
+      UserEvent.click(generateOTPButton);
+    });
 
     // enter otp
     const input = container.querySelector('input[type="text"]');
     UserEvent.type(input, '4567');
-    await wait();
 
     // enter password
     const password = container.querySelector('input[type="password"]');
     UserEvent.type(password, 'pass123456');
-    await wait();
 
     // click on save button
     const saveButton = screen.getByText('Save');
     UserEvent.click(saveButton);
-    await wait();
   });
 });
