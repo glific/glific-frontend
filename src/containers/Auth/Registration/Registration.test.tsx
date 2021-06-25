650c674bacb2c0b8ae6b18e9e9a37c28033f7b1f
Kurund Jalmi <kurund.jalmi@gmail.com>
2021-06-25 23:08:37 +0100

    remove duplicate test and now we are actually validating the response

--- src/containers/Auth/Registration/Registration.test.tsx
+++ src/containers/Auth/Registration/Registration.test.tsx
@@ -39,15 +39,14 @@ describe('<Registration />', () => {
     const password = await container.querySelector('input[type="password"]');
     userEvent.type(password, 'pass123456');
 
-    await waitFor(() => {
-      // click on continue
-      const continueButton = screen.getByText('Continue');
-      userEvent.click(continueButton);
-    });
+    // click on continue
+    const continueButton = screen.getByText('Continue');
+    userEvent.click(continueButton);
 
     // let's mock successful registration submission
-    const responseData = { data: { data: { data: {} } } };
-    axios.post.mockImplementationOnce(() => Promise.resolve(responseData));
+    // as we cannot register without opt in we will always get error response
+    const errorMessage = 'Cannot register 919978776554';
+    axios.post.mockImplementationOnce(() => Promise.reject(new Error(errorMessage)));
 
     await waitFor(() => {
       const authContainer = screen.getByTestId('AuthContainer');
@@ -55,28 +54,5 @@ describe('<Registration />', () => {
         'We are unable to register, kindly contact your technical team.'
       );
     });
-  });
-
-  it('should submit the form correctly and give error', async () => {
-    const { container } = render(wrapper);
-
-    const userName = await container.querySelector('input[name="userName"]');
-    userEvent.type(userName, 'Jane Doe');
-
-    const phone = await container.querySelector('input[type="tel"]');
-    userEvent.type(phone, '+919978776554');
-
-    const password = await container.querySelector('input[type="password"]');
-    userEvent.type(password, 'pass123456');
-
-    await waitFor(() => {
-      // click on continue
-      const continueButton = screen.getByText('Continue');
-      userEvent.click(continueButton);
-    });
-
-    // set the mock error case while registration
-    const errorMessage = 'Cannot register 919978776554';
-    axios.post.mockImplementationOnce(() => Promise.reject(new Error(errorMessage)));
   });
 });
