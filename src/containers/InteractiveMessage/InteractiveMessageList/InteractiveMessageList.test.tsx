6f86b2715be32bcb39b99890e790f76d95afb580
mdshamoon <mohdshamoon841@gmail.com>
2021-07-09 17:13:10 +0530

    fixed deepscan issue

--- src/containers/InteractiveMessage/InteractiveMessageList/InteractiveMessageList.test.tsx
+++ src/containers/InteractiveMessage/InteractiveMessageList/InteractiveMessageList.test.tsx
@@ -1,4 +1,4 @@
-import { render, waitFor, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { MockedProvider } from '@apollo/client/testing';
 import InteractiveMessageList from './InteractiveMessageList';
 import { BrowserRouter as Router } from 'react-router-dom';
