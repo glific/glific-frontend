0cc388d32d9a29f4cb66e406284ab6817eec31c7
kumudini <kumudinikshirsagar2@gmail.com>
2021-07-11 13:49:35 +0530

    fixed deep scan issue

--- src/containers/Form/FormLayout.tsx
+++ src/containers/Form/FormLayout.tsx
@@ -169,7 +169,7 @@ export const FormLayout: React.SFC<FormLayoutProps> = ({
       const updatedItem = itemUpdatedObject[listItem];
       const { errors } = itemUpdatedObject;
 
-      if (itemUpdatedObject && errors) {
+      if (errors) {
         if (customHandler) {
           customHandler(client, errors);
         } else {
