549f7490aea6f77570dfeea3c22c5a6b6b69138f
Kurund Jalmi <kurund.jalmi@gmail.com>
2021-07-29 00:10:49 +0100

    UI form cleanup

--- src/components/UI/Form/Calendar/Calendar.test.tsx
+++ src/components/UI/Form/Calendar/Calendar.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import UserEvent from '@testing-library/user-event';
+
 import { Calendar } from './Calendar';
 
 describe('<Calendar />', () => {
