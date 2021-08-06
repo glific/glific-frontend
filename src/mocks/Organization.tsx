b9896bc72fb1af8667b21411dbf3c8d4018f86c9
Shreesh Nirgudkar <shreesh.nirgudkar@gmail.com>
2021-08-06 17:39:14 +0530

    Updated testcases for organisations

--- src/mocks/Organization.tsx
+++ src/mocks/Organization.tsx
@@ -99,41 +99,37 @@ export const getOrganizationQuery = [
   {
     request: {
       query: GET_ORGANIZATION,
-      variables: { id: '1' },
+      variables: { id: 1 },
     },
     result: {
       data: {
         organization: {
           organization: {
-            defaultLanguage: { id: 1, label: 'English (United States)' },
             activeLanguages: [
-              {
-                id: 1,
-                label: 'English (United States)',
-              },
+              { id: '1', label: 'English' },
+              { id: '2', label: 'Hindi' },
             ],
-            id: 1,
+            contact: { phone: '917834811114' },
+            defaultLanguage: { id: '1', label: 'English' },
+            id: '1',
+            name: 'Glific',
             outOfOffice: {
+              defaultFlowId: '7',
               enabled: true,
-              defaultFlowId: 1,
               enabledDays: [
                 { enabled: true, id: 1 },
                 { enabled: true, id: 2 },
                 { enabled: true, id: 3 },
                 { enabled: true, id: 4 },
                 { enabled: true, id: 5 },
-                { enabled: false, id: 6 },
-                { enabled: false, id: 7 },
+                { enabled: true, id: 6 },
+                { enabled: true, id: 7 },
               ],
-              endTime: '12:30:27',
-              flowId: 2,
-              startTime: '12:31:27',
+              endTime: '20:00:00',
+              flowId: '1',
+              startTime: '09:00:00',
             },
-            name: 'Glific',
-            signaturePhrase: 'Sample text',
-            contact: {
-              phone: 911111111111,
-            },
+            signaturePhrase: 'Please change me, NOW!',
           },
         },
       },
