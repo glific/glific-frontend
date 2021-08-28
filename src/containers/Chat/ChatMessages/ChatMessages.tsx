d75ab3051b6cff0f8404819a36ad8c1e56a90d50
Kurund Jalmi <kurund.jalmi@gmail.com>
2021-08-28 11:09:52 +0100

    lets also show date separator for last message if its greater than today

--- src/containers/Chat/ChatMessages/ChatMessages.tsx
+++ src/containers/Chat/ChatMessages/ChatMessages.tsx
@@ -575,10 +575,19 @@ export const ChatMessages: React.SFC<ChatMessagesProps> = ({ contactId, collecti
   };
 
   const showDaySeparator = (currentDate: string, nextDate: string) => {
-    // if it's first message and its date is greater than current date then show day separator
-    if (moment(currentDate).format('YYYY-MM-DD') > moment(nextDate).format('YYYY-MM-DD')) {
+    // if it's last message and its date is greater than current date then show day separator
+    if (!nextDate && moment(currentDate).format('YYYY-MM-DD') < moment().format('YYYY-MM-DD')) {
       return true;
     }
+
+    // if the day is changed then show day separator
+    if (
+      nextDate &&
+      moment(currentDate).format('YYYY-MM-DD') > moment(nextDate).format('YYYY-MM-DD')
+    ) {
+      return true;
+    }
+
     return false;
   };
 
@@ -613,14 +622,10 @@ export const ChatMessages: React.SFC<ChatMessagesProps> = ({ contactId, collecti
             : true
         }
         jumpToMessage={jumpToMessage}
-        daySeparator={
-          reverseConversation[index + 1]
-            ? showDaySeparator(
-                reverseConversation[index].insertedAt,
-                reverseConversation[index + 1].insertedAt
-              )
-            : false
-        }
+        daySeparator={showDaySeparator(
+          reverseConversation[index].insertedAt,
+          reverseConversation[index + 1] ? reverseConversation[index + 1].insertedAt : null
+        )}
       />
     ));
 
