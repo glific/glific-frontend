908d34df44ea92fe99f6521dd0f82bd85537b6ef
mdshamoon <mohdshamoon841@gmail.com>
2021-09-20 17:24:10 +0530

    fixed deepscan issue

--- src/containers/Profile/Contact/ContactProfile.test.tsx
+++ src/containers/Profile/Contact/ContactProfile.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { fireEvent, render, waitFor } from '@testing-library/react';
 import { MockedProvider } from '@apollo/client/testing';
 
 import { LOGGED_IN_USER_MOCK } from 'mocks/Contact';
