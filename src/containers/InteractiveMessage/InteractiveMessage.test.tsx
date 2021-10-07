ef2e01286ccbcd88342374fb3900fa036529ad22
mdshamoon <mohdshamoon841@gmail.com>
2021-10-07 15:26:02 +0530

    uncommented test cases

--- src/containers/InteractiveMessage/InteractiveMessage.test.tsx
+++ src/containers/InteractiveMessage/InteractiveMessage.test.tsx
@@ -46,128 +46,128 @@ const whenStable = async () => {
   });
 };
 
-// test('it renders empty interactive form', async () => {
-//   axiosApiCall();
+test('it renders empty interactive form', async () => {
+  axiosApiCall();
 
-//   render(
-//     <MockedProvider mocks={mockData} addTypename={false}>
-//       <Router>
-//         <InteractiveMessage match={{ params: {} }} />
-//       </Router>
-//     </MockedProvider>
-//   );
+  render(
+    <MockedProvider mocks={mockData} addTypename={false}>
+      <Router>
+        <InteractiveMessage match={{ params: {} }} />
+      </Router>
+    </MockedProvider>
+  );
 
-//   // Getting contact variables
-//   jest.spyOn(axios, 'get').mockResolvedValueOnce(responseMock1);
-//   await whenStable();
+  // Getting contact variables
+  jest.spyOn(axios, 'get').mockResolvedValueOnce(responseMock1);
+  await whenStable();
 
-//   // Adding another quick reply button
-//   await waitFor(() => {
-//     const addQuickReplyButton = screen.getByText('Add quick reply');
-//     expect(addQuickReplyButton).toBeInTheDocument();
-//     fireEvent.click(addQuickReplyButton);
-//   });
+  // Adding another quick reply button
+  await waitFor(() => {
+    const addQuickReplyButton = screen.getByText('Add quick reply');
+    expect(addQuickReplyButton).toBeInTheDocument();
+    fireEvent.click(addQuickReplyButton);
+  });
 
-//   await waitFor(() => {
-//     // Get all input elements
-//     const [title, quickReply1, quickReply2, , attachmentUrl] = screen.getAllByRole('textbox');
-//     expect(title).toBeInTheDocument();
-//     expect(quickReply1).toBeInTheDocument();
-//     expect(quickReply2).toBeInTheDocument();
-//     expect(attachmentUrl).toBeInTheDocument();
+  await waitFor(() => {
+    // Get all input elements
+    const [title, quickReply1, quickReply2, , attachmentUrl] = screen.getAllByRole('textbox');
+    expect(title).toBeInTheDocument();
+    expect(quickReply1).toBeInTheDocument();
+    expect(quickReply2).toBeInTheDocument();
+    expect(attachmentUrl).toBeInTheDocument();
 
-//     fireEvent.change(title, { target: { value: 'new title' } });
-//     fireEvent.blur(title);
-//     fireEvent.change(quickReply1, { target: { value: 'Yes' } });
-//     fireEvent.change(quickReply2, { target: { value: 'No' } });
-//     fireEvent.change(attachmentUrl, { target: { value: 'https://picsum.photos/200/300' } });
-//     fireEvent.blur(attachmentUrl);
-//   });
+    fireEvent.change(title, { target: { value: 'new title' } });
+    fireEvent.blur(title);
+    fireEvent.change(quickReply1, { target: { value: 'Yes' } });
+    fireEvent.change(quickReply2, { target: { value: 'No' } });
+    fireEvent.change(attachmentUrl, { target: { value: 'https://picsum.photos/200/300' } });
+    fireEvent.blur(attachmentUrl);
+  });
 
-//   // Changing language to marathi
-//   await waitFor(() => {
-//     const language = screen.getByText('Marathi');
-//     expect(language).toBeInTheDocument();
+  // Changing language to marathi
+  await waitFor(() => {
+    const language = screen.getByText('Marathi');
+    expect(language).toBeInTheDocument();
 
-//     fireEvent.click(language);
-//   });
+    fireEvent.click(language);
+  });
 
-//   await waitFor(() => {
-//     const [attachmentType] = screen.getAllByTestId('autocomplete-element');
+  await waitFor(() => {
+    const [attachmentType] = screen.getAllByTestId('autocomplete-element');
 
-//     expect(attachmentType).toBeInTheDocument();
+    expect(attachmentType).toBeInTheDocument();
 
-//     attachmentType.focus();
-//     fireEvent.keyDown(attachmentType, { key: 'ArrowDown' });
-//     fireEvent.keyDown(attachmentType, { key: 'Enter' });
-//   });
+    attachmentType.focus();
+    fireEvent.keyDown(attachmentType, { key: 'ArrowDown' });
+    fireEvent.keyDown(attachmentType, { key: 'Enter' });
+  });
 
-//   // Switiching between radio buttons
-//   const [quickReplyRadio, listRadio] = screen.getAllByRole('radio');
-//   await waitFor(() => {
-//     expect(quickReplyRadio).toBeInTheDocument();
-//     expect(listRadio).toBeInTheDocument();
-//     fireEvent.click(listRadio);
-//   });
+  // Switiching between radio buttons
+  const [quickReplyRadio, listRadio] = screen.getAllByRole('radio');
+  await waitFor(() => {
+    expect(quickReplyRadio).toBeInTheDocument();
+    expect(listRadio).toBeInTheDocument();
+    fireEvent.click(listRadio);
+  });
 
-//   await waitFor(() => {
-//     // Adding list data
-//     const [, header, listTitle, listItemTitle, listItemDesc] = screen.getAllByRole('textbox');
-//     expect(header).toBeInTheDocument();
-//     expect(listTitle).toBeInTheDocument();
-//     expect(listItemTitle).toBeInTheDocument();
-//     expect(listItemDesc).toBeInTheDocument();
+  await waitFor(() => {
+    // Adding list data
+    const [, header, listTitle, listItemTitle, listItemDesc] = screen.getAllByRole('textbox');
+    expect(header).toBeInTheDocument();
+    expect(listTitle).toBeInTheDocument();
+    expect(listItemTitle).toBeInTheDocument();
+    expect(listItemDesc).toBeInTheDocument();
 
-//     fireEvent.change(header, { target: { value: 'Section 1' } });
-//     fireEvent.blur(header);
-//     fireEvent.change(listTitle, { target: { value: 'title' } });
-//     fireEvent.change(listItemTitle, { target: { value: 'red' } });
-//     fireEvent.change(listItemDesc, { target: { value: 'red is color' } });
-//   });
+    fireEvent.change(header, { target: { value: 'Section 1' } });
+    fireEvent.blur(header);
+    fireEvent.change(listTitle, { target: { value: 'title' } });
+    fireEvent.change(listItemTitle, { target: { value: 'red' } });
+    fireEvent.change(listItemDesc, { target: { value: 'red is color' } });
+  });
 
-//   await waitFor(() => {
-//     // Adding another list item
-//     const addAnotherListItemButton = screen.getByText('Add another list item');
-//     expect(addAnotherListItemButton);
-//     fireEvent.click(addAnotherListItemButton);
-//   });
+  await waitFor(() => {
+    // Adding another list item
+    const addAnotherListItemButton = screen.getByText('Add another list item');
+    expect(addAnotherListItemButton);
+    fireEvent.click(addAnotherListItemButton);
+  });
 
-//   await waitFor(() => {
-//     // Adding another list
-//     const addAnotherListButton = screen.getByText('Add another list');
-//     expect(addAnotherListButton);
-//     fireEvent.click(addAnotherListButton);
-//   });
+  await waitFor(() => {
+    // Adding another list
+    const addAnotherListButton = screen.getByText('Add another list');
+    expect(addAnotherListButton);
+    fireEvent.click(addAnotherListButton);
+  });
 
-//   await waitFor(() => {
-//     // Deleting list
-//     const deleteListButton = screen.getByText('Dark.svg');
-//     expect(deleteListButton).toBeInTheDocument();
-//     fireEvent.click(deleteListButton);
-//   });
+  await waitFor(() => {
+    // Deleting list
+    const deleteListButton = screen.getByText('Dark.svg');
+    expect(deleteListButton).toBeInTheDocument();
+    fireEvent.click(deleteListButton);
+  });
 
-//   await waitFor(() => {
-//     // Deleting list item
-//     const deleteListItemButton = screen.getByText('Cross.svg');
-//     expect(deleteListItemButton).toBeInTheDocument();
-//     fireEvent.click(deleteListItemButton);
-//   });
+  await waitFor(() => {
+    // Deleting list item
+    const deleteListItemButton = screen.getByText('Cross.svg');
+    expect(deleteListItemButton).toBeInTheDocument();
+    fireEvent.click(deleteListItemButton);
+  });
 
-//   // Switching back to quick reply radio
-//   fireEvent.click(quickReplyRadio);
+  // Switching back to quick reply radio
+  fireEvent.click(quickReplyRadio);
 
-//   await waitFor(() => {
-//     const saveButton = screen.getByText('Save');
-//     expect(saveButton).toBeInTheDocument();
-//     fireEvent.click(saveButton);
-//   });
+  await waitFor(() => {
+    const saveButton = screen.getByText('Save');
+    expect(saveButton).toBeInTheDocument();
+    fireEvent.click(saveButton);
+  });
 
-//   // TODOS: need to fix below
-//   // // succesful save
-//   // await waitFor(() => {
-//   //   expect(screen.getByText('Interactive created successfully!')).toBeInTheDocument();
-//   // });
-// });
+  // TODOS: need to fix below
+  // // succesful save
+  // await waitFor(() => {
+  //   expect(screen.getByText('Interactive created successfully!')).toBeInTheDocument();
+  // });
+});
 
 test('it renders interactive quick reply in edit mode', async () => {
   axiosApiCall();
@@ -202,38 +202,38 @@ test('it renders interactive quick reply in edit mode', async () => {
   });
 });
 
-// test('it renders interactive list in edit mode', async () => {
-//   axiosApiCall();
+test('it renders interactive list in edit mode', async () => {
+  axiosApiCall();
 
-//   render(
-//     <MockedProvider mocks={mockData} addTypename={false}>
-//       <Router>
-//         <InteractiveMessage match={{ params: { id: '2' } }} />
-//       </Router>
-//     </MockedProvider>
-//   );
+  render(
+    <MockedProvider mocks={mockData} addTypename={false}>
+      <Router>
+        <InteractiveMessage match={{ params: { id: '2' } }} />
+      </Router>
+    </MockedProvider>
+  );
 
-//   jest.spyOn(axios, 'get').mockResolvedValueOnce(responseMock1);
-//   await whenStable();
+  jest.spyOn(axios, 'get').mockResolvedValueOnce(responseMock1);
+  await whenStable();
 
-//   await waitFor(() => {
-//     const saveButton = screen.getByText('Save');
-//     expect(saveButton).toBeInTheDocument();
-//     fireEvent.click(saveButton);
-//   });
-// });
+  await waitFor(() => {
+    const saveButton = screen.getByText('Save');
+    expect(saveButton).toBeInTheDocument();
+    fireEvent.click(saveButton);
+  });
+});
 
-// test('it renders interactive quick reply with media in edit mode', async () => {
-//   axiosApiCall();
+test('it renders interactive quick reply with media in edit mode', async () => {
+  axiosApiCall();
 
-//   render(
-//     <MockedProvider mocks={mockData} addTypename={false}>
-//       <Router>
-//         <InteractiveMessage match={{ params: { id: '3' } }} />
-//       </Router>
-//     </MockedProvider>
-//   );
+  render(
+    <MockedProvider mocks={mockData} addTypename={false}>
+      <Router>
+        <InteractiveMessage match={{ params: { id: '3' } }} />
+      </Router>
+    </MockedProvider>
+  );
 
-//   jest.spyOn(axios, 'get').mockResolvedValueOnce(responseMock3);
-//   await whenStable();
-// });
+  jest.spyOn(axios, 'get').mockResolvedValueOnce(responseMock3);
+  await whenStable();
+});
