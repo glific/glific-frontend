077a92a282352f3255f34090fdf86cd7def537e0
Kurund Jalmi <kurund.jalmi@gmail.com>
2021-10-19 22:11:40 +0100

    more fixes

--- src/components/UI/Layout/Loading/Loading.tsx
+++ src/components/UI/Layout/Loading/Loading.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { CircularProgress, Typography } from '@material-ui/core';
+import { useTranslation } from 'react-i18next';
 
 import styles from './Loading.module.css';
 
@@ -7,13 +8,18 @@ export interface LoadingProps {
   message?: string;
 }
 
-export const Loading: React.SFC<LoadingProps> = ({ message = 'Loading...' }) => (
-  <div className={styles.CenterItems} data-testid="loader">
-    <div className={styles.LoadingPadding}>
-      <CircularProgress />
+export const Loading: React.SFC<LoadingProps> = ({ message }) => {
+  const { t } = useTranslation();
+  const messageToDisplay = message || t('Loading...');
+
+  return (
+    <div className={styles.CenterItems} data-testid="loader">
+      <div className={styles.LoadingPadding}>
+        <CircularProgress />
+      </div>
+      <Typography variant="h5">{messageToDisplay}</Typography>
     </div>
-    <Typography variant="h5">{message}</Typography>
-  </div>
-);
+  );
+};
 
 export default Loading;
