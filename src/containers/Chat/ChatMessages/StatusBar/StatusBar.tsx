288a125c45cd8913194a84bc7b89e5f1649c55f3
Kurund Jalmi <kurund.jalmi@gmail.com>
2022-02-17 22:43:14 +0000

    correct css and add messages

--- src/containers/Chat/ChatMessages/StatusBar/StatusBar.tsx
+++ src/containers/Chat/ChatMessages/StatusBar/StatusBar.tsx
@@ -5,10 +5,31 @@ import styles from './StatusBar.module.css';
 export interface StatusBarProps {}
 
 export const StatusBar: React.FC<StatusBarProps> = () => {
-  const showStatusMessage = true;
+  const walletBalance = 0.99;
 
-  if (showStatusMessage) {
-    return <div className={styles.statusMessage}>Status message goes here</div>;
+  let statusMessage;
+  if (walletBalance < 1 && walletBalance > 0) {
+    statusMessage =
+      'Please recharge your Gupshup wallet immediately to continue sending messages. Users will not receive the messages that get stuck during this time.';
+  } else if (walletBalance <= 0) {
+    statusMessage =
+      'All the outgoing messages have been suspended. Please note: on recharging, the messages that were stuck will not be sent.';
+  }
+
+  const limitReached = false;
+
+  if (limitReached) {
+    statusMessage =
+      'You have reached today’s rate limit for sending HSM templates to your users. Your rate limit will be refreshed tomorrow. Please check again later.';
+  }
+
+  if (statusMessage) {
+    return (
+      <div className={styles.StatusMessage}>
+        <span className={styles.StatusTitle}>Attention! </span>
+        {statusMessage}
+      </div>
+    );
   }
 
   return null;
