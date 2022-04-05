c52589ad36abcdef88ebb67ce31143053adbffa0
mdshamoon <mohdshamoon841@gmail.com>
2022-04-05 14:08:25 +0530

    fixed interactive document issue

--- src/containers/InteractiveMessage/InteractiveMessage.helper.ts
+++ src/containers/InteractiveMessage/InteractiveMessage.helper.ts
@@ -11,8 +11,15 @@ export const validator = (templateType: any, t: any) => {
       .required(t('Title is required'))
       .max(60, t('Title can be at most 60 characters')),
     body: Yup.string()
-      .transform((current, original) => original.getCurrentContent().getPlainText())
-      .required(t('Message content is required.')),
+      .transform((_current, original) => original.getCurrentContent().getPlainText())
+      .when('type', {
+        is: (val: any) => {
+          console.log('s');
+          return val && val.id && val.id === 'DOCUMENT';
+        },
+        then: Yup.string().nullable(),
+        otherwise: Yup.string().required(t('Message content is required.')),
+      }),
   };
 
   if (templateType === LIST) {
@@ -87,6 +94,8 @@ export const convertJSONtoStateData = (JSONData: any, interactiveType: string, l
       case 'file':
         result.type = 'DOCUMENT';
         result.attachmentURL = url;
+        result.body = '';
+        result.title = label;
         break;
       default:
         result.type = null;
