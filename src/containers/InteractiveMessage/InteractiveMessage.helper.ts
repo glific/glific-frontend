80b6384344e232b4c7738c92898d17617cc58be1
Kurund Jalmi <kurund.jalmi@gmail.com>
2022-04-19 23:03:33 +0100

    fixed string localization

--- src/containers/InteractiveMessage/InteractiveMessage.helper.ts
+++ src/containers/InteractiveMessage/InteractiveMessage.helper.ts
@@ -57,7 +57,7 @@ export const validator = (templateType: any, t: any) => {
           options: Yup.array().of(
             Yup.object().shape({
               title: Yup.string()
-                .test('is-emoji', 'Sorry! Emojis are not allowed in title', (value) => {
+                .test('is-emoji', t('Sorry! Emojis are not allowed in the title'), (value) => {
                   if (value) {
                     const testEmoji =
                       /(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/gi.test(
