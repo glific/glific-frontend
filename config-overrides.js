e8a1974ec2cc7cf2ee08dd57e44101326a2e66ff
Kurund Jalmi <kurund.jalmi@gmail.com>
2022-05-15 21:51:57 +0100

    more fixes

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,16 @@
+const webpack = require('webpack');
 module.exports = function override(config, env) {
   config.resolve.fallback = Object.assign(config.resolve.fallback || {}, {
+    buffer: require.resolve('buffer'),
     stream: require.resolve('stream-browserify'),
-    process: require.resolve('process/browser'),
   });
+
+  config.plugins.push(
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+      process: 'process/browser',
+    })
+  );
 
   config.module.rules = [
     ...config.module.rules,
