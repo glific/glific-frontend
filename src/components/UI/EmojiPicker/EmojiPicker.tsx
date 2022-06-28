7269f3332c3e7039b60ea6568ed98f5a44ca30b3
Kurund Jalmi <kurund.jalmi@gmail.com>
2022-06-28 22:44:30 +0100

    fixes for duplicate emoji picker

--- src/components/UI/EmojiPicker/EmojiPicker.tsx
+++ src/components/UI/EmojiPicker/EmojiPicker.tsx
@@ -4,10 +4,16 @@ import { Picker } from 'emoji-mart';
 
 export const EmojiPicker = (props: any) => {
   const ref = useRef<HTMLDivElement>(null);
+  const executedRef = useRef(false);
+
   const { displayStyle } = props;
   useEffect(() => {
+    // this is workaround to fix issue with duplicate emoji picker
+    // https://github.com/missive/emoji-mart/issues/617
+    if (executedRef.current) return;
     // eslint-disable-next-line no-new
     new Picker({ ...props, data, ref });
+    executedRef.current = true;
   }, []);
 
   return <div data-testid="emoji-container" ref={ref} style={displayStyle} />;
