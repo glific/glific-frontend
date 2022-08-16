d29250337b6fdd8cfc95698b84bad3050de2dee9
Kurund Jalmi <kurund.jalmi@gmail.com>
2022-08-16 23:26:51 +0100

    code cleanup

--- src/components/UI/EmojiPicker/EmojiPicker.tsx
+++ src/components/UI/EmojiPicker/EmojiPicker.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import data from '@emoji-mart/data';
 import Picker from '@emoji-mart/react';
 
-export const EmojiPicker = (props: any) => {
-  const { displayStyle, onEmojiSelect } = props;
-  return (
-    <div data-testid="emoji-container" style={displayStyle}>
-      <Picker data={data} onEmojiSelect={onEmojiSelect} />
-    </div>
-  );
-};
+export interface EmojiPickerProps {
+  displayStyle: object;
+  onEmojiSelect: Function;
+}
+
+export const EmojiPicker = ({ displayStyle, onEmojiSelect }: EmojiPickerProps) => (
+  <div data-testid="emoji-container" style={displayStyle}>
+    <Picker data={data} onEmojiSelect={onEmojiSelect} />
+  </div>
+);
 
 export default EmojiPicker;
