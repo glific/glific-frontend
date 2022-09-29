e43c922f2b351bd351983b878e27f0a8858e39e7
mdshamoon <mohdshamoon841@gmail.com>
2022-09-29 12:06:58 +0530

    fixed deepscan issues

--- src/containers/ContactManagement/ContactManagement.test.tsx
+++ src/containers/ContactManagement/ContactManagement.test.tsx
@@ -1,4 +1,4 @@
-import { render, cleanup, act, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { MockedProvider } from '@apollo/client/testing';
 
 import { BrowserRouter as Router } from 'react-router-dom';
