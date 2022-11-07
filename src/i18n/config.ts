4cd47a2d7da2814dfe9debd85203fae8c00d11a6
Kurund Jalmi <kurund.jalmi@gmail.com>
2022-11-07 23:56:57 +0000

    i18n implementation fixes

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,9 +1,11 @@
-import i18n from 'i18next';
+import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
 import en from './en/en.json';
 import hi from './hi/hi.json';
+
+export const defaultNS = 'en';
 
 const resources = {
   en: {
@@ -14,9 +16,8 @@ const resources = {
   },
 };
 
-i18n.use(LanguageDetector).use(initReactI18next).init({
+i18next.use(LanguageDetector).use(initReactI18next).init({
   resources,
+  defaultNS,
   fallbackLng: 'en',
 });
-
-export default i18n;
