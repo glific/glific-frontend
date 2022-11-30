6c43c76eb91b4b30d4bcc1c002674e4cf55d4912
mdshamoon <mohdshamoon841@gmail.com>
2022-11-30 19:35:11 +0530

    fixed deepscan issue

--- src/containers/Auth/Registration/Registration.test.tsx
+++ src/containers/Auth/Registration/Registration.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
 import axios from 'axios';
