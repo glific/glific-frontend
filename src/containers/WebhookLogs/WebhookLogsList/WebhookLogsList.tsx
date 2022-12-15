4828844b5a906650f7ad8f619af894b3d362a754
Kurund Jalmi <kurund.jalmi@gmail.com>
2022-12-15 23:52:22 +0000

    webhook log listing

--- src/containers/WebhookLogs/WebhookLogsList/WebhookLogsList.tsx
+++ src/containers/WebhookLogs/WebhookLogsList/WebhookLogsList.tsx
@@ -43,18 +43,6 @@ const getStatus = (status: string) => {
 
 const getText = (text: string) => <div className={styles.TableText}>{text}</div>;
 
-const columnNames = [
-  'TIME',
-  'URL',
-  'STATUS',
-  'STATUS CODE',
-  'ERROR',
-  'METHOD',
-  'REQUEST HEADER',
-  'REQUEST JSON',
-  'RESPONSE JSON',
-];
-
 const columnStyles = [
   styles.Time,
   styles.Url,
@@ -81,6 +69,18 @@ export const WebhookLogsList = () => {
   const [open, setOpen] = useState(false);
   const [text, setText] = useState<any>();
   const { t } = useTranslation();
+
+  const columnNames = [
+    { name: 'updated_at', label: t('Time') },
+    { name: 'url', label: t('URL') },
+    { name: 'status', label: t('Status') },
+    { name: 'status_code', label: t('Status Code') },
+    { name: 'error', label: t('Error') },
+    { name: 'method', label: t('Method') },
+    { name: 'request_headers', label: t('Request header') },
+    { name: 'request_json', label: t('Request JSON') },
+    { name: 'response_json', label: t('Response JSON') },
+  ];
 
   const handleClick = (event: any) => {
     setAnchorEl(event.currentTarget);
