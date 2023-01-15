76d105a067d7ba89a37130a1159d6c1081aa4821
Kurund Jalmi <kurund.jalmi@gmail.com>
2023-01-15 00:18:54 +0000

    load local dev with https certificate

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,33 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
+import fs from 'fs';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
-  build: {
-    outDir: 'build',
-  },
-  server: {
-    open: true,
-  },
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  if (command === 'serve') {
+    return {
+      // dev specific config
+      plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+      build: {
+        outDir: 'build',
+      },
+      server: {
+        open: true,
+        https: {
+          key: fs.readFileSync('../glific/priv/cert/glific.test+1-key.pem'),
+          cert: fs.readFileSync('../glific/priv/cert/glific.test+1.pem'),
+        },
+      },
+    };
+  } else {
+    // command === 'build'
+    return {
+      // build specific config
+      plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+      build: {
+        outDir: 'build',
+      },
+    };
+  }
 });
