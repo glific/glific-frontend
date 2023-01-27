f544536c6e257717957680661cd4332f8916e74f
Kurund Jalmi <kurund.jalmi@gmail.com>
2023-01-27 22:56:11 +0000

    more fixes

--- src/global.ts
+++ src/global.ts
@@ -1,4 +1,5 @@
-import { Buffer } from 'buffer'; //or 'buffer/', with trailing slash
+import { Buffer } from 'buffer';
 import Process from 'process';
 globalThis.process = Process;
 globalThis.Buffer = Buffer;
+document.title = import.meta.env.VITE_APPLICATION_NAME;
