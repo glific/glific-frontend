93211d7eff9d12455e2fd081b1151b7c54b74282
mdshamoon <mohdshamoon841@gmail.com>
2023-02-14 12:20:33 +0530

    added warnings for sheet

--- src/containers/SheetIntegration/SheetIntegrationList/SheetIntegrationList.tsx
+++ src/containers/SheetIntegration/SheetIntegrationList/SheetIntegrationList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { ReactComponent as SheetIcon } from 'assets/images/icons/Sheets/Sheet.svg';
 import { ReactComponent as UpdatesheetIcon } from 'assets/images/icons/Sheets/Updatesheet.svg';
@@ -10,7 +10,7 @@ import { useMutation } from '@apollo/client';
 import { List } from 'containers/List/List';
 import moment from 'moment';
 import { DATE_TIME_FORMAT } from 'common/constants';
-
+import { DialogBox } from 'components/UI/DialogBox/DialogBox';
 import styles from './SheetIntegrationList.module.css';
 
 const getName = (text: string, sheetDataCount: string) => (
@@ -38,10 +38,48 @@ const queries = {
 export const SheetIntegrationList = () => {
   const { t } = useTranslation();
 
+  const [warnings, setWarnings] = useState<any>({});
+  const [showdialog, setShowDialog] = useState(false);
+
+  let dialog;
+
+  if (showdialog) {
+    const warningKeys = Object.keys(warnings);
+    dialog = (
+      <DialogBox
+        open
+        title="Please check the warnings"
+        skipCancel
+        alignButtons="center"
+        buttonOk="Close"
+        colorOk="secondary"
+        handleOk={() => setShowDialog(false)}
+      >
+        {warningKeys.map((key, index) => (
+          <div className={styles.DialogContent}>
+            <strong>
+              {index + 1}. {key}:
+            </strong>{' '}
+            {warnings[key]}
+          </div>
+        ))}
+      </DialogBox>
+    );
+  }
+
   const [syncSheetMutation] = useMutation(SYNC_SHEET, {
     fetchPolicy: 'network-only',
-    onCompleted: async () => {
-      setNotification('Data is successfully fetched from the Google sheet.');
+    onCompleted: async ({ syncSheet }) => {
+      if (syncSheet.sheet && syncSheet.sheet.warnings) {
+        const sheetWarnings = JSON.parse(syncSheet.sheet.warnings);
+
+        if (Object.keys(sheetWarnings).length) {
+          setShowDialog(true);
+          setWarnings(sheetWarnings);
+        }
+      } else {
+        setNotification('Data is successfully fetched from the Google sheet.');
+      }
     },
   });
 
@@ -87,18 +125,21 @@ export const SheetIntegrationList = () => {
   };
 
   return (
-    <List
-      title={t('Google sheets')}
-      listItem="sheets"
-      listItemName="Sheet"
-      pageLink="Sheet-integration"
-      listIcon={sheetIcon}
-      dialogMessage={dialogMessage}
-      {...queries}
-      {...columnAttributes}
-      additionalAction={additionalAction}
-      button={{ show: true, label: t('Add Sheet'), symbol: '+' }}
-    />
+    <>
+      {dialog}
+      <List
+        title={t('Google sheets')}
+        listItem="sheets"
+        listItemName="Sheet"
+        pageLink="Sheet-integration"
+        listIcon={sheetIcon}
+        dialogMessage={dialogMessage}
+        {...queries}
+        {...columnAttributes}
+        additionalAction={additionalAction}
+        button={{ show: true, label: t('Add Sheet'), symbol: '+' }}
+      />
+    </>
   );
 };
 
