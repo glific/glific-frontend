f5c2927f9c6ca8b6072d3e0055b69962ce2f269f
Kurund Jalmi <kurund.jalmi@gmail.com>
2023-02-14 09:11:23 +0000

    type definition fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite';
+import { defineConfig, ConfigEnv, UserConfigExport } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
@@ -7,9 +7,9 @@ import fs from 'fs';
 import inject from '@rollup/plugin-inject';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode, ssrBuild }) => {
+export default ({ command }: ConfigEnv): UserConfigExport => {
   if (command === 'serve') {
-    return {
+    return defineConfig({
       // dev specific config
       plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
       build: {
@@ -39,10 +39,10 @@ export default defineConfig(({ command, mode, ssrBuild }) => {
           exclude: ['node_modules/', 'src/setupTests.ts'],
         },
       },
-    };
+    });
   } else {
     // command === 'build'
-    return {
+    return defineConfig({
       // build specific config
       plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
       build: {
@@ -55,6 +55,6 @@ export default defineConfig(({ command, mode, ssrBuild }) => {
         global: {},
       },
       resolve: { alias: { util: 'util/' } },
-    };
+    });
   }
-});
+};
