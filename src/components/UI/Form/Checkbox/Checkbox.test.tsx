8d58af70a21ab2b97e461974461810bb6d64b1ad
mdshamoon <mohdshamoon841@gmail.com>
2023-02-18 17:40:15 +0530

    cleaned up code

--- src/components/UI/Form/Checkbox/Checkbox.test.tsx
+++ src/components/UI/Form/Checkbox/Checkbox.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 
 import { Checkbox } from './Checkbox';
 
