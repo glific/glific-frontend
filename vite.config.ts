071d7d4d365ef8b867de829a07e21356844054ba
mdshamoon <mohdshamoon841@gmail.com>
2023-02-21 18:51:34 +0530

    update test changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 /// <reference types="vitest" />
 /// <reference types="vite-plugin-svgr/client" />
-import { defineConfig, ConfigEnv, UserConfigExport } from 'vite';
+import { defineConfig, ConfigEnv, UserConfigExport, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
@@ -8,7 +8,34 @@ import fs from 'fs';
 import inject from '@rollup/plugin-inject';
 
 // https://vitejs.dev/config/
-export default ({ command }: ConfigEnv): UserConfigExport => {
+export default ({ command, mode }: ConfigEnv): UserConfigExport => {
+  if (mode === 'test' && command === 'serve') {
+    return defineConfig({
+      // dev specific config
+      plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+
+      optimizeDeps: {
+        esbuildOptions: {
+          // Node.js global to browser globalThis
+          define: {
+            global: 'globalThis',
+          },
+        },
+      },
+
+      resolve: { alias: { util: 'util/' } },
+      test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/setupTests.ts',
+        coverage: {
+          reporter: ['text', 'html'],
+          exclude: ['node_modules/'],
+        },
+        css: true,
+      },
+    });
+  }
   if (command === 'serve') {
     return defineConfig({
       // dev specific config
@@ -36,16 +63,6 @@ export default ({ command }: ConfigEnv): UserConfigExport => {
         },
       },
       resolve: { alias: { util: 'util/' } },
-      test: {
-        globals: true,
-        environment: 'jsdom',
-        setupFiles: './src/setupTests.ts',
-        coverage: {
-          reporter: ['text', 'html'],
-          exclude: ['node_modules/'],
-        },
-        css: true,
-      },
     });
   } else {
     // command === 'build'
@@ -65,5 +82,3 @@ export default ({ command }: ConfigEnv): UserConfigExport => {
     });
   }
 };
-
-console.log('sq1');
