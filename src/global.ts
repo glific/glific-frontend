de78a4a4cdcaba57f0b3a2d263b830899dc0a1ee
Kurund Jalmi <kurund.jalmi@gmail.com>
2023-02-28 00:04:01 +0000

    eslint fixes

--- src/global.ts
+++ src/global.ts
@@ -1,5 +1,6 @@
 import { Buffer } from 'buffer';
 import Process from 'process';
+
 globalThis.process = Process;
 globalThis.Buffer = Buffer;
 document.title = import.meta.env.VITE_APPLICATION_NAME;
