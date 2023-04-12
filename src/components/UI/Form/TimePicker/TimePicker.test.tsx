49525cf48799d15f1fd7d682b250fe595982063f
mdshamoon <mohdshamoon841@gmail.com>
2023-04-12 05:35:20 +0530

    deepscan fixes

--- src/components/UI/Form/TimePicker/TimePicker.test.tsx
+++ src/components/UI/Form/TimePicker/TimePicker.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import UserEvent from '@testing-library/user-event';
 
 import { TimePicker } from './TimePicker';
