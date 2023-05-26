f8d3ef1d5e3f8897f627091ab8dfb5f5985a9bc7
mdshamoon <mohdshamoon841@gmail.com>
2023-05-26 14:41:23 +0530

    added complete ticketing system

---
+++ src/containers/Ticket/Ticket.test.tsx
@@ -0,0 +1,117 @@
+import { fireEvent, render, waitFor, cleanup, screen, within } from '@testing-library/react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { MockedProvider } from '@apollo/client/testing';
+
+import {
+  getConsultingHour,
+  getOrganizationList,
+  createConsultingHour,
+  updateConsultingHour,
+} from 'mocks/Consulting';
+import { setUserSession } from 'services/AuthService';
+import { Ticket } from './Ticket';
+import userEvent from '@testing-library/user-event';
+
+afterEach(cleanup);
+const setOpenDialogMock = vi.fn();
+setUserSession(JSON.stringify({ organization: { id: '1' }, roles: ['Admin'] }));
+
+// const mocks = [getOrganizationList, getConsultingHour, createConsultingHour, updateConsultingHour];
+// const wrapper = (
+//   <MockedProvider mocks={mocks} addTypename={false}>
+//     <Router>
+//       <Routes>
+//         <Route
+//           path="/"
+//           element={<Consulting organizationId="" setOpenDialog={setOpenDialogMock} />}
+//         />
+//         <Route path="consulting-hours" element={<div />} />
+//       </Routes>
+//     </Router>
+//   </MockedProvider>
+// );
+
+// test('Render component correctly with empty form', async () => {
+//   const user = userEvent.setup();
+//   render(wrapper);
+
+//   expect(screen.getByText('Loading...')).toBeInTheDocument();
+
+//   await waitFor(() => {
+//     expect(screen.getByText('Add consulting record')).toBeInTheDocument();
+//   });
+
+//   // Get all input elements
+//   const inputElements = screen.getAllByRole('textbox');
+//   // Get all radio buttons
+//   const radioButtons = screen.getAllByRole('radio');
+
+//   // For selecting organization from dropdown
+//   const autoComplete = screen.getByTestId('autocomplete-element');
+//   const input = within(autoComplete).getByRole('combobox');
+//   autoComplete.focus();
+//   fireEvent.change(input, { target: { value: 'G' } });
+
+//   await waitFor(() => {
+//     const selectedOption = screen.getByText('Glific');
+
+//     fireEvent.click(selectedOption);
+//   });
+
+//   await waitFor(() => {
+//     fireEvent.change(inputElements[0], { target: { value: 'Glific' } });
+//     fireEvent.change(inputElements[1], { target: { value: 'John Doe' } });
+//     fireEvent.change(inputElements[3], { target: { value: 15 } });
+//     fireEvent.change(inputElements[4], { target: { value: 'Glific Tean' } });
+//     fireEvent.click(radioButtons[0]);
+//   });
+
+//   await waitFor(() => {
+//     const submitButton = screen.getByText('Save');
+//     expect(submitButton).toBeInTheDocument();
+//     fireEvent.click(submitButton);
+//   });
+// });
+
+// const consultingEditForm = (
+//   <MockedProvider mocks={mocks} addTypename={false}>
+//     <Router>
+//       <Routes>
+//         <Route
+//           path="/"
+//           element={<Consulting organizationId="1" setOpenDialog={setOpenDialogMock} />}
+//         />
+//         <Route path="consulting-hours" element={<div />} />
+//       </Routes>
+//     </Router>
+//   </MockedProvider>
+// );
+
+// test('it renders consulting hours in edit mode', async () => {
+//   const { getByText, getByTestId } = render(consultingEditForm);
+
+//   expect(getByText('Loading...')).toBeInTheDocument();
+//   await waitFor(() => {
+//     expect(getByTestId('AutocompleteInput').querySelector('input')?.value).toBe('Glific');
+//   });
+// });
+
+// test('it renders consulting hours in edit mode', async () => {
+//   const { getByText, getByTestId } = render(consultingEditForm);
+
+//   expect(getByText('Loading...')).toBeInTheDocument();
+//   await waitFor(() => {
+//     expect(getByTestId('AutocompleteInput').querySelector('input')?.value).toBe('Glific');
+//   });
+// });
+
+// test('Click in cancel button', async () => {
+//   const { getByText } = render(consultingEditForm);
+
+//   expect(getByText('Loading...')).toBeInTheDocument();
+//   await waitFor(() => {
+//     expect(getByText('Cancel')).toBeInTheDocument();
+//     fireEvent.click(getByText('Cancel'));
+//     expect(setOpenDialogMock).toHaveBeenCalledWith(false);
+//   });
+// });
--- src/containers/TicketList/TicketList.tsx
+++ src/containers/TicketList/TicketList.tsx
@@ -1,31 +1,45 @@
 import { useTranslation } from 'react-i18next';
-import { ReactComponent as TriggerIcon } from 'assets/images/icons/Trigger/Union.svg';
 import { DELETE_TRIGGER } from 'graphql/mutations/Trigger';
 import { List } from 'containers/List/List';
 import styles from './TicketList.module.css';
 import { ReactComponent as EditIcon } from 'assets/images/icons/Edit.svg';
 import { TICKET_COUNT_QUERY, TICKET_LIST_QUERY } from 'graphql/queries/Ticket';
+import { useState } from 'react';
+import { Dialog, DialogContent, FormControlLabel, Radio, RadioGroup } from '@mui/material';
+import Ticket from 'containers/Ticket/Ticket';
+import { SupportAgent } from '@mui/icons-material';
+import { Dropdown } from 'components/UI/Form/Dropdown/Dropdown';
+import { getUserSession } from 'services/AuthService';
 
-const getBody = (body: any) => <div className={styles.Collection}>{body}</div>;
-const getStatus = (status: any) => <div className={styles.Collection}>{status}</div>;
-const getTopic = (topic: any) => <div className={styles.Collection}>{topic}</div>;
-const getUser = (user: any) => <div className={styles.Collection}>{user?.name}</div>;
+const getBody = (body: any) => <div className={styles.TableText}>{body}</div>;
+const getStatus = (status: string) => {
+  let showStatus;
+  switch (status) {
+    case 'open':
+      showStatus = <div className={styles.Success}>{status}</div>;
+      break;
+    case 'closed':
+      showStatus = <div className={styles.ErrorStyle}>{status}</div>;
+      break;
+    default:
+      showStatus = status;
+  }
 
-const getColumns = ({ body, remarks, status, topic, user }: any) => ({
+  return <div className={styles.StatusContainer}>{showStatus}</div>;
+};
+
+const getLabel = (label: any) => <div className={styles.LabelText}>{label}</div>;
+const getUser = (user: any) => <div className={styles.TableText}>{user?.name}</div>;
+
+const getColumns = ({ body, status, topic, user }: any) => ({
+  label: getLabel(topic),
   body: getBody(body),
-  topic: getTopic(topic),
   status: getStatus(status),
   user: getUser(user),
 });
 
-const columnStyles = [
-  styles.Name,
-  styles.EndDate,
-  styles.Collections,
-  styles.Collections,
-  styles.Actions,
-];
-const triggerIcon = <TriggerIcon className={styles.Icon} />;
+const columnStyles = [styles.Label, styles.Issue, styles.Status, styles.Assigned, styles.Actions];
+const ticketIcon = <SupportAgent className={styles.Icon} />;
 
 const queries = {
   countQuery: TICKET_COUNT_QUERY,
@@ -33,12 +47,41 @@ const queries = {
   deleteItemQuery: DELETE_TRIGGER,
 };
 
+const filterList = [
+  { label: 'All', value: false },
+  { label: 'My tickets', value: true },
+];
+
 export const TicketList = () => {
   const { t } = useTranslation();
 
+  const userId = getUserSession('id');
+
+  const [openDialog, setOpenDialog] = useState(false);
+  const [selectedTicket, setSelectedTicket] = useState('');
+  const [assignedToUser, setAssignedToUser] = useState<any>(false);
+  const [status, setStatus] = useState<any>('open');
+
+  let dialog;
+
+  if (openDialog) {
+    dialog = (
+      <Dialog
+        open
+        classes={{
+          paper: styles.Dialogbox,
+        }}
+      >
+        <DialogContent classes={{ root: styles.DialogContent }}>
+          <Ticket selectedTicket={selectedTicket} setOpenDialog={setOpenDialog}></Ticket>
+        </DialogContent>
+      </Dialog>
+    );
+  }
+
   const columnNames: any = [
-    { name: 'body', label: t('Issue') },
     { name: 'topic', label: t('Label') },
+    { name: 'body', label: t('Issue') },
     { label: t('Status') },
     { label: t('Assigned to') },
     { label: t('Actions') },
@@ -58,25 +101,77 @@ export const TicketList = () => {
     {
       icon: <EditIcon />,
       parameter: 'id',
-      dialog: () => {},
+      dialog: (id: any) => {
+        setSelectedTicket(id);
+        setOpenDialog(true);
+      },
       label: t('Add remark'),
     },
   ];
 
+  console.log();
+
+  const activeFilter = (
+    <div className={styles.Filters}>
+      <RadioGroup
+        aria-label="ticket-type"
+        name="ticket-type"
+        row
+        value={assignedToUser}
+        onChange={(event) => {
+          const { value } = event.target;
+          setAssignedToUser(JSON.parse(value));
+        }}
+      >
+        {filterList.map((filter) => (
+          <div className={styles.RadioLabelWrapper} key={filter.label}>
+            <FormControlLabel
+              value={filter.value}
+              control={<Radio color="primary" />}
+              classes={{ root: styles.RadioLabel }}
+              label={filter.label}
+              data-testid="radio"
+            />
+          </div>
+        ))}
+      </RadioGroup>
+      <div className={styles.DropdownContainer}>
+        <Dropdown
+          options={[
+            { id: 'open', label: 'Open' },
+            { id: 'closed', label: 'Closed' },
+          ]}
+          placeholder="Status"
+          field={{ value: status, onChange: (event: any) => setStatus(event.target.value) }}
+        ></Dropdown>
+      </div>
+    </div>
+  );
+
+  const filter: any = { status };
+
+  if (assignedToUser) {
+    filter.userId = userId;
+  }
+
   return (
-    <List
-      restrictedAction={getRestrictedAction}
-      title="Tickets"
-      listItem="tickets"
-      listItemName="ticket"
-      pageLink="ticket"
-      button={{ show: false }}
-      listIcon={triggerIcon}
-      // dialogMessage={dialogMessage}
-      {...queries}
-      {...columnAttributes}
-      additionalAction={additionalAction}
-    />
+    <>
+      {dialog}
+      <List
+        restrictedAction={getRestrictedAction}
+        title="Tickets"
+        listItem="tickets"
+        listItemName="ticket"
+        pageLink="ticket"
+        button={{ show: false }}
+        listIcon={ticketIcon}
+        {...queries}
+        {...columnAttributes}
+        additionalAction={additionalAction}
+        filters={filter}
+        filterList={activeFilter}
+      />
+    </>
   );
 };
 
