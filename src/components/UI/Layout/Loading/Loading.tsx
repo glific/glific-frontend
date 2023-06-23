9bb97182edcf6ce371ee18a4fdf9633c70214470
nishant25062002 <mainwalnishant9@gmail.com>
2023-06-23 13:13:14 +0530

    Resolved DeepScan issues

--- src/components/UI/Layout/Loading/Loading.tsx
+++ src/components/UI/Layout/Loading/Loading.tsx
@@ -1,4 +1,4 @@
-import { CircularProgress, Typography } from '@mui/material';
+import { Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 
 import styles from './Loading.module.css';
