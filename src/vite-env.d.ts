f4642e071bbb64796ed46baee915261160d522c6
mdshamoon <mohdshamoon841@gmail.com>
2023-07-05 12:54:52 +0530

    package upgrades

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,4 @@
 /// <reference types="vite/client" />
+/// <reference types="@testing-library/jest-dom" />
+/// <reference types="vite-plugin-svgr/client" />
+/// <reference types="vitest/globals" />
