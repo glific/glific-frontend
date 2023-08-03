1878f66abeecd8b0b032f76d9ecac84304966fde
mdshamoon <mohdshamoon841@gmail.com>
2023-08-03 12:13:23 +0530

    url intact login

--- src/routes/UnauthenticatedRoute/UnauthenticatedRoute.tsx
+++ src/routes/UnauthenticatedRoute/UnauthenticatedRoute.tsx
@@ -1,11 +1,13 @@
 import { lazy, Suspense } from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
 
 import Loading from 'components/UI/Layout/Loading/Loading';
 import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 import { RECAPTCHA_CLIENT_KEY } from 'config';
 
 export const UnauthenticatedRoute = () => {
+  const location = useLocation();
+
   const Login = lazy(() => import('containers/Auth/Login/Login'));
   const Registration = lazy(() => import('containers/Auth/Registration/Registration'));
   const ConfirmOTP = lazy(() => import('containers/Auth/ConfirmOTP/ConfirmOTP'));
@@ -16,17 +18,20 @@ export const UnauthenticatedRoute = () => {
   const OrganizationRegistration = lazy(() => import('containers/Organization/RouteSetupSteps'));
 
   return (
-    <Suspense fallback={<Loading/>}>
+    <Suspense fallback={<Loading />}>
       <GoogleReCaptchaProvider reCaptchaKey={RECAPTCHA_CLIENT_KEY}>
         <Routes>
-          <Route index element={<Navigate to="/logout/user" replace />} />
+          <Route index element={<Navigate to="/logout/user" replace state={location.pathname} />} />
           <Route path="/login" element={<Login />} />
           <Route path="/registration" element={<Registration />} />
           <Route path="/confirmotp" element={<ConfirmOTP />} />
           <Route path="/resetpassword-phone" element={<ResetPasswordPhone />} />
           <Route path="/resetpassword-confirmotp" element={<ResetPasswordConfirmOTP />} />
           <Route path="/organization-registration" element={<OrganizationRegistration />} />
-          <Route path="/*" element={<Navigate to="/logout/user" replace />} />
+          <Route
+            path="/*"
+            element={<Navigate to="/logout/user" replace state={location.pathname} />}
+          />
         </Routes>
       </GoogleReCaptchaProvider>
     </Suspense>
