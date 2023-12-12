72a3cdedd336059ec7ad3b0d1b29b6366eecf960
Kurund Jalmi <kurund.jalmi@gmail.com>
2023-12-12 00:25:31 +0000

    last login, more fixes

--- src/containers/LastLogin/LastLogin.tsx
+++ src/containers/LastLogin/LastLogin.tsx
@@ -2,13 +2,11 @@ import moment from 'moment';
 import { useTranslation } from 'react-i18next';
 import UserIcon from 'assets/images/icons/UserIcon.svg?react';
 import { getAuthSession, getUserSession } from 'services/AuthService';
-import { DATE_TIME_FORMAT, GUPSHUP_ENTERPRISE_SHORTCODE } from 'common/constants';
+import { DATE_TIME_FORMAT } from 'common/constants';
 
 import styles from './LastLogin.module.css';
 import { Divider, Typography } from '@mui/material';
 import { useNavigate } from 'react-router';
-import { useContext } from 'react';
-import { ProviderContext } from 'context/session';
 
 interface LastLoginProps {
   drawerOpen: boolean;
