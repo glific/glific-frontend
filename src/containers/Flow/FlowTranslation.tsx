dd771cff4785e6ff4944ececf366ff0fe35217a4
Kurund Jalmi <kurund.jalmi@gmail.com>
2023-12-26 22:37:59 +0000

    more work on UI and add translation strings

--- src/containers/Flow/FlowTranslation.tsx
+++ src/containers/Flow/FlowTranslation.tsx
@@ -1,5 +1,8 @@
+import { useState } from 'react';
 import { DialogBox } from 'components/UI/DialogBox/DialogBox';
 import styles from './FlowTranslation.module.css';
+import { FormControl, FormLabel, RadioGroup, FormControlLabel, Radio } from '@mui/material';
+import { useTranslation } from 'react-i18next';
 
 export interface FlowTranslationProps {
   flowId: string | undefined;
@@ -7,14 +10,51 @@ export interface FlowTranslationProps {
 }
 
 export const FlowTranslation = ({ flowId, setDialog }: FlowTranslationProps) => {
+  const [action, setAction] = useState('auto');
+
+  const { t } = useTranslation();
+
+  const handleAuto = () => {
+    console.log('handle auto');
+  };
+  const handleExport = () => {
+    console.log('handle export');
+  };
+
+  const handleImport = () => {
+    console.log('handle import');
+  };
+
   const handleOk = () => {
-    console.log('handle ok!');
+    if (action === 'auto') {
+      handleAuto();
+    } else if (action === 'export') {
+      handleExport();
+    } else {
+      handleImport();
+    }
+
     setDialog(false);
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAction((event.target as HTMLInputElement).value);
   };
 
   const dialogContent = (
     <div>
-      <div>Show Options:</div>
+      <FormControl>
+        <RadioGroup
+          aria-labelledby="radio-buttons-group"
+          name="radio-buttons-group"
+          value={action}
+          onChange={handleChange}
+        >
+          <FormControlLabel value="auto" control={<Radio />} label={t('Automatic translation')} />
+          <FormControlLabel value="export" control={<Radio />} label={t('Export translations')} />
+          <FormControlLabel value="import" control={<Radio />} label={t('Import translations')} />
+        </RadioGroup>
+      </FormControl>
     </div>
   );
 
@@ -22,7 +62,7 @@ export const FlowTranslation = ({ flowId, setDialog }: FlowTranslationProps) => 
     <DialogBox
       title="Translate Flow"
       alignButtons="center"
-      buttonOk="Auto translate"
+      buttonOk="Submit"
       buttonCancel="Cancel"
       additionalTitleStyles={styles.Title}
       handleOk={handleOk}
