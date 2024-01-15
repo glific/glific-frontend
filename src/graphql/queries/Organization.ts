6cd2a41a531c815b813e13d3ce8deeac000f8ef1
mdshamoon <mohdshamoon841@gmail.com>
2024-01-15 16:14:37 +0530

    added fix for translation

--- src/graphql/queries/Organization.ts
+++ src/graphql/queries/Organization.ts
@@ -115,6 +115,7 @@ export const GET_ORGANIZATION_SERVICES = gql`
   query organizationServices {
     organizationServices {
       dialogflow
+      autoTranslationEnabled
       googleCloudStorage
       flowUuidDisplay
       rolesAndPermission
