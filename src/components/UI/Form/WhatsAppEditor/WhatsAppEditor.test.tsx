d9816aed47117b5e35d87cdaed55a2e7cd090c7c
Snehil Shah <130062020+Snehil-Shah@users.noreply.github.com>
2024-01-15 22:58:21 +0000

    Add Test Cases: WhatsAppEditor.tsx 34% to 85% Coverage
    Signed-off-by: Snehil Shah <130062020+Snehil-Shah@users.noreply.github.com>

--- src/components/UI/Form/WhatsAppEditor/WhatsAppEditor.test.tsx
+++ src/components/UI/Form/WhatsAppEditor/WhatsAppEditor.test.tsx
@@ -1,8 +1,8 @@
 import { render, fireEvent } from '@testing-library/react';
-import draftJs, { EditorState, ContentState } from 'draft-js';
+import draftJs, { EditorState, ContentState, SelectionState, RichUtils } from 'draft-js';
 import { vi } from 'vitest';
 
-import WhatsAppEditor from './WhatsAppEditor';
+import { WhatsAppEditor, updatedValue } from './WhatsAppEditor';
 
 const mockHandleKeyCommand = vi.fn();
 
@@ -48,6 +48,21 @@ describe('<WhatsAppEditor/>', () => {
 
   const editorContent = EditorState.createWithContent(ContentState.createFromText('Hello'));
 
+  function mockEditorCommands(command: string) {
+    vi.spyOn(draftJs, 'Editor').mockImplementation((props: any, _context: any) => {
+      const input: any = (
+        <input
+          data-testid='editor'
+          onClick={() => {
+            props.handleKeyCommand(command);
+          }}
+          onChange={(event) => props.onChange(event)}
+        ></input>
+      );
+      return input;
+    });
+  }
+
   test('input change should trigger callBacks', () => {
     const { getByTestId } = render(<WhatsAppEditor {...defaultProps(editorContent)} />);
     fireEvent.change(getByTestId('editor'), {
@@ -68,6 +83,58 @@ describe('<WhatsAppEditor/>', () => {
     fireEvent.click(getByTestId('resizer'));
     expect(handleHeightChange).toHaveBeenCalled();
   });
+
+  test('testing function updatedValue', () => {
+    // No selection with cursor at the beg
+    const selection = new SelectionState({
+      anchorKey: editorContent.getCurrentContent().getFirstBlock().getKey(),
+      anchorOffset: 0,
+      focusKey: editorContent.getCurrentContent().getFirstBlock().getKey(),
+      focusOffset: 0,
+    });
+
+    editorContent.getSelection = vi.fn().mockReturnValue(selection);
+
+    // Write 'Test' in the editor
+    const updatedEditorState = updatedValue('Test', editorContent);
+    const resultText = updatedEditorState.getCurrentContent().getPlainText();
+
+    expect(resultText).toBe('TestHello');
+  });
+
+  test('testing Enter Key Command',() => {
+    vi.mock('common/RichEditor', () => ({
+      getPlainTextFromEditor: vi.fn()
+    }));
+    mockEditorCommands('enter')
+    const { getByTestId } = render(<WhatsAppEditor {...defaultProps(editorContent)} />);
+    fireEvent.click(getByTestId('editor'));
+    expect(sendMessage).toHaveBeenCalled();
+  })
+
+  test('testing Bold Command', () => {
+    mockEditorCommands('bold')
+    const { getByTestId } = render(<WhatsAppEditor {...defaultProps(editorContent)} />);
+    fireEvent.click(getByTestId('editor'));
+    expect(setEditorState).toHaveBeenCalled();
+  })
+
+  test('testing italic Command', () => {
+    mockEditorCommands('italic')
+    const { getByTestId } = render(<WhatsAppEditor {...defaultProps(editorContent)} />);
+    fireEvent.click(getByTestId('editor'));
+    expect(setEditorState).toHaveBeenCalled();
+  })
+
+  test('testing custom Command', () => {
+    vi.spyOn(RichUtils, 'handleKeyCommand').mockImplementation(() => null);
+    mockEditorCommands('myCommand')
+    const { getByTestId } = render(<WhatsAppEditor {...defaultProps(editorContent)} />);
+    fireEvent.click(getByTestId('editor'));
+    expect(RichUtils.handleKeyCommand).toHaveBeenCalled();
+  })
+
+
 
   // since we are mocking emoji mart picker we need to implement the following functionalities
 
