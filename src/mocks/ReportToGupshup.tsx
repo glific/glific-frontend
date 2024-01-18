41c3a5e1ca39a21dfc2ad1bd9ded7e75d20aa752
akanshaaaa19 <akansha@projecttech4dev@org>
2024-01-18 09:19:33 +0530

    added testcases

---
+++ src/mocks/ReportToGupshup.tsx
@@ -0,0 +1,8 @@
+export const responseData = {
+    data: {
+        reportToGupshup: {
+            errors: null,
+            message: 'Successfully sent mail to Gupshup Support',
+        },
+    },
+};
