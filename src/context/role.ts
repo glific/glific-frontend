5e143cb4812c54057c18d8c908383b49aa65745d
mdshamoon <mohdshamoon841@gmail.com>
2024-02-17 16:23:07 +0530

    fixed all test cases

--- src/context/role.ts
+++ src/context/role.ts
@@ -43,7 +43,6 @@ export const setUserRolePermissions = () => {
   if (!role || role.length === 0) {
     role = getUserRole();
   }
-  console.log(role);
 
   const hasDynamicRole = checkDynamicRole();
 
