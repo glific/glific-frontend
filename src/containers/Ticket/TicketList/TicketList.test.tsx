bfd5b2eac36e2997b5aa6fd0577cd20b51cbd237
akanshaaaa19 <asakhre2002@gmail.com>
2024-03-26 11:31:02 +0530

    added more test cases

--- src/containers/Ticket/TicketList/TicketList.test.tsx
+++ src/containers/Ticket/TicketList/TicketList.test.tsx
@@ -1,12 +1,22 @@
-import { render, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { MockedProvider } from '@apollo/client/testing';
 import { MemoryRouter } from 'react-router-dom';
 
 import { setUserSession } from 'services/AuthService';
 import { TicketList } from './TicketList';
-import { ticketCountQuery, ticketListQuery } from 'mocks/Ticket';
+import { getTicketQuery, ticketCountQuery, ticketListQuery } from 'mocks/Ticket';
+import { getUsersQuery } from 'mocks/User';
+import { getAllFlowLabelsQuery } from 'mocks/Flow';
 
-const mocks = [ticketListQuery, ticketCountQuery];
+const mocks = [
+  ticketListQuery,
+  ticketCountQuery,
+  getUsersQuery,
+  ticketListQuery,
+  ticketCountQuery,
+  getTicketQuery,
+  getAllFlowLabelsQuery,
+];
 
 const wrapper = (
   <MockedProvider mocks={mocks} addTypename={false}>
@@ -24,5 +34,53 @@ test('should load the ticket list', async () => {
   expect(getByTestId('loading')).toBeInTheDocument();
   await waitFor(() => {
     expect(getByText('Tickets')).toBeInTheDocument();
+  });
+});
+
+test('it should edit', async () => {
+  const { getByText, getByTestId, getAllByTestId } = render(wrapper);
+
+  expect(getByTestId('loading')).toBeInTheDocument();
+
+  await waitFor(() => {
+    expect(getByText('Tickets')).toBeInTheDocument();
+  });
+
+  fireEvent.click(getAllByTestId('edit-icon')[0]);
+
+  await waitFor(() => {
+    expect(screen.getByTestId('dialogBox')).toBeInTheDocument();
+  });
+});
+
+test('it should click on export', async () => {
+  const { getByText, getByTestId, getAllByText } = render(wrapper);
+
+  expect(getByTestId('loading')).toBeInTheDocument();
+
+  await waitFor(() => {
+    expect(getByText('Tickets')).toBeInTheDocument();
+  });
+
+  fireEvent.click(getAllByText('Export')[0]);
+
+  await waitFor(() => {
+    expect(screen.getByTestId('dialogBox')).toBeInTheDocument();
+  });
+});
+
+test('it should click on bulk update', async () => {
+  const { getByText, getByTestId } = render(wrapper);
+
+  expect(getByTestId('loading')).toBeInTheDocument();
+
+  await waitFor(() => {
+    expect(getByText('Tickets')).toBeInTheDocument();
+  });
+
+  fireEvent.click(getByText('Bulk Update'));
+
+  await waitFor(() => {
+    expect(screen.getByTestId('dialogBox')).toBeInTheDocument();
   });
 });
--- src/mocks/Notifications.tsx
+++ src/mocks/Notifications.tsx
@@ -13,15 +13,53 @@ export const getNotificationsQuery = {
     data: {
       notifications: [
         {
+          category: 'Partner',
+          entity: '{"shortcode":"bigquery","id":2}',
+          id: '1',
+          isRead: true,
+          message:
+            'Disabling bigquery. Account does not have sufficient permissions to insert data to BigQuery.',
+          severity: '"Critical"',
+          updatedAt: '2024-03-23T15:26:41Z',
+        },
+        {
           category: 'Message',
           entity:
-            '{"status":"valid","phone":"8535124479","name":"Adelle Cavin","last_message_at":"2021-05-19T14:01:17Z","is_hsm":false,"id":9,"group_id":null,"flow_id":null,"bsp_status":"hsm"}',
-
-          id: '15',
-          isRead: false,
-          message: 'Could not send message to contact: Check Gupshup Setting',
-          severity: '"Critical"',
-          updatedAt: '2021-05-20T12:06:26Z',
+            '{"status":"valid","phone":"91987656789","name":"Adelle Cavin","last_message_at":"2021-03-23T17:05:01Z","is_hsm":null,"id":1,"group_id":null,"flow_id":1,"bsp_status":"hsm"}',
+          id: '2',
+          isRead: true,
+          message: 'Cannot send session message to contact, invalid bsp status.',
+          severity: '"Warning"',
+          updatedAt: '2024-03-23T15:26:41Z',
+        },
+        {
+          category: 'Flow',
+          entity:
+            '{"parent_id":6,"name":"Preference Workflow","flow_uuid":"12c25af0-37a2-4a69-8e26-9cfd98cab5c6","flow_id":3,"contact_id":3}',
+          id: '3',
+          isRead: true,
+          message: 'Cannot send session message to contact, invalid bsp status.',
+          severity: '"Warning"',
+          updatedAt: '2024-03-23T15:26:41Z',
+        },
+        {
+          category: 'Templates',
+          entity: null,
+          id: '4',
+          isRead: true,
+          message: 'Template Account balance has been approved',
+          severity: '"Information"',
+          updatedAt: '2024-03-23T15:26:41Z',
+        },
+        {
+          category: 'Ticket',
+          entity:
+            '{"uuid":"98c7dec4-f05a-4a76-a25a-f7a50d821f27","shortcode":"otp","label":"OTP Message","id":9}',
+          id: '5',
+          isRead: true,
+          message: 'Template OTP Message has been rejected',
+          severity: '"Information"',
+          updatedAt: '2024-03-23T15:26:41Z',
         },
       ],
     },
