b5ef2cc1fc732d7900ad8c3e227fcc8dd23e70c1
akanshaaaa19 <asakhre2002@gmail.com>
2024-04-15 21:02:12 +0530

    added more test cases

--- src/containers/Ticket/TicketList/BulkAction/BulkAction.test.tsx
+++ src/containers/Ticket/TicketList/BulkAction/BulkAction.test.tsx
@@ -1,4 +1,4 @@
-import { render, waitFor } from '@testing-library/react';
+import { fireEvent, render, waitFor } from '@testing-library/react';
 import { MockedProvider } from '@apollo/client/testing';
 import { BrowserRouter as Router } from 'react-router-dom';
 
@@ -6,14 +6,18 @@ import { setUserSession } from 'services/AuthService';
 import { BulkAction } from './BulkAction';
 import { getAllOrganizations } from 'mocks/Organization';
 import { getAllFlowLabelsQuery } from 'mocks/Flow';
+import * as Notification from 'common/notification';
+import { bulkActionQuery } from 'mocks/Ticket';
 
-const mocks = [...getAllOrganizations, getAllFlowLabelsQuery];
+const mocks = [...getAllOrganizations, getAllFlowLabelsQuery, bulkActionQuery];
 setUserSession(JSON.stringify({ organization: { id: '1' }, roles: ['Admin'] }));
+
+const setShowBulkCloseMock = vi.fn();
 
 const bulkUpdate = (
   <MockedProvider mocks={mocks} addTypename={false}>
     <Router>
-      <BulkAction setShowBulkClose={() => vi.fn()} />
+      <BulkAction setShowBulkClose={setShowBulkCloseMock} />
     </Router>
   </MockedProvider>
 );
@@ -23,5 +27,36 @@ it('Renders Export ticket component successfully', async () => {
 
   await waitFor(() => {
     expect(getByText('All tickets will be closed for this topic')).toBeInTheDocument();
+  });
+});
+
+it('Bulk updates successfully', async () => {
+  const notificationSpy = vi.spyOn(Notification, 'setNotification');
+  const { getByText, getByTestId } = render(bulkUpdate);
+
+  await waitFor(() => {
+    expect(getByText('All tickets will be closed for this topic')).toBeInTheDocument();
+  });
+
+  const autocomplete = getByTestId('autocomplete-element');
+  autocomplete.focus();
+  fireEvent.keyDown(autocomplete, { key: 'ArrowDown' });
+  fireEvent.keyDown(autocomplete, { key: 'ArrowDown' });
+  fireEvent.keyDown(autocomplete, { key: 'Enter' });
+
+  fireEvent.click(getByTestId('ok-button'));
+
+  await waitFor(() => {
+    expect(notificationSpy).toHaveBeenCalledWith('Tickets closed successfully');
+  });
+});
+
+it('Closes the dialog box on clicking cancel ', async () => {
+  const { getByText, getByTestId } = render(bulkUpdate);
+
+  fireEvent.click(getByTestId('cancel-button'));
+
+  await waitFor(() => {
+    expect(setShowBulkCloseMock).toHaveBeenCalled();
   });
 });
