4ec33db000f2173876a132705c4d59383a5ea541
akanshaaaa19 <asakhre2002@gmail.com>
2024-04-17 20:31:22 +0530

    changed text to lowercase

--- src/containers/Auth/Promotion/Promotion.tsx
+++ src/containers/Auth/Promotion/Promotion.tsx
@@ -28,7 +28,7 @@ export const Promotion = () => {
 
       {!minimized && (
         <>
-          <div className={styles.Image}>Glific payment timelines & Late Payment Consequences</div>
+          <div className={styles.Image}>Glific payment timelines & late payment consequences</div>
 
           <div className={styles.Points}>
             <div className={styles.BodyListText}>
