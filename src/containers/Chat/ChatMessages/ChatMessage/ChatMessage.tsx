5393f241104d25292823f58c89da173b14a65424
Akansha Sakhre <asakhre2002@gmail.com>
2025-01-20 11:30:33 +0530

    removed download icon

--- src/containers/Chat/ChatMessages/ChatMessage/ChatMessage.tsx
+++ src/containers/Chat/ChatMessages/ChatMessage/ChatMessage.tsx
@@ -56,6 +56,7 @@ export interface ChatMessageProps {
   groups?: boolean;
   status?: string;
   contact?: any;
+  showIcon?: boolean;
 }
 
 export const ChatMessage = ({
@@ -81,6 +82,7 @@ export const ChatMessage = ({
   groups,
   status,
   contact,
+  showIcon = true,
 }: ChatMessageProps) => {
   const [showSaveMessageDialog, setShowSaveMessageDialog] = useState(false);
   const Ref = useRef(null);
@@ -335,8 +337,8 @@ export const ChatMessage = ({
           </Tooltip>
         ) : null}
 
-        <div className={styles.Inline}>
-          {iconLeft && icon}
+        <div className={`${styles.Inline} ${!showIcon && styles.NoIcon}`}>
+          {showIcon && iconLeft && icon}
           {ErrorIcon}
           <div className={chatMessageClasses.join(' ')}>
             <Tooltip title={tooltipTitle} placement={isSender ? 'right' : 'left'}>
@@ -361,47 +363,49 @@ export const ChatMessage = ({
               </div>
             </Tooltip>
 
-            <Popper
-              id={popperId}
-              open={open}
-              modifiers={[
-                {
-                  name: 'preventOverflow',
-                  options: {
-                    altBoundary: true,
+            {showIcon && (
+              <Popper
+                id={popperId}
+                open={open}
+                modifiers={[
+                  {
+                    name: 'preventOverflow',
+                    options: {
+                      altBoundary: true,
+                    },
                   },
-                },
-              ]}
-              anchorEl={anchorEl}
-              placement={placement}
-              transition
-              data-testid="popup"
-            >
-              {({ TransitionProps }) => (
-                <Fade {...TransitionProps} timeout={350}>
-                  <Paper elevation={3}>
-                    <Button className={styles.Popper} color="primary" onClick={() => setShowSaveMessageDialog(true)}>
-                      {t('Add to speed sends')}
-                    </Button>
-                    {type !== 'TEXT' && (
-                      <span>
-                        <br />
-                        <Button
-                          className={styles.Popper}
-                          color="primary"
-                          onClick={() => downloadMedia()}
-                          data-testid="downloadMedia"
-                        >
-                          {t('Download media')}
-                        </Button>
-                      </span>
-                    )}
-                  </Paper>
-                </Fade>
-              )}
-            </Popper>
+                ]}
+                anchorEl={anchorEl}
+                placement={placement}
+                transition
+                data-testid="popup"
+              >
+                {({ TransitionProps }) => (
+                  <Fade {...TransitionProps} timeout={350}>
+                    <Paper elevation={3}>
+                      <Button className={styles.Popper} color="primary" onClick={() => setShowSaveMessageDialog(true)}>
+                        {t('Add to speed sends')}
+                      </Button>
+                      {type !== 'TEXT' && (
+                        <span>
+                          <br />
+                          <Button
+                            className={styles.Popper}
+                            color="primary"
+                            onClick={() => downloadMedia()}
+                            data-testid="downloadMedia"
+                          >
+                            {t('Download media')}
+                          </Button>
+                        </span>
+                      )}
+                    </Paper>
+                  </Fade>
+                )}
+              </Popper>
+            )}
           </div>
-          {iconLeft ? null : icon}
+          {iconLeft ? null : showIcon && icon}
         </div>
         <div className={styles.SendBy}>{sendBy}</div>
 
