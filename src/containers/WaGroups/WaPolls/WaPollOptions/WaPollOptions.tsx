9647e915f1e262532f64ca55a5ee597dc3496636
Akansha Sakhre <asakhre2002@gmail.com>
2025-02-17 12:01:02 +0530

    deepscan fixes

--- src/containers/WaGroups/WaPolls/WaPollOptions/WaPollOptions.tsx
+++ src/containers/WaGroups/WaPolls/WaPollOptions/WaPollOptions.tsx
@@ -21,8 +21,8 @@ const emojiStyles = {
 
 export const WaPollOptions = ({ form: { values, setFieldValue, errors, touched }, isEditing }: WaPollOptionsProps) => {
   const handleAddOption = () => {
-    const lastId = values?.options[values?.options.length - 1]?.id;
-    setFieldValue('options', [...values?.options, { name: '', id: lastId + 1 }]);
+    const lastId = values.options[values.options.length - 1]?.id;
+    setFieldValue('options', [...values.options, { name: '', id: lastId + 1 }]);
   };
 
   const handleInput = (value: any, id: any) => {
