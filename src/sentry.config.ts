0a1c2f207c5e8b667bbd6a278f66b38d5f908a2e
Akansha Sakhre <asakhre2002@gmail.com>
2025-04-22 15:26:20 +0530

    using vite env mode

--- src/sentry.config.ts
+++ src/sentry.config.ts
@@ -2,7 +2,7 @@ import * as Sentry from '@sentry/react';
 
 Sentry.init({
   dsn: import.meta.env.VITE_SENTRY_DSN,
-  environment: import.meta.env.VITE_ENV || 'development',
+  environment: import.meta.env.MODE || 'development',
 
   sendDefaultPii: true,
   tracesSampleRate: 1,
