230e26a838c901b977b55180f95af082a0fde3a5
priyanshu6238 <priyanshusingh0520@gmail.com>
2025-06-19 18:25:24 +0530

    fix: correct comment typo and enhance language selection logic to prioritize English

--- src/containers/HSM/HSM.tsx
+++ src/containers/HSM/HSM.tsx
@@ -673,11 +673,14 @@ export const HSM = () => {
   useEffect(() => {
     if (languages) {
       const lang = languages.currentUser.user.organization.activeLanguages.slice();
-      // sort languages by thaeir name
+      // sort languages by their name
       lang.sort((first: any, second: any) => (first.label > second.label ? 1 : -1));
-
       setLanguageOptions(lang);
-      if (!isEditing) setLanguageId(lang[0]);
+      if (!isEditing) {
+        // Try to find English
+        const englishLang = lang.find((l: any) => l.label.toLowerCase() === 'english');
+        setLanguageId(englishLang || lang[0]);
+      }
     }
   }, [languages]);
 
