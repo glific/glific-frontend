327c2acfe0ee4400c4400d66aa23bd80238142d6
Akansha Sakhre <asakhre2002@gmail.com>
2025-09-16 10:30:08 +0530

    Remove unused navigate import from NotificationList component

--- src/containers/NotificationList/NotificationList.tsx
+++ src/containers/NotificationList/NotificationList.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState, useRef } from 'react';
 import { Popover, FormControlLabel, RadioGroup, Radio } from '@mui/material';
-import { useNavigate } from 'react-router';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import { useApolloClient, useMutation } from '@apollo/client';
 import dayjs from 'dayjs';
@@ -42,8 +41,6 @@ export const NotificationList = () => {
   const [text, setText] = useState<any>();
   const { t } = useTranslation();
   const [filter, setFilter] = useState<any>('');
-
-  const navigate = useNavigate();
 
   const menuRef = useRef(null);
 
