4c4039526eb336f11ce67ea7aed7d5e6f889886e
priyanshu6238 <priyanshusingh0520@gmail.com>
2025-09-22 18:51:48 +0530

    fix: Update setCredential function to handle 'gupshup' type app_id condition

--- src/containers/SettingList/Providers/Providers.tsx
+++ src/containers/SettingList/Providers/Providers.tsx
@@ -50,7 +50,7 @@ export const Providers = () => {
   const setCredential = (item: any) => {
     const keysObj = JSON.parse(item.keys);
     const secretsObj = JSON.parse(item.secrets);
-    if (secretsObj.app_id === 'NA') {
+    if (type === 'gupshup' && secretsObj.app_id === 'NA') {
       secretsObj.app_id = 'To be updated';
       secretsObj.app_name = 'To be updated';
       secretsObj.api_key = 'To be updated';
