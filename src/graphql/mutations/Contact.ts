1c7daa16c3358fe983b5a4aa571241ffaa0e69dc
priyanshu6238 <priyanshusingh0520@gmail.com>
2025-09-22 10:03:45 +0530

    Fix: Remove unused parameter from ImportContacts mutation

--- src/graphql/mutations/Contact.ts
+++ src/graphql/mutations/Contact.ts
@@ -56,8 +56,8 @@ export const MOVE_CONTACTS = gql`
 `;
 
 export const IMPORT_CONTACTS = gql`
-  mutation ImportContacts($data: String!, $groupLabel: String, $importContactsId: ID, $type: ImportContactsTypeEnum) {
-    importContacts(data: $data, groupLabel: $groupLabel, id: $importContactsId, type: $type) {
+  mutation ImportContacts($data: String!, $groupLabel: String, $type: ImportContactsTypeEnum) {
+    importContacts(data: $data, groupLabel: $groupLabel, type: $type) {
       status
       errors {
         message
