11a1029f73d9f1dc444a9ad992f8a019bc219d0c
priyanshu6238 <priyanshusingh0520@gmail.com>
2025-10-29 17:03:48 +0530

    Refactor: rename google sheet

--- src/containers/SettingList/SettingList.tsx
+++ src/containers/SettingList/SettingList.tsx
@@ -58,7 +58,13 @@ export const SettingList = () => {
     const sortedProviders = providers.sort((first: any, second: any) => (first.name > second.name ? 1 : -1));
 
     // create setting list of Organisation & providers
-    providersList = [...sortedProviders];
+    providersList = sortedProviders.map((provider: any) => {
+      console.log(provider.name);
+      if (provider.name === 'Google sheet') {
+        return { ...provider, name: 'Google Sheet' };
+      }
+      return provider;
+    });
   }
 
   const drawer = (
