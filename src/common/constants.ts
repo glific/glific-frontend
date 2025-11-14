42c15e9db73d9be71f1ed211b5b754aa381a3f68
Akansha Sakhre <asakhre2002@gmail.com>
2025-11-14 15:08:15 +0530

    Add Gupshup WhatsApp Form constant and update TemplateOptions for structured data collection

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -174,6 +174,8 @@ export const GUPSHUP_CALL_TO_ACTION =
 export const GUPSHUP_QUICK_REPLY =
   'You may get user responses via buttons. Whatsapp allows 3 quick replies. These are static unline the call to actions where you can define call or link actions.';
 
+export const GUPSHUP_WHATSAPP_FORM = 'Whatsapp Forms allow you to collect structured data from users.';
+
 // Call to action button
 export const CALL_TO_ACTION = 'CALL_TO_ACTION';
 export const LIST = 'LIST';
