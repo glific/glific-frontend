978ee107bae9ff491611540a5d623b272edf5e65
Akansha Sakhre <asakhre2002@gmail.com>
2025-11-14 11:54:27 +0530

    Enhance WhatsApp Form integration by updating button structure and handling in HSM and TemplateOptions components

--- src/containers/HSM/HSM.helper.ts
+++ src/containers/HSM/HSM.helper.ts
@@ -1,4 +1,4 @@
-import { CALL_TO_ACTION, MEDIA_MESSAGE_TYPES, QUICK_REPLY } from 'common/constants';
+import { CALL_TO_ACTION, MEDIA_MESSAGE_TYPES, QUICK_REPLY, WHATSAPP_FORM } from 'common/constants';
 
 export interface CallToActionTemplate {
   type: string;
@@ -43,6 +43,9 @@ export const convertButtonsToTemplate = (templateButtons: Array<any>, templateTy
     if (templateType === QUICK_REPLY && value) {
       result.push(`[${value}]`);
     }
+    if (templateType === WHATSAPP_FORM && temp.form_id && temp.text && temp.navigate_screen) {
+      result.push(`[${temp.text}, ${temp.navigate_screen}, ${temp.form_id}]`);
+    }
     return result;
   }, []);
 
@@ -71,6 +74,13 @@ export const getTemplateAndButtons = (templateType: string, message: string, but
     result = templateButtons.map((button: any) => {
       const { text, type } = button;
       return { type, value: text };
+    });
+  }
+
+  if (templateType === WHATSAPP_FORM) {
+    result = templateButtons.map((button: any) => {
+      const { flow_id, text, navigate_screen } = button;
+      return { form_id: flow_id, text, navigate_screen };
     });
   }
 
