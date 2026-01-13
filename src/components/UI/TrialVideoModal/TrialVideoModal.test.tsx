46089aaff625c72f3e5e4d47c648a0c2b34b57d3
AmishaBisht <amishabisht33@gmail.com>
2026-01-13 14:44:44 +0530

    chore: fix codacy warnings

--- src/components/UI/TrialVideoModal/TrialVideoModal.test.tsx
+++ src/components/UI/TrialVideoModal/TrialVideoModal.test.tsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import TrialVideoModal from './TrialVideoModal';
 
