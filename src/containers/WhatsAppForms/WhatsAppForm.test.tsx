0a82dec5b803266b64218a59ce1390534a0563c5
priyanshu6238 <priyanshusingh0520@gmail.com>
2026-01-14 08:31:28 +0530

    refactor: change tiltle for whatsapp form

--- src/containers/WhatsAppForms/WhatsAppForm.test.tsx
+++ src/containers/WhatsAppForms/WhatsAppForm.test.tsx
@@ -69,7 +69,7 @@ describe('<WhatsAppForms />', () => {
     expect(getByText('Loading...')).toBeInTheDocument();
 
     await waitFor(() => {
-      expect(getByText('WhatsApp Form')).toBeInTheDocument();
+      expect(getByText('Edit WhatsApp Form')).toBeInTheDocument();
     });
 
     const inputs = getAllByRole('textbox');
