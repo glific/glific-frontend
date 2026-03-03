c96e2ca6d7f1f838ed7407bfaa78960f6fe79434
Shijith Karumathil <shijithjayan@gmail.com>
2026-03-03 19:47:48 +0530

    Fix Unified API Bugs (#3796)

--- src/containers/Assistants/AssistantOptions/AssistantOptions.tsx
+++ src/containers/Assistants/AssistantOptions/AssistantOptions.tsx
@@ -82,7 +82,7 @@ export const AssistantOptions = ({
 
     const validFiles = Array.from(inputFiles).filter((file: any) => {
       if (file.size / (1024 * 1024) > 20) {
-        setNotification('File size should be less than 20MB', 'error');
+        setNotification('File size should be less than 20MB', 'warning');
         return false;
       }
       return true;
@@ -262,7 +262,7 @@ export const AssistantOptions = ({
             )}
           </div>
         )}
-        {formikTouched?.knowledgeBaseId && formikErrors?.knowledgeBaseId && (
+        {formikTouched?.knowledgeBaseId && !formikValues.knowledgeBaseId && (
           <p className={styles.ErrorText}>{formikErrors.knowledgeBaseId}</p>
         )}
       </div>
