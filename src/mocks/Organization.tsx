14c630258d9de0ec9680af58b52a7c7d3b9fb515
priyanshu6238 <priyanshusingh0520@gmail.com>
2026-07-17 08:50:04 +0530

    feat: add templateV2Enabled support for HSM Templates routing and update related services

--- src/mocks/Organization.tsx
+++ src/mocks/Organization.tsx
@@ -259,6 +259,7 @@ export const getOrganizationServicesQuery = {
         whatsappGroupEnabled: true,
         copyNodeEnabled: false,
         promptGeneratorEnabled: false,
+        templateV2Enabled: true,
       },
     },
   },
