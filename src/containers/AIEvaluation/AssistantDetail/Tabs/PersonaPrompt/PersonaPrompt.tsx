fe98399ea3a520d6e6cbb170edb2e03782c5d48d
Ayush8923 <80516839+Ayush8923@users.noreply.github.com>
2026-08-12 12:34:56 +0530

    fix(*): remove the unused import

--- src/containers/AIEvaluation/AssistantDetail/Tabs/PersonaPrompt/PersonaPrompt.tsx
+++ src/containers/AIEvaluation/AssistantDetail/Tabs/PersonaPrompt/PersonaPrompt.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useState } from 'react';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { BetaTag } from 'components/UI/BetaTag/BetaTag';
 import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
